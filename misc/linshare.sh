0ae0d579e7f5ceba8627f84f1e07acd89509a95c
Sébastien BAHLOUL <sbahloul@linagora.com>
2010-02-02 12:48:54 +0000

    Adding LinShare home settings to launch script

--- misc/linshare.sh
+++ misc/linshare.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-export JETTY_OPTS=-Dorg.linshare.demo=true
-`dirname $0`/bin/jetty.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
+export LINSHARE_HOME="`dirname $0`"
+
+export JETTY_OPTS="-Dorg.linshare.demo=true -Dlinshare.files.directory=\"$LINSHARE_HOME/var\" -Dlinshare.files.configuration=\"$LINSHARE_HOME/etc\""
+
+$LINSHARE_HOME/bin/jetty.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
