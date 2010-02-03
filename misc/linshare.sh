f0bee1d89d3012e9aa84b9ff5d7b6d810502934f
Sébastien BAHLOUL <sbahloul@linagora.com>
2010-02-03 22:09:31 +0000

    Updated script

--- misc/linshare.sh
+++ misc/linshare.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export LINSHARE_HOME="`dirname $0`"
+export LINSHARE_HOME=$( cd `dirname $0` && pwd )
 
-export JETTY_OPTS="-Dorg.linshare.demo=true -Dlinshare.files.directory=\"$LINSHARE_HOME/var\" -Dlinshare.files.configuration=\"$LINSHARE_HOME/etc\""
+
+export JAVA_OPTIONS="-Dorg.linshare.demo=true -Dlinshare.files.directory=$LINSHARE_HOME/var -Dlinshare.config.path=$LINSHARE_HOME/etc"
 
 $LINSHARE_HOME/bin/jetty.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
