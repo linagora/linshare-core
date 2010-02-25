ddfab3fc31df9c6747a929a50b2b6652307cb428
Matthieu LABORIE <mlaborie@linagora.com>
2010-02-25 15:06:07 +0000

    #44 Add default properties and change installer to configure smtp

--- misc/linshare.sh
+++ misc/linshare.sh
@@ -3,6 +3,6 @@
 export LINSHARE_HOME=$( cd `dirname $0` && pwd )
 
 
-export JAVA_OPTIONS="-Dorg.mortbay.jetty.webapp.parentLoaderPriority=true -Dorg.linshare.demo=true -DLINSHARE_HOME=$LINSHARE_HOME -Dlinshare.files.directory=$LINSHARE_HOME/var"
+export JAVA_OPTIONS="-Dorg.mortbay.jetty.webapp.parentLoaderPriority=true -DLINSHARE_HOME=$LINSHARE_HOME "
 
 $LINSHARE_HOME/jetty/bin/jetty.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
