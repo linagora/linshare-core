9e1eb1961b18771bd08ff8b88be0bf30008f9d6b
Matthieu LABORIE <mlaborie@linagora.com>
2010-02-26 11:30:05 +0000

    Add UTF-8 encoding for windows and mac

--- misc/linshare.bat
+++ misc/linshare.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%OS%" == "Windows_NT" setlocal
 set CURRENT_DIR=%cd%
-set JAVA_OPTIONS=-Xmx512m -Xms256m  -Dorg.mortbay.jetty.webapp.parentLoaderPriority=true -DLINSHARE_HOME="%CURRENT_DIR%"
+set JAVA_OPTIONS=-Xmx512m -Xms256m -Dfile.encoding=UTF-8 -Dorg.mortbay.jetty.webapp.parentLoaderPriority=true -DLINSHARE_HOME="%CURRENT_DIR%"
 
 
 set _EXECJAVA=start java
--- misc/linshare.sh
+++ misc/linshare.sh
@@ -3,6 +3,6 @@
 export LINSHARE_HOME=$( cd `dirname $0` && pwd )
 
 
-export JAVA_OPTIONS="-Dorg.mortbay.jetty.webapp.parentLoaderPriority=true -DLINSHARE_HOME=$LINSHARE_HOME "
+export JAVA_OPTIONS="-Xmx512m -Xms256m -Dfile.encoding=UTF-8 -Dorg.mortbay.jetty.webapp.parentLoaderPriority=true -DLINSHARE_HOME=$LINSHARE_HOME "
 
 $LINSHARE_HOME/jetty/bin/jetty.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
