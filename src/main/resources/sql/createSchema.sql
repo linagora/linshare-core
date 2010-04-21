d9d13938a5662ffb1e0b0ea84f4fce604c0fe561
Sébastien BAHLOUL <sbahloul@linagora.com>
2010-04-21 16:41:35 +0000

    fixing spaces/newlines and include a cas profile

--- src/main/resources/sql/createSchema.sql
+++ src/main/resources/sql/createSchema.sql
@@ -390,4 +390,4 @@
 
     create sequence hibernate_sequence;
 
-    insert into linshare_version (id, description) values (7, 'LinShare version 0.7');
+    insert into linshare_version (id, description) values (8, 'LinShare version 0.8');
