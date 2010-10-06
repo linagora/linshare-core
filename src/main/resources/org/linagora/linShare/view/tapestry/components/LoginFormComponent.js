598df0323dbae82221aabe0c787bb8b4e39db5aa
Stéphanie DUPREY <sduprey@linagora.com>
2010-10-06 14:06:58 +0000

    Fix js NPE

--- src/main/resources/org/linagora/linShare/view/tapestry/components/LoginFormComponent.js
+++ src/main/resources/org/linagora/linShare/view/tapestry/components/LoginFormComponent.js
@@ -4,16 +4,21 @@ function submitWithCookie() {
 }
 
 document.observe("dom:loaded", function() {
-	var ca = document.cookie.split(';');
-	var login = "";
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') {
-			c = c.substring(1,c.length);
+	var loginElt = document.getElementById('login');
+	
+	if (loginElt != null) {
+		var ca = document.cookie.split(';');
+		var login = "";
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') {
+				c = c.substring(1,c.length);
+			}
+			if (c.indexOf("linshare_persistent_login=") == 0) {
+				login = c.substring("linshare_persistent_login=".length,c.length);
+				break;
+			}
 		}
-		if (c.indexOf("linshare_persistent_login=") == 0) {
-			login = c.substring("linshare_persistent_login=".length,c.length);
-		}
+		loginElt.value=login;
 	}
-	document.getElementById('login').value=login;
 });
