522834acda2e05fee49acc9c4178d14de27f5d4a
sduprey <sduprey@linagora.com>
2010-11-22 09:00:37 +0000

    fixes #148: avoid Junk status for LinShare emails : we should not set "From" with the user mail address since it could be a guest and have another domain than linshare domain. Set From=linshare@yourdomain and Reply-to=the user mail address.

--- src/main/java/org/linagora/linShare/core/service/impl/MailNotifierServiceImpl.java
+++ src/main/java/org/linagora/linShare/core/service/impl/MailNotifierServiceImpl.java
@@ -17,7 +17,7 @@
  *
  *   (c) 2008 Groupe Linagora - http://linagora.org
  *
-*/
+ */
 package org.linagora.linShare.core.service.impl;
 
 import java.util.Properties;
@@ -41,194 +41,198 @@ import org.linagora.linShare.core.exception.TechnicalErrorCode;
 import org.linagora.linShare.core.exception.TechnicalException;
 import org.linagora.linShare.core.service.NotifierService;
 
-
 /**
- *
+ * This class builds an email notification and sends the email.
+ * 
+ * 0.8.2 : now the sender is always linshare email address and
+ * the email address of the human acting in LinShare is in the 
+ * REPLY-TO address (avoid spam exclusion).
  */
 public class MailNotifierServiceImpl implements NotifierService {
 
-    /** The smtpServer that will send the email. */
-    private final String smtpServer;
+	/** The smtpServer that will send the email. */
+	private final String smtpServer;
 
-    /** The smtp user. */
-    private final String smtpUser;
+	/** The smtp user. */
+	private final String smtpUser;
 
-    /** The sender identity. */
-    private final String smtpSender;
+	/** The sender identity. */
+	private final String smtpSender;
 
-    /** The smtp password. */
-    private final String smtpPassword;
-    
-    /** The smtp port. */
-    private  final int smtpPort;
+	/** The smtp password. */
+	private final String smtpPassword;
 
-    /** Is the server needs authentification. */
-    private  final boolean needsAuth;
+	/** The smtp port. */
+	private final int smtpPort;
 
-    /** Mail charset. */
-    private final String charset;
-    
+	/** Is the server needing authentification. */
+	private final boolean needsAuth;
 
-    private final static Log logger = LogFactory.getLog(MailNotifierServiceImpl.class);
-    
+	/** Mail charset. */
+	private final String charset;
 
-    /**
-     * see  http://java.sun.com/developer/EJTechTips/2004/tt0625.html for multipart/alternative
-     * @param smtpServer
-     * @param smtpPort
-     * @param smtpSender
-     * @param smtpUser
-     * @param smtpPassword
-     * @param needsAuth
-     * @param charset
-     */
-    
-    public MailNotifierServiceImpl(String smtpServer,int smtpPort,  String smtpSender, String smtpUser, String smtpPassword,
-        boolean needsAuth, String charset) {
-        this.smtpServer = smtpServer;
-        this.smtpPort = smtpPort;
-        this.smtpSender = smtpSender;
-        this.smtpUser = smtpUser;
-        this.smtpPassword = smtpPassword;
-        this.needsAuth = needsAuth;
-        this.charset = charset;
-    }
+	/** Class logger */
+	private final static Log logger = LogFactory.getLog(MailNotifierServiceImpl.class);
 
-    
-    
-    
+	/**
+	 * see http://java.sun.com/developer/EJTechTips/2004/tt0625.html for
+	 * multipart/alternative
+	 */
+	public MailNotifierServiceImpl(String smtpServer, int smtpPort,
+			String smtpSender, String smtpUser, String smtpPassword,
+			boolean needsAuth, String charset) {
+		this.smtpServer = smtpServer;
+		this.smtpPort = smtpPort;
+		this.smtpSender = smtpSender;
+		this.smtpUser = smtpUser;
+		this.smtpPassword = smtpPassword;
+		this.needsAuth = needsAuth;
+		this.charset = charset;
+	}
 
+	/**
+	 * Send notification to a recipient.
+	 * 
+	 * @param recipient
+	 *            notification recipient.
+	 * @param subject
+	 *            subject.
+	 * @param content
+	 *            content.
+	 */
+	public void sendNotification(String fromUser, String recipient,
+			String subject, String htmlContent, String textContent) {
 
-	/** Send notification to a recipient.
-     * @param recipient notification recipient.
-     * @param subject subject.
-     * @param content content.
-     */
-    public void sendNotification(String fromUser, String recipient, String subject, String htmlContent, String textContent) {
+		// get the mail session
+		Session session = getMailSession();
 
-       // get the mail session
-        Session session = getMailSession();
+		// Define message
+		MimeMessage messageMim = new MimeMessage(session);
 
-        // Define message
-        MimeMessage messageMim = new MimeMessage(session);
-                try {
-            
-            if(fromUser==null)
-            messageMim.setFrom(new InternetAddress(smtpSender));
-            else    	
-            messageMim.setFrom(new InternetAddress(fromUser));
+		try {
+			messageMim.setFrom(new InternetAddress(smtpSender));
 
-            messageMim.addRecipient(javax.mail.Message.RecipientType.TO,
-                    new InternetAddress(recipient));
+			if (fromUser != null) {
+				InternetAddress reply[] = new InternetAddress[1];
+				reply[0] = new InternetAddress(fromUser);
+				messageMim.setReplyTo(reply);
+			}
 
-            messageMim.setSubject(subject,charset);
-            
-            
-            
- 	       // Create a "related" Multipart message
-           //content type is multipart/alternative
-           //it will contain two part BodyPart 1 and 2
- 	       Multipart mp = new MimeMultipart("alternative");
-            
- 	      //BodyPart 1 is text file
- 	      BodyPart alt_bp1 = new MimeBodyPart();
- 	      alt_bp1.setDataHandler(new DataHandler(new ByteArrayDataSource(textContent,"text/plain")));
-	      mp.addBodyPart(alt_bp1); 
-          
-	       
-	      //BodyPart 2 
-	      //content type is multipart/related
-	      //A multipart/related is used to indicate that message parts should not be considered individually but rather
-	      //as parts of an aggregate whole. The message consists of a root part (by default, the first) which reference other parts inline,
-	      //which may in turn reference other parts.
-	      Multipart html_mp = new MimeMultipart("related");
-	      
-	      //Include an HTML message with images.
-	      //BodyParts: the HTML file and an image 
+			messageMim.addRecipient(javax.mail.Message.RecipientType.TO,
+					new InternetAddress(recipient));
 
-	       // Get the HTML file
-	       BodyPart rel_bph = new MimeBodyPart();
-	       rel_bph.setDataHandler(new DataHandler(new ByteArrayDataSource(htmlContent,"text/html; charset="+charset)));
-	       html_mp.addBodyPart(rel_bph);
-	      
+			messageMim.setSubject(subject, charset);
 
-		    //inline image ?
-		    String cid = "image.part.1@linshare.org";
-		    MimeBodyPart rel_bpi = new MimeBodyPart();
-		       // Initialize and add the image file to the html body part
-		   rel_bpi.setFileName("mail_logo.png");
-		   rel_bpi.setText("linshare");
-		   
-		   
-		   rel_bpi.setDataHandler(new DataHandler(getClass().getResource("/org/linagora/linShare/core/service/mail_logo.png")));
-		   rel_bpi.setHeader("Content-ID", "<" + cid + ">");
-		   rel_bpi.setDisposition("inline");
-		   html_mp.addBodyPart(rel_bpi);
-	
-	       // Create the second BodyPart of the multipart/alternative,
-	       // set its content to the html multipart, and add the
-	       // second bodypart to the main multipart.
-	       BodyPart alt_bp2 = new MimeBodyPart();
-	       alt_bp2.setContent(html_mp);
-	       mp.addBodyPart(alt_bp2);
-            
-            messageMim.setContent(mp);
-            // Since we used html tags, the content must be marker as text/html
-            //messageMim.setContent(content,"text/html; charset="+charset);
-          
-            
-            Transport tr = session.getTransport("smtp");
-         
-            // Connect to smtp server, if needed
-            if (needsAuth) {
-                tr.connect(smtpServer, smtpPort ,smtpUser, smtpPassword);
-                messageMim.saveChanges();
-                tr.sendMessage(messageMim, messageMim.getAllRecipients());
-                tr.close();
-            } else {
-                // Send message
-            	Transport.send(messageMim);
-            }
-        } catch (MessagingException e) {
-        	logger.error("Error sending notification on "+smtpServer+" port "+smtpPort,e);
-            throw new TechnicalException(TechnicalErrorCode.MAIL_EXCEPTION,  "Error sending notification",e);
-        }  catch (Exception e) {
-          	logger.error("Error sending notification on "+smtpServer+" port "+smtpPort,e);
-            throw new TechnicalException(TechnicalErrorCode.MAIL_EXCEPTION, "Error sending notification",e);
-        }
-    }
+			// Create a "related" Multipart message
+			// content type is multipart/alternative
+			// it will contain two part BodyPart 1 and 2
+			Multipart mp = new MimeMultipart("alternative");
 
+			// BodyPart 1 is text file
+			BodyPart alt_bp1 = new MimeBodyPart();
+			alt_bp1.setDataHandler(new DataHandler(new ByteArrayDataSource(
+					textContent, "text/plain")));
+			mp.addBodyPart(alt_bp1);
 
-    private Session getMailSession(){
-    	
-    	 // create some properties and get the default Session
-    	
-    	// Set the host smtp address
-        Properties props = new Properties();
-        props.put("mail.smtp.host", smtpServer);
-        props.put("mail.smtp.port", smtpPort+"");
+			// BodyPart 2
+			// content type is multipart/related
+			// A multipart/related is used to indicate that message parts should
+			// not be considered individually but rather
+			// as parts of an aggregate whole. The message consists of a root
+			// part (by default, the first) which reference other parts inline,
+			// which may in turn reference other parts.
+			Multipart html_mp = new MimeMultipart("related");
 
-        if (needsAuth){
-            props.put("mail.smtp.auth", "true");
-        }else{
-            props.put("mail.smtp.auth", "false");
-        }
-        // create some properties and get the default Session
-        Session session = Session.getInstance(props, null);
-       if(logger.isDebugEnabled()){
-    	   session.setDebug(true);
-       } else {
-    	  session.setDebug(false);
-       }
+			// Include an HTML message with images.
+			// BodyParts: the HTML file and an image
 
-        return session;
-    }
+			// Get the HTML file
+			BodyPart rel_bph = new MimeBodyPart();
+			rel_bph.setDataHandler(new DataHandler(new ByteArrayDataSource(
+					htmlContent, "text/html; charset=" + charset)));
+			html_mp.addBodyPart(rel_bph);
 
-    public void sendNotification(String fromUser, String recipient,
-    		MailContainer mailContainer) {
-    	sendNotification(fromUser, recipient, mailContainer.getSubject(), 
-    			mailContainer.getContentHTML(), mailContainer.getContentTXT());
-    	
-    }
-    
+			// inline image ?
+			String cid = "image.part.1@linshare.org";
+			MimeBodyPart rel_bpi = new MimeBodyPart();
+			
+			// Initialize and add the image file to the html body part
+			rel_bpi.setFileName("mail_logo.png");
+			rel_bpi.setText("linshare");
+			rel_bpi.setDataHandler(new DataHandler(getClass().getResource(
+					"/org/linagora/linShare/core/service/mail_logo.png")));
+			rel_bpi.setHeader("Content-ID", "<" + cid + ">");
+			rel_bpi.setDisposition("inline");
+			html_mp.addBodyPart(rel_bpi);
+
+			// Create the second BodyPart of the multipart/alternative,
+			// set its content to the html multipart, and add the
+			// second bodypart to the main multipart.
+			BodyPart alt_bp2 = new MimeBodyPart();
+			alt_bp2.setContent(html_mp);
+			mp.addBodyPart(alt_bp2);
+
+			messageMim.setContent(mp);
+			
+			// Since we used html tags, the content must be marker as text/html
+			// messageMim.setContent(content,"text/html; charset="+charset);
+
+			Transport tr = session.getTransport("smtp");
+
+			// Connect to smtp server, if needed
+			if (needsAuth) {
+				tr.connect(smtpServer, smtpPort, smtpUser, smtpPassword);
+				messageMim.saveChanges();
+				tr.sendMessage(messageMim, messageMim.getAllRecipients());
+				tr.close();
+			} else {
+				// Send message
+				Transport.send(messageMim);
+			}
+		} catch (MessagingException e) {
+			logger.error("Error sending notification on " + smtpServer + " port " + smtpPort, e);
+			throw new TechnicalException(TechnicalErrorCode.MAIL_EXCEPTION, "Error sending notification", e);
+		} catch (Exception e) {
+			logger.error("Error sending notification on " + smtpServer + " port " + smtpPort, e);
+			throw new TechnicalException(TechnicalErrorCode.MAIL_EXCEPTION, "Error sending notification", e);
+		}
+	}
+
+	/**
+	 * Create some properties and get the default Session
+	 */
+	private Session getMailSession() {
+
+		// Set the host smtp address
+		Properties props = new Properties();
+		props.put("mail.smtp.host", smtpServer);
+		props.put("mail.smtp.port", smtpPort + "");
+
+		if (needsAuth) {
+			props.put("mail.smtp.auth", "true");
+		} else {
+			props.put("mail.smtp.auth", "false");
+		}
+		
+		// create some properties and get the default Session
+		Session session = Session.getInstance(props, null);
+		if (logger.isDebugEnabled()) {
+			session.setDebug(true);
+		} else {
+			session.setDebug(false);
+		}
+
+		return session;
+	}
+
+	/**
+	 * Send notification giving a mailContainer object.
+	 */
+	public void sendNotification(String fromUser, String recipient,
+			MailContainer mailContainer) {
+		sendNotification(fromUser, recipient, mailContainer.getSubject(),
+				mailContainer.getContentHTML(), mailContainer.getContentTXT());
+
+	}
+
 }
