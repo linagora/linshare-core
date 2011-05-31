6ed8c52694831c20869ac65fb20b362c50da7e89
Stéphanie DUPREY <sduprey@linagora.com>
2011-05-31 15:42:06 +0000

    Updating migration script

--- update/linshare8_to_linshare9.sh
+++ update/linshare8_to_linshare9.sh
@@ -12,23 +12,23 @@ echo "Please enter your ldap credentials (leave userDN and password empty for an
 read -p  "   userDN (was ldap.auth.userDN): " userDN
 read -p  "   password (was ldap.auth.password): " password
 
-echo "-------------------"
-echo "Superadmin configuration:"
-echo "-------------------"
-echo "Please enter the superadmin password: "
-read -s superadmin_pass
-echo "Please confirm the superadmin password: "
-read -s superadmin_pass_confirm
+#echo "-------------------"
+#echo "Superadmin configuration:"
+#echo "-------------------"
+#echo "Please enter the superadmin password: "
+#read -s superadmin_pass
+#echo "Please confirm the superadmin password: "
+#read -s superadmin_pass_confirm
 
-while [ "$superadmin_pass" != "$superadmin_pass_confirm" ]
-do
-echo ""
-echo "The password does not correspond to the confirmation !"
-echo "Please enter the superadmin password: "
-read -s superadmin_pass
-echo "Please confirm the superadmin password: "
-read -s superadmin_pass_confirm
-done
+#while [ "$superadmin_pass" != "$superadmin_pass_confirm" ]
+#do
+#echo ""
+#echo "The password does not correspond to the confirmation !"
+#echo "Please enter the superadmin password: "
+#read -s superadmin_pass
+#echo "Please confirm the superadmin password: "
+#read -s superadmin_pass_confirm
+#done
 echo "-------------------"
 
 hashpass=`echo -ne "$(echo -n $superadmin_pass | sha1sum | cut -f1 -d" " | sed -e 's/\(.\{2\}\)/\\\x\1/g')" | base64`
@@ -36,11 +36,11 @@ hashpass=`echo -ne "$(echo -n $superadmin_pass | sha1sum | cut -f1 -d" " | sed -
 echo "Please insert this lines into your database after the import.sql script :"
 echo "-------------------"
 
-echo "INSERT INTO linshare_ldap_connection(ldap_connection_id, identifier, provider_url, security_auth, security_principal, security_credentials) VALUES (1, 'baseLDAP', '$ldap_url', 'anonymous', '$userDN', '$password');"
+echo "INSERT INTO linshare_ldap_connection(ldap_connection_id, identifier, provider_url, security_auth, security_principal, security_credentials) VALUES (1, 'baseLDAP', '$ldap_url', 'simple', '$userDN', '$password');"
 
 echo "INSERT INTO linshare_domain_pattern(domain_pattern_id, identifier, description, get_user_command, get_all_domain_users_command, auth_command, search_user_command, get_user_result) VALUES (1, 'basePattern', '', 'ldap.entry(\"uid=\" + userId + \",$ldap_baseDN,\" + domain, \"objectClass=*\");', 'ldap.list(\"$ldap_baseDN,\" + domain, \"(&(objectClass=*)($ldap_auth_attribute=*)(givenName=*)(sn=*))\");', 'ldap.list(\"$ldap_baseDN,\" + domain, \"(&(objectClass=*)(givenName=*)(sn=*)(|($ldap_auth_attribute=\"+login+\")(uid=\"+login+\")))\");', 'ldap.list(\"$ldap_baseDN,\" + domain, \"(&(objectClass=*)($ldap_auth_attribute=\"+mail+\")(givenName=\"+firstName+\")(sn=\"+lastName+\"))\");', '$ldap_auth_attribute givenName sn');"
 
 echo "INSERT INTO linshare_domain(domain_id, identifier, differential_key, domain_pattern_id, ldap_connection_id, parameter_id) VALUES (1, 'baseDomain', '$ldap_base', 1, 1, 1);"
 
-echo "INSERT INTO linshare_user(user_id, user_type_id, login, first_name, last_name, mail, creation_date, role_id, password, expiry_date, can_upload, can_create_guest)   VALUES (4, 0, 'root@localhost.localdomain', '', '', 'root@localhost.localdomain', '2009-01-01', 3, '$hashpass', '2019-01-01', 'false','false');"
+#echo "INSERT INTO linshare_user(user_id, user_type_id, login, first_name, last_name, mail, creation_date, role_id, password, expiry_date, can_upload, can_create_guest)   VALUES (4, 0, 'root@localhost.localdomain', '', '', 'root@localhost.localdomain', '2009-01-01', 3, '$hashpass', '2019-01-01', 'false','false');"
 
