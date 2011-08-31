c5c80b07e7c027a96c8b608bf55b142b191b021f
Sébastien LEVESQUE <slevesque@linagora.com>
2011-08-31 15:12:14 +0000

    #196 fix home page (locale)

---
+++ src/main/java/org/linagora/linShare/view/tapestry/pages/StartingPage.java
@@ -0,0 +1,36 @@
+/*
+ *    This file is part of Linshare.
+ *
+ *   Linshare is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of
+ *   the License, or (at your option) any later version.
+ *
+ *   Linshare is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public
+ *   License along with Foobar.  If not, see
+ *                                    <http://www.gnu.org/licenses/>.
+ *
+ *   (c) 2008 Groupe Linagora - http://linagora.org
+ *
+*/
+package org.linagora.linShare.view.tapestry.pages;
+
+
+
+/**
+ * Start page of application linshare.
+ * this page is accessed from spring security (target URL)
+ * a redirection has to be done to fix the user locale in the page (we need to set the user locale read from database)
+ */
+public class StartingPage {
+
+   Object onActivate() {
+	   return Index.class;
+   } 
+   
+}
