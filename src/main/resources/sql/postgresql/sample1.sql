f1f91fd504a28569b3f631e521019be4ec8b684b
Frédéric MARTIN <fmartin@linagora.com>
2012-08-23 12:29:14 +0000

    update sql sample file

--- src/main/resources/sql/postgresql/sample1.sql
+++ src/main/resources/sql/postgresql/sample1.sql
@@ -25,3 +25,37 @@ INSERT INTO domain_abstract(id, type , identifier, label, enable, template, desc
 INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order) VALUES (4, 3, 'GuestDomain', 'GuestDomain', true, false, 'a simple description', 0, 'en', 0, null, 1, 2, 1, 4);
 
 
+
+
+
+
+
+-- Users
+INSERT INTO account(id, account_type, ls_uid, creation_date, modification_date, role_id, locale, enable, destroyed, domain_id) VALUES (50, 2, '9a9ece25-7a0e-4d75-bb55-d4070e25e1e1', current_date,current_date, 0, 'fr', true, false, 3);
+INSERT INTO users(account_id, First_name, Last_name, Mail, Can_upload, Comment, Restricted, CAN_CREATE_GUEST) VALUES (50, 'Bart', 'Simpson', 'bart.simpson@int1.linshare.dev', true, '', false, true);
+
+
+-- Thread
+INSERT INTO account(id, account_type, ls_uid, creation_date, modification_date, role_id, locale, enable, destroyed, domain_id) VALUES (51, 6, '9806de10-ed0b-11e1-877a-5404a6202d2c', current_date,current_date, 0, 'fr', true, false, 1);
+INSERT INTO thread (account_id, name) VALUES (51, 'cours des comptes');
+
+
+--Thread members
+INSERT INTO thread_member (id, thread_id, admin, can_upload, creation_date, modification_date, user_id) VALUES (1, 51, true, true, current_date,current_date, 50); 
+
+
+-- Tags
+INSERT INTO tag (id, account_id, name, system, visible, not_null, tag_type) VALUES (1, 50, 'Réponse', false, true, null,0);
+INSERT INTO tag (id, account_id, name, system, visible, not_null, tag_type) VALUES (2, 50, 'Demande', false, true, null,0);
+
+INSERT INTO tag (id, account_id, name, system, visible, not_null, tag_type) VALUES (3, 50, 'Projets', false, true, true,1);
+INSERT INTO tag_enum_value (id, tag_id, value) VALUES (1, 3, 'RATP'); 
+INSERT INTO tag_enum_value (id, tag_id, value) VALUES (2, 3, 'Ministère de lintérieur'); 
+
+INSERT INTO tag (id, account_id, name, system, visible, not_null, tag_type) VALUES (4, 50, 'Phases', false, true, true,1);
+INSERT INTO tag_enum_value (id, tag_id, value) VALUES (3, 4, 'Instruction'); 
+INSERT INTO tag_enum_value (id, tag_id, value) VALUES (4, 4, 'Contradiction'); 
+INSERT INTO tag_enum_value (id, tag_id, value) VALUES (5, 4, 'Recommandation'); 
+
+
+
