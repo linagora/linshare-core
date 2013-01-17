8134acec76b84edf4f4d8fe7735af28abe5a0da5
Frédéric MARTIN <fmartin@linagora.com>
2013-01-17 17:11:47 +0000

    convertion of windows files to linux files.

--- src/main/java/org/linagora/linshare/core/facade/impl/WebServiceShareFacadeImpl.java
+++ src/main/java/org/linagora/linshare/core/facade/impl/WebServiceShareFacadeImpl.java
@@ -17,7 +17,7 @@
  *
  *   (c) 2008 Groupe Linagora - http://linagora.org
  *
-*/
+ */
 package org.linagora.linshare.core.facade.impl;
 
 import java.util.ArrayList;
@@ -42,47 +42,41 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-
-
 public class WebServiceShareFacadeImpl implements WebServiceShareFacade {
 
-	
 	private final DocumentEntryService documentEntryService;
-	private final  AccountService accountService;
-	private final  ShareFacade shareFacade;
-	
+	private final AccountService accountService;
+	private final ShareFacade shareFacade;
+
 	private static final Logger logger = LoggerFactory.getLogger(WebServiceShareFacadeImpl.class);
 
 	public WebServiceShareFacadeImpl(final DocumentEntryService documentEntryService, final AccountService accountService, final ShareFacade shareFacade) {
 		this.documentEntryService = documentEntryService;
 		this.accountService = accountService;
-		this.shareFacade=shareFacade;
+		this.shareFacade = shareFacade;
 	}
-	
-	
+
 	@Override
 	public User checkAuthentication() throws BusinessException {
-		
-		User actor = getAuthentication();
-		
-		if (actor== null) {
-			throw new BusinessException(BusinessErrorCode.WEBSERVICE_UNAUTHORIZED, "You are not authorized to use this service");
-		}
-		
-		return actor;
-	}
-	
-	
-	
-	@Override
-	public void sharedocument(String targetMail, String uuid, int securedShare) throws BusinessException{
 
 		User actor = getAuthentication();
-		
-		if ((actor instanceof Guest  && !actor.getCanUpload())) {
+
+		if (actor == null) {
 			throw new BusinessException(BusinessErrorCode.WEBSERVICE_UNAUTHORIZED, "You are not authorized to use this service");
 		}
- 
+
+		return actor;
+	}
+
+	@Override
+	public void sharedocument(String targetMail, String uuid, int securedShare) throws BusinessException {
+
+		User actor = getAuthentication();
+
+		if ((actor instanceof Guest && !actor.getCanUpload())) {
+			throw new BusinessException(BusinessErrorCode.WEBSERVICE_UNAUTHORIZED, "You are not authorized to use this service");
+		}
+
 		// fetch the document
 		DocumentEntry documentEntry;
 		try {
@@ -90,95 +84,95 @@ public class WebServiceShareFacadeImpl implements WebServiceShareFacade {
 		} catch (BusinessException e) {
 			throw e;
 		}
-		
+
 		if (documentEntry == null) {
 			throw new BusinessException(BusinessErrorCode.WEBSERVICE_NOT_FOUND, "Document not found");
 		}
- 
-		DocumentVo docVo = new DocumentVo(documentEntry.getUuid(), documentEntry.getName(), documentEntry.getComment(), documentEntry.getCreationDate(), documentEntry.getExpirationDate(), documentEntry.getType(), documentEntry.getEntryOwner().getLsUuid(), documentEntry.getCiphered(), documentEntry.getShareEntries().size()>0, documentEntry.getSize());
-		
+
+		DocumentVo docVo = new DocumentVo(documentEntry.getUuid(), documentEntry.getName(), documentEntry.getComment(), documentEntry.getCreationDate(), documentEntry.getExpirationDate(),
+				documentEntry.getType(), documentEntry.getEntryOwner().getLsUuid(), documentEntry.getCiphered(), documentEntry.getShareEntries().size() > 0, documentEntry.getSize());
+
 		List<DocumentVo> listDoc = new ArrayList<DocumentVo>();
 		listDoc.add(docVo);
- 
+
 		List<String> listRecipient = new ArrayList<String>();
 		listRecipient.add(targetMail);
- 
+
 		SuccessesAndFailsItems<ShareDocumentVo> successes;
- 
-		
-		//give personal message and subject in WS in the future? null at this time 
+
+		// give personal message and subject in WS in the future? null at this
+		// time
 		String message = null;
 		String subject = null;
-		MailContainer mailContainer = new MailContainer (actor.getExternalMailLocale(),message,subject);
-		
-		UserVo uo =  new UserVo(actor);
-		
+		MailContainer mailContainer = new MailContainer(actor.getExternalMailLocale(), message, subject);
+
+		UserVo uo = new UserVo(actor);
+
 		try {
-			successes = shareFacade.createSharingWithMailUsingRecipientsEmail(uo, listDoc, listRecipient, (securedShare==1) , mailContainer);
-			
+			successes = shareFacade.createSharingWithMailUsingRecipientsEmail(uo, listDoc, listRecipient, (securedShare == 1), mailContainer);
+
 		} catch (BusinessException e) {
 			throw e;
 		}
- 
-		if ( (successes.getSuccessesItem()==null) || ((successes.getFailsItem()!=null) && (successes.getFailsItem().size()>0))) {
+
+		if ((successes.getSuccessesItem() == null) || ((successes.getFailsItem() != null) && (successes.getFailsItem().size() > 0))) {
 			throw new BusinessException(BusinessErrorCode.WEBSERVICE_FAULT, "Could not share the document");
 		}
- 
+
 	}
-	
-	
+
 	@Override
 	public void multiplesharedocuments(String targetMail, List<String> uuid, int securedShare, String messageOpt) throws BusinessException {
-		
+
 		User actor = getAuthentication();
-		
-		List<String>  listRecipient = new ArrayList<String>();
+
+		List<String> listRecipient = new ArrayList<String>();
 		listRecipient.add(targetMail);
-		 
-		 
+
 		List<DocumentVo> listDoc = new ArrayList<DocumentVo>();
-		
+
 		// fetch the document
 		DocumentEntry documentEntry;
-			
+
 		for (String onefileid : uuid) {
 			documentEntry = documentEntryService.findById(actor, onefileid);
-			DocumentVo docVo = new DocumentVo(documentEntry.getUuid(), documentEntry.getName(), documentEntry.getComment(), documentEntry.getCreationDate(), documentEntry.getExpirationDate(), documentEntry.getType(), documentEntry.getEntryOwner().getLsUuid(), documentEntry.getCiphered(), documentEntry.getShareEntries().size()>0, documentEntry.getSize());
+			DocumentVo docVo = new DocumentVo(documentEntry.getUuid(), documentEntry.getName(), documentEntry.getComment(), documentEntry.getCreationDate(), documentEntry.getExpirationDate(),
+					documentEntry.getType(), documentEntry.getEntryOwner().getLsUuid(), documentEntry.getCiphered(), documentEntry.getShareEntries().size() > 0, documentEntry.getSize());
 			listDoc.add(docVo);
 		}
-			
-		
-		//give personal message and subject in WS in the future? null at this time 
-		String message = (messageOpt==null)? "": messageOpt;
+
+		// give personal message and subject in WS in the future? null at this
+		// time
+		String message = (messageOpt == null) ? "" : messageOpt;
 		String subject = null;
-		MailContainer mailContainer = new MailContainer (actor.getExternalMailLocale(),message,subject);
-		
-		UserVo uo =  new UserVo(actor);
-		
+		MailContainer mailContainer = new MailContainer(actor.getExternalMailLocale(), message, subject);
+
+		UserVo uo = new UserVo(actor);
+
 		SuccessesAndFailsItems<ShareDocumentVo> successes;
- 
+
 		try {
-			successes = shareFacade.createSharingWithMailUsingRecipientsEmail(uo, listDoc, listRecipient, (securedShare==1), mailContainer);
+			successes = shareFacade.createSharingWithMailUsingRecipientsEmail(uo, listDoc, listRecipient, (securedShare == 1), mailContainer);
 		} catch (BusinessException e) {
 			throw e;
 		}
-	
-		if ( (successes.getSuccessesItem()==null) || ((successes.getFailsItem()!=null) && (successes.getFailsItem().size()>0))) {
+
+		if ((successes.getSuccessesItem() == null) || ((successes.getFailsItem() != null) && (successes.getFailsItem().size() > 0))) {
 			throw new BusinessException(BusinessErrorCode.WEBSERVICE_FAULT, "Could not share the document");
 		}
- 
+
 	}
-	
-	
-	//#############  utility methods
-	
-	
+
+	// ############# utility methods
+
 	private User getAuthentication() {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication(); 
-	     String name =  (auth != null) ? auth.getName() : null; //get logged in username
-	     if (name == null) return null;
-	     User user = (User) accountService.findByLsUid(name);
-	     return user;
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String name = (auth != null) ? auth.getName() : null; // get logged in
+																// username
+		if (name == null)
+			return null;
+		User user = (User) accountService.findByLsUid(name);
+		return user;
 	}
 
 }
