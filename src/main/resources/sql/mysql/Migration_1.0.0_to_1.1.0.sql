f6c32502a0da9b82e16c154dbd9d806e04550996
Nicolas BERTRAND <nbertrand@linagora.com>
2013-01-28 12:55:06 +0000

    Issue #477 : added Mime type and Signature functionality back + migration scripts from 1.0.0 to 1.1.0

---
+++ src/main/resources/sql/mysql/Migration_1.0.0_to_1.1.0.sql
@@ -0,0 +1,21 @@
+-- MySQL migration script : 1.0.0 to 1.1.0
+
+
+-- Functionality : RESTRICTED_GUEST
+INSERT INTO policy(id, status, default_status, policy, system) VALUES (47, false, false, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system) VALUES (48, false, false, 1, true);
+INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (24, true, 'RESTRICTED_GUEST', 47, 48, 1);
+
+-- Functionality : FORCE_GUEST_RESTRICTION
+INSERT INTO policy(id, status, default_status, policy, system) VALUES (49, false, false, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system) VALUES (50, false, false, 1, true);
+INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (25, true, 'FORCE_GUEST_RESTRICTION', 49, 50, 1);
+
+-- enable mime type filter functionality : bug fixed
+UPDATE policy SET system = false where id=7;
+
+-- enable signature functionality : bug fixed
+UPDATE policy SET system = false where id=9;
+
+-- LinShare version
+INSERT INTO linshare_version (description) VALUES ('1.1.0');
