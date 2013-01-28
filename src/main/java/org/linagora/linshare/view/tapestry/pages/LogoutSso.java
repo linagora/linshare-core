b9ee7eb692bc8e7059fb75e79a1b5652797ce8b4
Frédéric MARTIN <fmartin@linagora.com>
2013-01-28 13:12:27 +0000

    Adding logoutsso page.

---
+++ src/main/java/org/linagora/linshare/view/tapestry/pages/LogoutSso.java
@@ -0,0 +1,34 @@
+/*
+ *    This file is part of Linshare.
+ *
+ *   Linshare is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of
+ *   the License, or (at your option) any later version.
+ *
+ *   Linshare is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public
+ *   License along with Foobar.  If not, see
+ *                                    <http://www.gnu.org/licenses/>.
+ *
+ *   (c) 2008 Groupe Linagora - http://linagora.org
+ *
+ */
+package org.linagora.linshare.view.tapestry.pages;
+
+/**
+ * When you are using the SSO version of linshare, you are redirected to this page after logout.
+ * This page can be used by the SSO to know when the user wants to disconnect from the application. 
+ * @author fred
+ *
+ */
+public class LogoutSso {
+
+	Object onActivate() {
+		return Index.class;
+	}
+}
