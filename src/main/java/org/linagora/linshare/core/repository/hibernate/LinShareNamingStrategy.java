abc59135a7bffc996f735c6983e8b5f0a3fcf2de
Frédéric MARTIN <fmartin@linagora.com>
2013-02-27 15:28:52 +0000

    updating LinShare licenses (java file headers)

--- src/main/java/org/linagora/linshare/core/repository/hibernate/LinShareNamingStrategy.java
+++ src/main/java/org/linagora/linshare/core/repository/hibernate/LinShareNamingStrategy.java
@@ -1,53 +1,35 @@
 /*
- *    This file is part of Linshare.
- *
- *   Linshare is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU Affero General Public License as
- *   published by the Free Software Foundation, either version 3 of
- *   the License, or (at your option) any later version.
- *
- *   Linshare is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Affero General Public License for more details.
- *
- *   You should have received a copy of the GNU Affero General Public
- *   License along with Foobar.  If not, see
- *                                    <http://www.gnu.org/licenses/>.
- *
- *   (c) 2008 Groupe Linagora - http://linagora.org
- *
-*/
-/*
- * This file is a part of Linra.
- * This software is a computer program whose purpose 
- * is to manage the whole life-cycle of electronic certificate.
- * It relies on EJBCA, the open-source PKI, and provides 
- * an easy-to-use interface for end users, administration interface 
- * and advanced certificate management features.
- *
- * ==LICENSE NOTICE==
- * Linra is a free software subjected to the  
- * ** GNU Affero Public License ** as  published by the 
- * Free Software Foundation, ** version 3 ** of the license.
+ * LinShare is an open source filesharing software, part of the LinPKI software
+ * suite, developed by Linagora.
  * 
- * By application to section 7 in the GNU  Affero GPLv3, 
- * dynamic and static links do not extend license to other
- * softwares.
+ * Copyright (C) 2013 LINAGORA
  * 
- * You can redistribute  and/or modify since  you respect 
- * the term of the license. 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display of the “LinShare™” trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source
+ * and free version of LinShare™, powered by Linagora © 2009–2013. Contribute to
+ * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
+ * e-mails sent with the Program, (ii) retain all hypertext links between
+ * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
+ * refrain from infringing Linagora intellectual property rights over its
+ * trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
  * 
- * NOTICE : THIS LICENSE IS FREE OF CHARGE AND THE SOFTWARE
- * IS DISTRIBUTED WITHOUT ANY WARRANTIES OF ANY KIND 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
  * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *   
- * ==LICENSE NOTICE==
- * 
- * (c) 2008 Groupe Linagora - http://linagora.com
- * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to LinShare software.
  */
 package org.linagora.linshare.core.repository.hibernate;
 
