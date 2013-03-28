b86fabb682bb6ae4f2b6996d4508112257b6c522
Frédéric MARTIN <fmartin@linagora.com>
2013-03-28 13:57:23 +0000

    dump config script : output file name is define using database name.

--- src/main/resources/sql/postgresql/dump.domain.config.sh
+++ src/main/resources/sql/postgresql/dump.domain.config.sh
@@ -2,6 +2,7 @@
 set -e
 #set -x
 
+
 if [ "$1" == "-h" ] ; then
 	echo -e "This is script allow you to dump LinShare domain configuration (ldap connection, domain pattern and domain).\n"
 	echo -e "First argument is the database name : 
@@ -16,12 +17,15 @@ else
 	l_database="linshare"
 fi
 
-if [ -f dump.linshare.configuration.sql ] ; then 
-	echo "WARN: dump file already exists : dump.linshare.configuration.sql"
+
+g_dumpfile="${l_database}.configuration.sql"
+
+if [ -f "${g_dumpfile}" ] ; then 
+	echo "WARN: dump file already exists : ${g_dumpfile}"
 	echo "Press enter to continue ? (Ctrl C to stop)"
 	read
 else
-	echo "INFO dumping data to dump.linshare.configuration.sql."
+	echo "INFO dumping data to ${g_dumpfile}."
 fi
 
 
@@ -31,7 +35,7 @@ pg_dump -U linshare -a --insert --column-inserts --attribute-inserts \
 --table ldap_attribute \
 --table user_provider_ldap \
 --table domain_abstract \
-${l_database} -f dump.linshare.configuration.sql 
+${l_database} -f ${g_dumpfile}
 if [ $? -ne 0 ] ; then
 	echo
 	echo "ERROR: dump failed !"
@@ -41,11 +45,11 @@ else
 fi
 
 
-cp dump.linshare.configuration.sql{,.bak}
-sed -i -e '/INSERT INTO domain_pattern (domain_pattern_id, identifier, description, auth_command, search_user_command, system, auto_complete_command) VALUES (1,/ d' dump.linshare.configuration.sql
+cp ${g_dumpfile}{,.bak}
+sed -i -e '/INSERT INTO domain_pattern (domain_pattern_id, identifier, description, auth_command, search_user_command, system, auto_complete_command) VALUES (1,/ d' ${g_dumpfile}
 
-sed -i -e '/INSERT INTO domain_abstract (id, type, identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order) VALUES (1,/ d' dump.linshare.configuration.sql
+sed -i -e '/INSERT INTO domain_abstract (id, type, identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order) VALUES (1,/ d' ${g_dumpfile}
 
-sed -i -e '/INSERT INTO ldap_attribute (id, domain_pattern_id, field, attribute, sync, system, enable) VALUES (.*, 1,/ d' dump.linshare.configuration.sql
+sed -i -e '/INSERT INTO ldap_attribute (id, domain_pattern_id, field, attribute, sync, system, enable) VALUES (.*, 1,/ d' ${g_dumpfile}
 echo "INFO : removing default datas."
 
