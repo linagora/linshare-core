faf3519b8dcbe11e2270b0c01152b07c1afcd37f
nbertrand <nbertrand@linagora.com>
2013-07-24 15:35:26 +0000

    Added webservices support for functionalities' policies (activation / configuration)

--- src/main/java/org/linagora/linshare/core/facade/webservice/admin/impl/FunctionalityFacadeImpl.java
+++ src/main/java/org/linagora/linshare/core/facade/webservice/admin/impl/FunctionalityFacadeImpl.java
@@ -1,5 +1,6 @@
 package org.linagora.linshare.core.facade.webservice.admin.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections.ListUtils;
@@ -7,20 +8,19 @@ import org.apache.commons.collections.Transformer;
 import org.linagora.linshare.core.domain.entities.Functionality;
 import org.linagora.linshare.core.exception.BusinessException;
 import org.linagora.linshare.core.facade.webservice.admin.FunctionalityFacade;
-import org.linagora.linshare.core.facade.webservice.user.impl.GenericFacadeImpl;
+import org.linagora.linshare.core.facade.webservice.admin.impl.AdminGenericFacadeImpl;
 import org.linagora.linshare.core.service.AbstractDomainService;
 import org.linagora.linshare.core.service.AccountService;
 import org.linagora.linshare.core.service.FunctionalityService;
 import org.linagora.linshare.webservice.dto.FunctionalityDto;
 
-public class FunctionalityFacadeImpl extends
-		GenericFacadeImpl implements FunctionalityFacade {
+public class FunctionalityFacadeImpl extends AdminGenericFacadeImpl implements
+		FunctionalityFacade {
 
 	private AbstractDomainService abstractDomainService;
 	private FunctionalityService functionalityService;
 
-	public FunctionalityFacadeImpl(
-			final AccountService accountService,
+	public FunctionalityFacadeImpl(final AccountService accountService,
 			final AbstractDomainService abstractDomainService,
 			final FunctionalityService functionalityService) {
 		super(accountService);
@@ -34,49 +34,29 @@ public class FunctionalityFacadeImpl extends
 		Functionality func = functionalityService
 				.getFunctionalityByIdentifiers(domain, identifier);
 
-		return (FunctionalityDto) toDto().transform(func);
+		return new FunctionalityDto(func);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public List<FunctionalityDto> getAll(String domain)
 			throws BusinessException {
-		List<Functionality> funcs = functionalityService
+		List<Functionality> entities = functionalityService
 				.getAllFunctionalities(domain);
+		List<FunctionalityDto> ret = new ArrayList<FunctionalityDto>();
 
-		return ListUtils.transformedList(funcs, toDto());
+		for (Functionality e : entities) {
+			ret.add(new FunctionalityDto(e));
+		}
+		return ret;
 	}
 
 	@Override
 	public void update(String domain, FunctionalityDto func)
 			throws BusinessException {
-		Functionality entity = (Functionality) toEntity().transform(func);
+		Functionality entity = functionalityService
+				.getFunctionalityByIdentifiers(func.getDomain(),
+						func.getIdentifier());
 
 		functionalityService.update(domain, entity);
-	}
-	
-	/*
-	 * Helpers / Transformers
-	 */
-
-	private Transformer toDto() {
-		return new Transformer() {
-			@Override
-			public Object transform(Object input) {
-				return new FunctionalityDto((Functionality) input);
-			}
-		};
-	}
-
-	private Transformer toEntity() {
-		return new Transformer() {
-			@Override
-			public Object transform(Object input) {
-				FunctionalityDto dto = (FunctionalityDto) input;
-
-				return functionalityService.getFunctionalityByIdentifiers(
-						dto.getDomain(), dto.getIdentifier());
-			}
-		};
 	}
 }
--- src/main/java/org/linagora/linshare/webservice/dto/FunctionalityDto.java
+++ src/main/java/org/linagora/linshare/webservice/dto/FunctionalityDto.java
@@ -5,29 +5,32 @@ import java.util.AbstractMap.SimpleEntry;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.linagora.linshare.core.domain.entities.Functionality;
+import org.linagora.linshare.core.domain.entities.Policy;
 
-@XmlRootElement(name = "FunctionalityDto")
+@XmlRootElement(name = "Functionality")
 public class FunctionalityDto {
 
 	private String identifier;
 	private String domain;
 	private SimpleEntry<Boolean, Integer> activation;
 	private SimpleEntry<Boolean, Integer> configuration;
-
-	public FunctionalityDto(String identifier, String domain,
-			SimpleEntry<Boolean, Integer> activation,
-			SimpleEntry<Boolean, Integer> configuration) {
+	
+	public FunctionalityDto() {
 		super();
-		this.identifier = identifier;
-		this.domain = domain;
-		this.activation = activation;
-		this.configuration = configuration;
 	}
 
 	public FunctionalityDto(Functionality f) {
 		super();
+
+		Policy ap = f.getActivationPolicy();
+		Policy cp = f.getConfigurationPolicy();
+
 		this.identifier = f.getIdentifier();
 		this.domain = f.getDomain().getIdentifier();
+		this.activation = new SimpleEntry<Boolean, Integer>(ap.getStatus(),
+				ap.getPolicy().toInt());
+		this.configuration = new SimpleEntry<Boolean, Integer>(cp.getStatus(),
+				cp.getPolicy().toInt());
 	}
 
 	public String getIdentifier() {
@@ -44,6 +47,22 @@ public class FunctionalityDto {
 
 	public void setDomain(String domain) {
 		this.domain = domain;
+	}
+
+	public SimpleEntry<Boolean, Integer> getActivation() {
+		return activation;
+	}
+
+	public void setActivation(SimpleEntry<Boolean, Integer> activation) {
+		this.activation = activation;
+	}
+
+	public SimpleEntry<Boolean, Integer> getConfiguration() {
+		return configuration;
+	}
+
+	public void setConfiguration(SimpleEntry<Boolean, Integer> configuration) {
+		this.configuration = configuration;
 	}
 
 }
