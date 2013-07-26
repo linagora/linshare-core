62320c23a8c299a240e26807ed7e1ba8b5e53821
Frédéric MARTIN <fmartin@linagora.com>
2013-07-26 13:18:09 +0000

    Added rest api endpoint for cors support

---
+++ src/main/java/org/linagora/linshare/webservice/user/CORSRestService.java
@@ -0,0 +1,15 @@
+package org.linagora.linshare.webservice.user;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import org.linagora.linshare.webservice.dto.SimpleStringValue;
+
+@Path("/rest/cors")
+public interface CORSRestService {
+	
+	Response getOptions();
+	
+	SimpleStringValue isCorsAuthorized();
+	
+}
