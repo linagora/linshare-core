1abc72ba033e08d2c4b0dbe061d8a494939b7326
Benjamin GERMAN <bgerman@linagora.com>
2013-07-31 12:51:15 +0000

    - update ihm for threadContent grid - adminthread * search in view

--- src/main/java/org/linagora/linshare/view/tapestry/pages/administration/thread/AdminThread.java
+++ src/main/java/org/linagora/linshare/view/tapestry/pages/administration/thread/AdminThread.java
@@ -227,7 +227,11 @@ public class AdminThread {
 
 	public Object onSuccessFromFormSearch() throws BusinessException {
 		if (inSearch) {
+			if (recipientsSearchMember.equals("*")) {
+				members = threadEntryFacade.getThreadMembers(currentThread);
+			} else {
 			members = threadEntryFacade.searchAmongMembers(userVo, currentThread, recipientsSearchMember);
+			}
 		}
 		return null;
 	}
