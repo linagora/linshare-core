fa0204828ad0ba84373eeb67021e2ed5a20c1532
Frédéric MARTIN <fmartin@linagora.com>
2013-09-13 09:11:17 +0000

    Adding equals and hascode methods to MailingListVo object.

--- src/main/java/org/linagora/linshare/core/domain/vo/MailingListVo.java
+++ src/main/java/org/linagora/linshare/core/domain/vo/MailingListVo.java
@@ -62,7 +62,7 @@ public class MailingListVo {
 		this.isPublic = list.isPublic();
 		this.owner = new UserVo(list.getOwner());
 		this.domainId = list.getDomain().getIdentifier();
-		mails =new ArrayList<MailingListContactVo>();
+		mails = new ArrayList<MailingListContactVo>();
 		
 		for(MailingListContact current : list.getMails()) {
 			mails.add(new MailingListContactVo(current));
@@ -150,5 +150,32 @@ public class MailingListVo {
 	public void setUuid(String uuid) {
 		this.uuid = uuid;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MailingListVo other = (MailingListVo) obj;
+		if (uuid == null) {
+			if (other.uuid != null)
+				return false;
+		} else if (!uuid.equals(other.uuid))
+			return false;
+		return true;
+	}
+	
+	
 
 }
