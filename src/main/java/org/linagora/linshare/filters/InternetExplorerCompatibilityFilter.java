5a8ab8cc5578a327f447e830b05729eccf0113ed
Frédéric MARTIN <fmartin@linagora.com>
2013-11-20 17:40:55 +0000

    Adding InternetExplorerCompatibilityFilter for IE 8 and 9 uploading files API.

---
+++ src/main/java/org/linagora/linshare/filters/InternetExplorerCompatibilityFilter.java
@@ -0,0 +1,77 @@
+/*
+ * LinShare is an open source filesharing software, part of the LinPKI software
+ * suite, developed by Linagora.
+ * 
+ * Copyright (C) 2013 LINAGORA
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display of the “LinShare™” trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source
+ * and free version of LinShare™, powered by Linagora © 2009–2013. Contribute to
+ * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
+ * e-mails sent with the Program, (ii) retain all hypertext links between
+ * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
+ * refrain from infringing Linagora intellectual property rights over its
+ * trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to LinShare software.
+ */
+package org.linagora.linshare.filters;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * This filter is designed to fix Internet explorer 8 an d 9 compatibility with
+ * application/json content type for fineuploader upload URL .
+ * 
+ * @author fmartin
+ * 
+ */
+public class InternetExplorerCompatibilityFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		String method = request.getMethod();
+		if (method.equals("POST")) {
+
+			if (request.getHeader("User-Agent") != null) {
+				String userAgent = request.getHeader("User-Agent");
+				logger.debug("userAgent : " + userAgent);
+
+				Pattern pattern = Pattern.compile(".*MSIE [8,9]\\.0.*");
+				Matcher matcher = pattern.matcher(userAgent);
+				if (matcher.matches()) {
+					response.setHeader("Content-Type", "text/html");
+				}
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+
+}
