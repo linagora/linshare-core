feed5c094e2f7f908efcf061518579adcdcbcdd3
Frédéric MARTIN <fmartin@linagora.com>
2013-11-20 17:02:29 +0000

    fix licence headers of interceptors.

--- src/main/java/org/linagora/linshare/webservice/interceptor/TimingInInterceptor.java
+++ src/main/java/org/linagora/linshare/webservice/interceptor/TimingInInterceptor.java
@@ -1,3 +1,36 @@
+/*
+ * LinShare is an open source filesharing software, part of the LinPKI software
+ * suite, developed by Linagora.
+ * 
+ * Copyright (C) 2013 LINAGORA
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display of the “LinShare™” trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source
+ * and free version of LinShare™, powered by Linagora © 2009–2013. Contribute to
+ * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
+ * e-mails sent with the Program, (ii) retain all hypertext links between
+ * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
+ * refrain from infringing Linagora intellectual property rights over its
+ * trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to LinShare software.
+ */
 package org.linagora.linshare.webservice.interceptor;
 
 import org.apache.cxf.interceptor.Fault;
@@ -9,8 +42,7 @@ import org.slf4j.LoggerFactory;
 
 public class TimingInInterceptor extends AbstractPhaseInterceptor<Message> {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(TimingInInterceptor.class);
+	private static final Logger logger = LoggerFactory.getLogger(TimingInInterceptor.class);
 
 	public TimingInInterceptor() {
 		super(Phase.RECEIVE);
@@ -24,9 +56,7 @@ public class TimingInInterceptor extends AbstractPhaseInterceptor<Message> {
 		if (logger.isDebugEnabled()) {
 			logger.debug(String.format("%s:%s", method, url + " ..."));
 		}
-		message.getExchange().put(
-				"org.linagora.linshare.webservice.interceptor.start_time",
-				startTime);
+		message.getExchange().put("org.linagora.linshare.webservice.interceptor.start_time", startTime);
 		message.getExchange().put(Message.REQUEST_URI, url);
 		message.getExchange().put(Message.HTTP_REQUEST_METHOD, method);
 	}
