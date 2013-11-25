1d792e7aed551b3ab1f6d1e033ca565aaba15fd0
Frédéric MARTIN <fmartin@linagora.com>
2013-11-25 11:03:07 +0000

    Fix tests.

--- src/test/java/org/linagora/linshare/repository/hibernate/AbstractDomainRepositoryImplTest.java
+++ src/test/java/org/linagora/linshare/repository/hibernate/AbstractDomainRepositoryImplTest.java
@@ -36,9 +36,8 @@ package org.linagora.linshare.repository.hibernate;
 import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.Assert;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.linagora.linshare.core.domain.entities.AbstractDomain;
@@ -209,11 +208,6 @@ public class AbstractDomainRepositoryImplTest extends AbstractJUnit4SpringContex
 		domainPatternRepository.create(domainPattern);
 		logger.debug("Current pattern object: " + domainPattern.toString());
 
-		
-		DomainPolicy domainPolicy = new DomainPolicy(identifierP, "blabla",null);
-		domainPatternRepository.create(domainPattern);
-		logger.debug("Current pattern object: " + domainPattern.toString());
-		
 		LdapUserProvider provider = new LdapUserProvider("",ldapconnexion,domainPattern);
 		userProviderRepository.create(provider);
 		
