df3c2ea065162ce14af8f70f9bd65a044b57b6f2
Frédéric MARTIN <fmartin@linagora.com>
2014-01-17 17:31:51 +0100

    mysql scripts were uptdated for 1.5.0 support.

--- src/main/resources/sql/mysql/Migration_1.4.0_to_1.5.0.sql
+++ src/main/resources/sql/mysql/Migration_1.4.0_to_1.5.0.sql
@@ -47,10 +47,68 @@ END$$
 DELIMITER ;
 
 
-DELETE FROM mail_subjects WHERE subject_id=13 ;
+DELETE FROM mail_subjects WHERE subject_id=13;
 call copy_domain_abstract2mail_subjects();
+DROP PROCEDURE IF EXISTS copy_domain_abstract2mail_subjects;
 
 
+ALTER TABLE `domain_pattern` DROP `description` ;
+ALTER TABLE `domain_pattern`  ADD `description` TEXT AFTER `identifier` NOT NULL;
+
+ALTER TABLE ldap_attribute ADD COLUMN completion bool DEFAULT true NOT NULL;
+UPDATE ldap_attribute SET completion=false  WHERE field = 'user_uid';
+
+ALTER TABLE domain_pattern ADD COLUMN auto_complete_command_on_first_and_last_name text DEFAULT 'To be define' NOT NULL;
+ALTER TABLE domain_pattern ADD COLUMN auto_complete_command_on_all_attributes text DEFAULT 'To be define' NOT NULL;
+
+ALTER TABLE domain_pattern DROP COLUMN auto_complete_command;
+
+ALTER TABLE domain_pattern ADD COLUMN search_page_size int4 DEFAULT 100 NOT NULL;
+ALTER TABLE domain_pattern ADD COLUMN search_size_limit int4 DEFAULT 100 NOT NULL;
+ALTER TABLE domain_pattern ADD COLUMN completion_page_size int4 DEFAULT 10 NOT NULL;
+ALTER TABLE domain_pattern ADD COLUMN completion_size_limit int4 DEFAULT 10 NOT NULL;
+
+DELETE FROM ldap_attribute WHERE domain_pattern_id=1;
+DELETE FROM domain_pattern WHERE domain_pattern_id=1;
+
+
+-- system domain pattern
+INSERT INTO domain_pattern(
+ domain_pattern_id,
+ identifier,
+ description,
+ auth_command,
+ search_user_command,
+ system,
+ auto_complete_command_on_first_and_last_name,
+ auto_complete_command_on_all_attributes,
+ search_page_size,
+ search_size_limit,
+ completion_page_size,
+ completion_size_limit)
+VALUES (
+ 1,
+ 'default-pattern-obm',
+ 'This is pattern the default pattern for the ldap obm structure.',
+ 'ldap.search(domain, "(&(objectClass=obmUser)(mail=*)(givenName=*)(sn=*)(|(mail="+login+")(uid="+login+")))");',
+ 'ldap.search(domain, "(&(objectClass=obmUser)(mail="+mail+")(givenName="+first_name+")(sn="+last_name+"))");',
+ true,
+ 'ldap.search(domain, "(&(objectClass=obmUser)(mail=*)(givenName=*)(sn=*)(|(&(sn=" + first_name + ")(givenName=" + last_name + "))(&(sn=" + last_name + ")(givenName=" + first_name + "))))");',
+ 'ldap.search(domain, "(&(objectClass=obmUser)(mail=*)(givenName=*)(sn=*)(|(mail=" + pattern + ")(sn=" + pattern + ")(givenName=" + pattern + ")))");',
+ 100,
+ 100,
+ 10,
+ 10
+ );
+
+
+INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pattern_id, completion) VALUES (1, 'user_mail', 'mail', false, true, true, 1, true);
+INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pattern_id, completion) VALUES (2, 'user_firstname', 'givenName', false, true, true, 1, true);
+INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pattern_id, completion) VALUES (3, 'user_lastname', 'sn', false, true, true, 1, true);
+INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pattern_id, completion) VALUES (4, 'user_uid', 'uid', false, true, true, 1, false);
+
+
+-- LinShare version
 INSERT INTO version (description) VALUES ('1.5.0');
 
 COMMIT;
