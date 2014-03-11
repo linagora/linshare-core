a41ec190914bd0781dfbbddc751bedbd750a3584
Frédéric MARTIN <fmartin@linagora.com>
2014-03-11 11:04:31 +0100

    licence check and compliance commit

--- src/main/java/org/linagora/linshare/core/utils/AESCrypt.java
+++ src/main/java/org/linagora/linshare/core/utils/AESCrypt.java
@@ -1,36 +1,20 @@
 /*
- * LinShare is an open source filesharing software, part of the LinPKI software
- * suite, developed by Linagora.
- * 
- * Copyright (C) 2013 LINAGORA
- * 
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version, provided you comply with the Additional Terms applicable for
- * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
- * Public License, subsections (b), (c), and (e), pursuant to which you must
- * notably (i) retain the display of the “LinShare™” trademark/logo at the top
- * of the interface window, the display of the “You are using the Open Source
- * and free version of LinShare™, powered by Linagora © 2009–2013. Contribute to
- * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
- * e-mails sent with the Program, (ii) retain all hypertext links between
- * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
- * refrain from infringing Linagora intellectual property rights over its
- * trademarks and commercial brands. Other Additional Terms apply, see
- * <http://www.linagora.com/licenses/> for more details.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Affero General Public License and
- * its applicable Additional Terms for LinShare along with this program. If not,
- * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
- * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
- * applicable to LinShare software.
+ * Copyright 2008 Vocali Sistemas Inteligentes
+ * Copyright 2009 Groupe Linagora for modifications marked "LINAGORA".
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.linagora.linshare.core.utils;
 
 import java.io.BufferedInputStream;
