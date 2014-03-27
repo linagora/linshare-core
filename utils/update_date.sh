7d5b3bca38b20f35604fee21ec915f96d051688b
Nicolas BERTRAND <nbertrand@linagora.com>
2014-03-27 16:17:40 +0100

    Updated update_date script

--- utils/update_date.sh
+++ utils/update_date.sh
@@ -15,7 +15,13 @@ if [ ! -d "$path" ]; then
     usage
 fi
 
+yearfrom='2013'
+yearto='2014'
+
 grep -l "LinShare" --include "*.java" --include "*.properties" --include "*.tml" -R ${path} 2>/dev/null | xargs -i \
-    sed -i 's/2013 LINAGORA/2014 LINAGORA/g' {}
+    sed -i 's/2009-'${yearfrom}' LINAGORA/'${yearto}' LINAGORA/g' {}
 grep -l "LinShare" --include "*.java" --include "*.properties" --include "*.tml" -R ${path} 2>/dev/null | xargs -i \
-    sed -i 's/2009-2013/2009-2014/g' {}
+    sed -i 's/2009–'${yearfrom}'/2009–'${yearto}'/g' {}
+# some legacy license use - instead of –
+grep -l "LinShare" --include "*.java" --include "*.properties" --include "*.tml" -R ${path} 2>/dev/null | xargs -i \
+    sed -i 's/2009-'${yearfrom}'/2009–'${yearto}'/g' {}
