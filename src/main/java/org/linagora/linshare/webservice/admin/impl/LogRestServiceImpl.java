f77568326d6eb5075cf3ba01a6073658f1b0cdf0
Camille Tjhoa <ctjhoa@linagora.com>
2014-04-17 16:57:22 +0200

    remove unused import

--- src/main/java/org/linagora/linshare/webservice/admin/impl/LogRestServiceImpl.java
+++ src/main/java/org/linagora/linshare/webservice/admin/impl/LogRestServiceImpl.java
@@ -37,7 +37,6 @@ package org.linagora.linshare.webservice.admin.impl;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
