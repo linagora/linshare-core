8f6586c6a8fd5b72546995249292956c9449236d
Nicolas BERTRAND <nbertrand@linagora.com>
2014-05-14 16:58:42 +0200

    Fixed wrong merge artefact

--- src/main/java/org/linagora/linshare/core/service/impl/MailingListServiceImpl.java
+++ src/main/java/org/linagora/linshare/core/service/impl/MailingListServiceImpl.java
@@ -42,7 +42,6 @@ import org.linagora.linshare.core.domain.constants.Role;
 import org.linagora.linshare.core.domain.constants.VisibilityType;
 import org.linagora.linshare.core.domain.entities.MailingList;
 import org.linagora.linshare.core.domain.entities.MailingListContact;
-import org.linagora.linshare.core.domain.entities.Role;
 import org.linagora.linshare.core.domain.entities.User;
 import org.linagora.linshare.core.exception.BusinessErrorCode;
 import org.linagora.linshare.core.exception.BusinessException;
