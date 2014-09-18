413860d3fc7e67a19835e620189919ec2912bc48
Frédéric MARTIN <fmartin@linagora.com>
2014-09-18 16:05:09 +0200

    Fix mysql sample1 (mime policies, mail config)

--- src/main/resources/sql/mysql/sample1.sql
+++ src/main/resources/sql/mysql/sample1.sql
@@ -52,6 +52,8 @@ INSERT INTO domain_abstract(id, type , identifier, label, enable, template, desc
 -- Guest domain (example domain)
 INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order) VALUES (4, 3, 'GuestDomain', 'GuestDomain', true, false, 'a simple description', 0, 'en', 0, null, 1, 2, 1, 4);
 
+UPDATE domain_abstract SET mailconfig_id = 1;
+UPDATE domain_abstract SET mime_policy_id=1;
 
 
 
