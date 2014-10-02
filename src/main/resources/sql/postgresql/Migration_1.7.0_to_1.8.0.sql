14b854bc7cb9d72d18348b14112b0100c37171dc
Frédéric MARTIN <fmartin@linagora.com>
2014-10-02 11:17:31 +0200

    Add migration script 1.7 to 1.8 for postgresql

---
+++ src/main/resources/sql/postgresql/Migration_1.7.0_to_1.8.0.sql
@@ -0,0 +1,22 @@
+-- MySQL migration script : 1.7.0 to 1.8.0
+
+SET storage_engine=INNODB;
+SET NAMES UTF8 COLLATE utf8_general_ci;
+SET CHARACTER SET UTF8;
+
+SET AUTOCOMMIT=0;
+START TRANSACTION;
+
+ALTER TABLE document
+	ADD COLUMN sha1sum varchar(255),
+	ADD COLUMN sha256sum varchar(255);
+
+ALTER TABLE entry
+	ADD COLUMN meta_data text;
+
+-- LinShare version
+INSERT INTO version (id, version) VALUES ((SELECT nextVal('hibernate_sequence')),'1.8.0');
+
+
+COMMIT;
+SET AUTOCOMMIT=1;
