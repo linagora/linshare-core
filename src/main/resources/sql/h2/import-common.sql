c4ae640bda743a2046750e632562550b47756da3
Frédéric MARTIN <fmartin@linagora.com>
2015-01-21 16:00:42 +0100

    Refactoring import.sql : splitted in multiple files.

--- src/main/resources/sql/h2/import-common.sql
+++ src/main/resources/sql/h2/import-common.sql
@@ -24,8 +24,6 @@ INSERT INTO domain_abstract(id, type , identifier, label, enable, template, desc
 -- Default mime policy
 INSERT INTO mime_policy(id, domain_id, uuid, name, mode, displayable, creation_date, modification_date) VALUES(1, 1, '3d6d8800-e0f7-11e3-8ec0-080027c0eef0', 'Default Mime Policy', 0, 0, now(), now());
 
-INSERT INTO ldap_connection(ldap_connection_id, identifier, provider_url, security_auth, security_principal, security_credentials) VALUES (1, 'baseLDAP', 'ldap://localhost:33389', 'simple', '', '');
-
 -- system domain pattern
 INSERT INTO domain_pattern( domain_pattern_id, identifier, description, auth_command, search_user_command, system, auto_complete_command_on_first_and_last_name, auto_complete_command_on_all_attributes, search_page_size, search_size_limit, completion_page_size,  completion_size_limit) VALUES ( 1, 'default-pattern-obm', 'This is pattern the default pattern for the ldap obm structure.', 'ldap.search(domain, "(&(objectClass=*)(mail=*)(givenName=*)(sn=*)(|(mail="+login+")(uid="+login+")))");', 'ldap.search(domain, "(&(objectClass=*)(mail="+mail+")(givenName="+first_name+")(sn="+last_name+"))");',  true, 'ldap.search(domain, "(&(objectClass=*)(mail=*)(givenName=*)(sn=*)(|(&(sn=" + first_name + ")(givenName=" + last_name + "))(&(sn=" + last_name + ")(givenName=" + first_name + "))))");',  'ldap.search(domain, "(&(objectClass=*)(mail=*)(givenName=*)(sn=*)(|(mail=" + pattern + ")(sn=" + pattern + ")(givenName=" + pattern + ")))");', 0, 100, 0, 10 );
 
@@ -35,40 +33,15 @@ INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pa
 INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pattern_id, completion) VALUES (4, 'user_uid', 'uid', false, true, true, 1, false);
 
 
--- user domain pattern
-INSERT INTO domain_pattern( domain_pattern_id, identifier, description, auth_command, search_user_command, system, auto_complete_command_on_first_and_last_name, auto_complete_command_on_all_attributes, search_page_size, search_size_limit, completion_page_size,  completion_size_limit) VALUES ( 2, 'basePattern', 'pattern', 'ldap.search(domain, "(&(objectClass=*)(mail=*)(givenName=*)(sn=*)(|(mail="+login+")(uid="+login+")))");', 'ldap.search(domain, "(&(objectClass=*)(mail="+mail+")(givenName="+first_name+")(sn="+last_name+"))");',  false, 'ldap.search(domain, "(&(objectClass=*)(mail=*)(givenName=*)(sn=*)(|(&(sn=" + first_name + ")(givenName=" + last_name + "))(&(sn=" + last_name + ")(givenName=" + first_name + "))))");',  'ldap.search(domain, "(&(objectClass=*)(mail=*)(givenName=*)(sn=*)(|(mail=" + pattern + ")(sn=" + pattern + ")(givenName=" + pattern + ")))");', 0, 100, 0, 10 );
-
-INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pattern_id, completion) VALUES (5, 'user_mail', 'mail', false, true, true, 2, true);
-INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pattern_id, completion) VALUES (6, 'user_firstname', 'givenName', false, true, true, 2, true);
-INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pattern_id, completion) VALUES (7, 'user_lastname', 'sn', false, true, true, 2, true);
-INSERT INTO ldap_attribute(id, field, attribute, sync, system, enable, domain_pattern_id, completion) VALUES (8, 'user_uid', 'uid', false, true, true, 2, false);
-
-
-
-
-INSERT INTO user_provider_ldap(id, differential_key, domain_pattern_id, ldap_connection_id) VALUES (1, 'dc=linpki,dc=org', 1, 1);
--- Top domain (example domain)
-INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order, mailconfig_id) VALUES (2, 1, 'MyDomain', 'MyDomain', true, false, 'a simple description', 0, 'en', 0, null, 1, 1, 1, 2, null);
--- Sub domain (example domain)
-INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order, mailconfig_id) VALUES (3, 2, 'MySubDomain', 'MySubDomain', true, false, 'a simple description', 0, 'en', 0, 1, 1, 2, 1 , 3, null);
--- Guest domain (example domain)
-INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order, mailconfig_id) VALUES (4, 3, 'GuestDomain', 'GuestDomain', true, false, 'a simple description', 0, 'en', 0, null, 1, 2, 1, 4, null);
-
-UPDATE domain_abstract SET mime_policy_id=1 WHERE id < 100000;
-
-
-
 -- login is e-mail address 'root@localhost.localdomain' and password is 'adminlinshare'
 INSERT INTO account(id, account_type, ls_uuid, creation_date, modification_date, role_id, locale, external_mail_locale, enable, password, destroyed, domain_id) VALUES (1, 6, 'root@localhost.localdomain', current_date(), current_date(), 3, 'en', 'en', true, 'JYRd2THzjEqTGYq3gjzUh2UBso8=', false, 1);
 INSERT INTO users(account_id, First_name, Last_name, Mail, Can_upload, Comment, Restricted, CAN_CREATE_GUEST) VALUES (1, 'Administrator', 'LinShare', 'root@localhost.localdomain', false, '', false, false);
 
 -- system account :
-
 INSERT INTO account(id, account_type, ls_uuid, creation_date, modification_date, role_id, locale, external_mail_locale, enable, destroyed, domain_id) VALUES (2, 7, 'system', current_date(), current_date(), 3, 'en', 'en', true, false, 1);
 
 -- system account for upload-request:
 INSERT INTO account(id, account_type, ls_uuid, creation_date, modification_date, role_id, locale, external_mail_locale, enable, destroyed, domain_id) VALUES (3, 7, 'system-account-uploadrequest', current_date(), current_date(), 3, 'en', 'en', true, false, 1);
-
 
 
 
@@ -370,185 +343,6 @@ INSERT INTO functionality_boolean(functionality_id, boolean_value) VALUES (43, t
 INSERT INTO policy(id, status, default_status, policy, system) VALUES (101, false, false, 2, false);
 INSERT INTO policy(id, status, default_status, policy, system) VALUES (102, true, true, 1, true);
 INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES(44, false, 'UPLOAD_PROPOSITION', 101, 102, 1);
-
-
-
-
--- TESTS
-
--- default domain policy
-INSERT INTO domain_access_policy(id) VALUES (100001);
-INSERT INTO domain_access_rule(id, domain_access_rule_type, domain_id, domain_access_policy_id, rule_index) VALUES (100001, 0, null, 100001,0);
-INSERT INTO domain_policy(id, identifier, domain_access_policy_id) VALUES (100001, 'TestAccessPolicy0-test', 100001);
-
-
--- Root domain (application domain)
-INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id) VALUES (100001, 0, 'TEST_Domain-0', 'TEST_Domain-0', true, false, 'The root test application domain', 3, 'en', 0, null, 100001, null, 1);
--- id : 100001
-
--- topDomainName
--- id : 100002
-INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order) VALUES (100002, 1, 'TEST_Domain-0-1', 'TEST_Domain-0-1 (Topdomain)', true, false, 'a simple description', 0, 'en', 0, null, 100001, 100001, 1, 2);
-
--- topDomainName2
--- id : 100003
-INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order) VALUES (100003, 1, 'TEST_Domain-0-2', 'TEST_Domain-0-2 (Topdomain)', true, false, 'a simple description', 0, 'en', 0, null, 100001, 100001, 1, 3);
-
--- subDomainName1
--- id : 100004
-INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order) VALUES (100004, 2, 'TEST_Domain-0-1-1', 'TEST_Domain-0-1-1 (Subdomain)', true, false, 'a simple description', 0, 'en', 0, null, 100001, 100002, 1 , 4);
-
--- subDomainName2
--- id : 100005
-INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order) VALUES (100005, 2, 'TEST_Domain-0-1-2', 'TEST_Domain-0-1-2 (Subdomain)', true, false, 'a simple description', 0, 'en', 0, null, 100001, 100002, 1 , 5);
-
-
--- Guest domain (example domain)
--- id : 100006
-INSERT INTO domain_abstract(id, type , identifier, label, enable, template, description, default_role, default_locale, used_space, user_provider_id, domain_policy_id, parent_id, messages_configuration_id, auth_show_order) VALUES (100006, 3, 'guestDomainName1', 'guestDomainName1 (GuestDomain)', true, false, 'a simple description', 0, 'en', 0, null, 100001, 100002, 1 , 6);
-
--- Default mime policy
-INSERT INTO mime_policy(id, domain_id, uuid, name, mode, displayable, creation_date, modification_date) VALUES(100001, 100001, 'ec51317c-086c-442a-a4bf-1afdf8774079', 'Default Mime Policy de test', 0, 0, now(), now());
-UPDATE domain_abstract SET mime_policy_id=1 WHERE id >= 100001;
-
-
-
-INSERT INTO account(id, account_type, ls_uuid, creation_date, modification_date, role_id, locale, external_mail_locale, enable, password, destroyed, domain_id) VALUES (100001, 6, 'root@localhost.localdomain@test', current_date(), current_date(), 3, 'en', 'en', true, 'JYRd2THzjEqTGYq3gjzUh2UBso8=', false, 100001);
-INSERT INTO users(account_id, First_name, Last_name, Mail, Can_upload, Comment, Restricted, CAN_CREATE_GUEST) VALUES (100001, 'Administrator', 'LinShare', 'root@localhost.localdomain@test', false, '', false, false);
-
-
-
-
--- root domain de test
--- Functionality : TEST_TIME_STAMPING
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110013, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110014, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (110007, false, 'TEST_TIME_STAMPING', 110013, 110014, 100001);
-INSERT INTO functionality_string(functionality_id, string_value) VALUES (110007, 'http://server/service');
-
--- Functionality : TEST_FILESIZE_MAX
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110001, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110002, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (110001, false, 'TEST_FILESIZE_MAX', 110001, 110002, 100001);
--- Size : MEGA
-INSERT INTO unit(id, unit_type, unit_value) VALUES (110001, 1, 1);
--- Value : 200
-INSERT INTO functionality_unit(functionality_id, integer_value, unit_id) VALUES (110001, 200, 110001);
-
-
--- Functionality : TEST_QUOTA_GLOBAL
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110003, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110004, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (110002, true, 'TEST_QUOTA_GLOBAL', 110003, 110004, 100001);
--- Size : GIGA
-INSERT INTO unit(id, unit_type, unit_value) VALUES (110002, 1, 2);
--- Value : 1
-INSERT INTO functionality_unit(functionality_id, integer_value, unit_id) VALUES (110002, 1, 110002);
-
-
--- Functionality : TEST_QUOTA_USER
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110005, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110006, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (110003, false, 'TEST_QUOTA_USER', 110005, 110006, 100001);
--- Size : GIGA
-INSERT INTO unit(id, unit_type, unit_value) VALUES (110003, 1, 1);
--- Value : 500
-INSERT INTO functionality_unit(functionality_id, integer_value, unit_id) VALUES (110003, 500, 110003);
-
-
--- Functionality : GUESTS
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110027, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110028, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (110014, true, 'GUESTS', 110027, 110028, 100001);
-
-
--- Functionality : TEST_FUNC1
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110059, true, true, 2, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110060, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (110029, false, 'TEST_FUNC1', 110059, 110060, 100001);
-INSERT INTO functionality_string(functionality_id, string_value) VALUES (110029, 'blabla');
-
--- Functionality : TEST_FUNC2
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110061, true, true, 2, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110062, true, true, 2, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES(110030, false, 'TEST_FUNC2', 110061, 110062, 100001); 
-INSERT INTO functionality_string(functionality_id, string_value) VALUES (110030, 'blabla');
-
-
--- Functionality : TEST_FUNC3
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110049, true, true, 0, true);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110050, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (110025, false, 'TEST_FUNC3', 110049, 110050, 100001);
-INSERT INTO functionality_string(functionality_id, string_value) VALUES (110025, 'blabla');
-
-
--- Functionality : TEST_FUNC4
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110017, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110018, true, true, 1, true);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (110009, false, 'TEST_FUNC4', 110017, 110018, 100001);
-INSERT INTO functionality_string(functionality_id, string_value) VALUES (110009, 'blabla');
-
-
--- Functionality : TEST_FUNC5
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110025, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (110026, false, false, 1, true);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (110013, true, 'TEST_FUNC5', 110025, 110026, 100001);
-INSERT INTO functionality_string(functionality_id, string_value) VALUES (110013, 'blabla');
-
-
-
-
-
-
-
-
-
-
--- topDomainName 1
--- Functionality : TEST_FILESIZE_MAX
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (111001, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (111002, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (111001, false, 'TEST_FILESIZE_MAX', 111001, 111002, 100002);
--- Size : MEGA
-INSERT INTO unit(id, unit_type, unit_value) VALUES (111001, 1, 1);
--- Value : 200
-INSERT INTO functionality_unit(functionality_id, integer_value, unit_id) VALUES (111001, 100, 111001);
-
-
--- Functionality : TEST_QUOTA_USER
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (111005, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (111006, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (111003, false, 'TEST_QUOTA_USER', 111005, 111006, 100002);
--- Size : GIGA
-INSERT INTO unit(id, unit_type, unit_value) VALUES (111003, 1, 1);
--- Value : 500
-INSERT INTO functionality_unit(functionality_id, integer_value, unit_id) VALUES (111003, 250, 111003);
-
-
-
-
-
-
--- subDomainName 1
--- Functionality : TEST_FILESIZE_MAX
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (112001, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (112002, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (112001, false, 'TEST_FILESIZE_MAX', 112001, 112002, 100004);
--- Size : MEGA
-INSERT INTO unit(id, unit_type, unit_value) VALUES (112001, 1, 1);
--- Value : 200
-INSERT INTO functionality_unit(functionality_id, integer_value, unit_id) VALUES (112001, 50, 112001);
-
--- subDomainName 2
--- Functionality : TEST_QUOTA_USER
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (113005, true, true, 1, false);
-INSERT INTO policy(id, status, default_status, policy, system) VALUES (113006, true, true, 1, false);
-INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, domain_id) VALUES (113003, false, 'TEST_QUOTA_USER', 113005, 113006, 100005);
--- Size : GIGA
-INSERT INTO unit(id, unit_type, unit_value) VALUES (113003, 1, 1);
--- Value : 500
-INSERT INTO functionality_unit(functionality_id, integer_value, unit_id) VALUES (113003, 125, 113003);
-
 
 
 
