2c835c38cd1e85d8dd5ec36ecef85573340ec97e
Frédéric MARTIN <fmartin@linagora.com>
2015-01-28 19:03:00 +0100

    Fix previous commit (NPE in all upload methods )

--- src/main/java/org/linagora/linshare/webservice/user/impl/FineUploaderServiceImpl.java
+++ src/main/java/org/linagora/linshare/webservice/user/impl/FineUploaderServiceImpl.java
@@ -114,7 +114,7 @@ public class FineUploaderServiceImpl extends WebserviceBase implements
 			logger.error("Can not encode file name " + e1.getMessage());
 		}
 		try {
-			DocumentDto doc = documentFacade.create(file, fileName, "");
+			DocumentDto doc = documentFacade.create(file, "", fileName);
 			return new FineUploaderDto(true, doc.getUuid());
 		} catch (BusinessException e) {
 			return new FineUploaderDto(e);
