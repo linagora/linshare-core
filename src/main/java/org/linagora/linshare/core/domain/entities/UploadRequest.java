d0c9b2e9a1854a31c32109c47f9edf9951fcf7a6
Frédéric MARTIN <fmartin@linagora.com>
2016-04-13 11:20:23 +0200

    Fix #1153 Multiple mail send when creating an upload request

--- src/main/java/org/linagora/linshare/core/domain/entities/UploadRequest.java
+++ src/main/java/org/linagora/linshare/core/domain/entities/UploadRequest.java
@@ -292,6 +292,9 @@ public class UploadRequest implements Cloneable {
 			cnse.printStackTrace(System.err);
 		}
 		req.id = 0;
+		uploadRequestURLs = Sets.newHashSet();
+		uploadRequestEntries = Sets.newHashSet();
+		uploadRequestHistory = Sets.newHashSet();
 		return req;
 	}
 
