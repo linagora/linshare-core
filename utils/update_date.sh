c01ef08ab6a3f0f217b8c4b94d51f17ac5b24436
Alioune KONE <akone@linagora.com>
2016-05-25 14:58:05 +0200

    Fix mysql import file.

--- utils/update_date.sh
+++ utils/update_date.sh
@@ -18,12 +18,18 @@ fi
 yearfrom='2014'
 yearto='2015'
 
-grep -l "LinShare" --include "*.java" --include "*.properties" --include "*.tml" -R ${path} 2>/dev/null | xargs -i \
-    sed -i 's/2009-'${yearfrom}' LINAGORA/'${yearto}' LINAGORA/g' {}
-grep -l "LinShare" --include "*.java" --include "*.properties" --include "*.tml" -R ${path} 2>/dev/null | xargs -i \
-    sed -i 's/2009–'${yearfrom}'/2009–'${yearto}'/g' {}
-grep -l "LinShare" --include "*.java" --include "*.properties" --include "*.tml" -R ${path} 2>/dev/null | xargs -i \
-    sed -i 's/(C) '${yearfrom}' LINAGORA/(C) '${yearto}' LINAGORA/g' {}
 # some legacy license use - instead of –
 grep -l "LinShare" --include "*.java" --include "*.properties" --include "*.tml" -R ${path} 2>/dev/null | xargs -i \
-    sed -i 's/2009-'${yearfrom}'/2009–'${yearto}'/g' {}
+    sed -i -r 's/\(C\) (20[0-9]{2})–(20[0-9]{2}) LINAGORA/(C) \1-\2 LINAGORA/g'
+
+# Match date like 2015
+grep -l "LinShare" --include "*.java" --include "*.properties" --include "*.tml" -R ${path} 2>/dev/null | xargs -i \
+    sed -i -r 's/\(C\) (20[0-9]{2}) LINAGORA/(C) \1-'${yearto}' LINAGORA/g'
+
+# Match date like 2010-2015
+grep -l "LinShare" --include "*.java" --include "*.properties" --include "*.tml" -R ${path} 2>/dev/null | xargs -i \
+    sed -i -r 's/\(C\) (20[0-9]{2}-)(20[0-9]{2}) LINAGORA/(C) \1'${yearto}' LINAGORA/g'
+
+
+# get original date for a file :
+# git rev-list --reverse  HEAD AuthRole.java |head -n1 | xargs git  show -q --format="%ci" | grep -Eo "20[0-9]{2}(-[0-1][1-9]){2}" | grep -Eo "20[0-9]{2}"
