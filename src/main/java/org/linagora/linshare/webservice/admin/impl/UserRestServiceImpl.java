f15e92687a7695628a1353473d93f9180acabff2
Alpha SALL <asall@linagora.com>
2016-07-06 15:17:41 +0200

    Fix method lessThan3Char when the arg is null

--- src/main/java/org/linagora/linshare/webservice/admin/impl/UserRestServiceImpl.java
+++ src/main/java/org/linagora/linshare/webservice/admin/impl/UserRestServiceImpl.java
@@ -2,7 +2,7 @@
  * LinShare is an open source filesharing software, part of the LinPKI software
  * suite, developed by Linagora.
  * 
- * Copyright (C) 2015 LINAGORA
+ * Copyright (C) 2015-2016 LINAGORA
  * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License as published by the Free
@@ -12,7 +12,7 @@
  * Public License, subsections (b), (c), and (e), pursuant to which you must
  * notably (i) retain the display of the “LinShare™” trademark/logo at the top
  * of the interface window, the display of the “You are using the Open Source
- * and free version of LinShare™, powered by Linagora © 2009–2015. Contribute to
+ * and free version of LinShare™, powered by Linagora © 2009–2016. Contribute to
  * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
  * e-mails sent with the Program, (ii) retain all hypertext links between
  * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
@@ -54,6 +54,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.lang.StringUtils;
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
 import org.linagora.linshare.core.exception.BusinessErrorCode;
@@ -256,7 +257,7 @@ public class UserRestServiceImpl extends WebserviceBase implements
 	}
 
 	private boolean lessThan3Char(String s) {
-		return s.length() < 3;
+		return StringUtils.trimToEmpty(s).length() < 3;
 	}
 
 	@Path("/mail_migration")
