c111f11d19448365414faa472467395d12640580
Frédéric MARTIN <fmartin@linagora.com>
2017-02-28 15:11:00 +0100

    Mysql database backend is no longer supported.

--- src/main/resources/sql/mysql/Migration_1.1.0_to_1.2.0.sql
+++
@@ -1,9 +0,0 @@
--- MySQL migration script : 1.1.0 to 1.2.0
-
-ALTER TABLE allowed_mimetype ADD constraint allowed_mimetype_unique_mimetype UNIQUE(mimetype);
-DELETE FROM allowed_mimetype ;
-
-ALTER TABLE document ADD COLUMN check_mime_type bool  DEFAULT false NOT NULL;
-
--- LinShare version
-INSERT INTO version (description) VALUES ('1.2.0');
