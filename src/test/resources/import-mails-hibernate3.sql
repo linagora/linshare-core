65bfe8d4cd6711645af623317f16449ffa39ff1c
Frédéric MARTIN <fmartin@linagora.com>
2017-09-04 11:18:57 +0200

    update layout for update settting

--- src/test/resources/import-mails-hibernate3.sql
+++ src/test/resources/import-mails-hibernate3.sql
@@ -156,6 +156,19 @@ INSERT INTO mail_layout (id, domain_abstract_id, description, visible, layout, c
 <!--/* End of common base layout template*/-->
  </body>
  </html>
+<!--/* Common lower info title style */-->
+<div style="margin-bottom:17px;" data-th-fragment="infoEditedItem(titleInfo,oldValue,newValue)">
+     <span style="font-weight:bold;" ><th:block th:replace="${titleInfo}" /> </span>
+    <br/>
+      <th:block th:replace="${oldValue}" />  -> <th:block th:replace="${newValue}" />
+</div>
+<!--/* Edited  date  display settings  style */-->
+<div style="margin-bottom:17px;" data-th-fragment="infoEditedDateArea(titleInfo,oldValue,newValue)">
+     <span style="font-weight:bold;" data-th-text="${titleInfo}" >Shared the </span>
+    <br/>
+ <th:block  th:with="df=#{date.format}" data-th-text="${#dates.format(oldValue,df)}"/> -> 
+ <th:block  th:with="df=#{date.format}" data-th-text="${#dates.format(newValue,df)}"/>
+</div>
 <!--/* Common header template */-->
 <head  data-th-fragment="header">
   <title data-th-text="${mailSubject}">Mail subject</title>
@@ -203,19 +216,6 @@ border-top: 1px solid #e1e1e1;">
      <span style="font-weight:bold;" ><th:block th:replace="${titleInfo}" /> </span>
     <br/>
       <th:block th:replace="${contentInfo}" />
-</div>
-<!--/* Common lower info title style */-->
-<div style="margin-bottom:17px;" data-th-fragment="infoEditedItem(titleInfo,oldValue,newValue)">
-     <span style="font-weight:bold;" ><th:block th:replace="${titleInfo}" /> </span>
-    <br/>
-      <th:block th:replace="${oldValue}" /> fred <th:block th:replace="${newValue}" />
-</div>
-<!--/* Edited  date  display settings  style */-->
-<div style="margin-bottom:17px;" data-th-fragment="infoEditedDateArea(titleInfo,oldValue,newValue)">
-     <span style="font-weight:bold;" data-th-text="${titleInfo}" >Shared the </span>
-    <br/>
- <th:block  th:with="df=#{date.format}" data-th-text="${#dates.format(oldValue,df)}"/> fred
- <th:block  th:with="df=#{date.format}" data-th-text="${#dates.format(newValue,df)}"/>
 </div>
 <!--/* Common button action style */-->
 <span   data-th-fragment="actionButtonLink(labelBtn,urlLink)">
