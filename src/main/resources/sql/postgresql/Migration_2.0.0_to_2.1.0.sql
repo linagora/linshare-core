f5ac4d542e80df64d260555bf1c815cba9b47eaf
Yazid SEBAHI <ysebahi@linagora.com>
2017-12-15 17:36:42 +0100

    Add mail_activation to the script

--- src/main/resources/sql/postgresql/Migration_2.0.0_to_2.1.0.sql
+++ src/main/resources/sql/postgresql/Migration_2.0.0_to_2.1.0.sql
@@ -178,6 +178,11 @@ SELECT ls_fix_current_value_for_subdomains(1);
 -- ROOTDOMAIN(0)
 SELECT ls_fix_current_value_for_subdomains(0);
 
+TRUNCATE mail_content CASCADE;
+TRUNCATE mail_footer CASCADE;
+DELETE FROM mail_config;
+DELETE FROM mail_layout;
+
 ALTER TABLE document ALTER COLUMN sha256sum SET NOT NULL;
 ALTER TABLE document ALTER COLUMN thmb_uuid DROP NOT NULL;
 ALTER TABLE document ADD COLUMN has_thumbnail bool DEFAULT 'false' NOT NULL;
@@ -357,6 +362,162 @@ VALUES
   null);
 
 -- End Upgrade Task 2.1.0
+
+-- MailActivation : BEGIN
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (231, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (232, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (233, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(17, false, 'UPLOAD_REQUEST_ACTIVATED_FOR_RECIPIENT', 231, 232, 233, 1, true);
+
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (234, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (235, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (236, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(18, false, 'UPLOAD_REQUEST_ACTIVATED_FOR_OWNER', 234, 235, 236, 1, true);
+
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (237, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (238, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (239, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(19, false, 'UPLOAD_REQUEST_REMINDER', 237, 238, 239, 1, true);
+
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (240, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (241, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (242, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(20, false, 'UPLOAD_REQUEST_PASSWORD_RENEWAL', 240, 241, 242, 1, true);
+
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (243, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (244, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (245, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(21, false, 'UPLOAD_REQUEST_CREATED', 243, 244, 245, 1, true);
+
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (246, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (247, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (248, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(22, false, 'UPLOAD_REQUEST_CLOSED_BY_OWNER', 246, 247, 248, 1, true);
+
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (249, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (250, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (251, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(23, false, 'UPLOAD_REQUEST_RECIPIENT_REMOVED', 249, 250, 251, 1, true);
+
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (252, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (253, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (254, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(24, false, 'UPLOAD_REQUEST_UPDATED_SETTINGS', 252, 253, 254, 1, true);
+
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (255, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (256, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (257, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(25, false, 'UPLOAD_REQUEST_FILE_DELETED_BY_OWNER', 255, 256, 257, 1, true);
+
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (258, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (259, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (260, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(26, false, 'GUEST_WARN_OWNER_ABOUT_GUEST_EXPIRATION', 258, 259, 260, 1, true);
+
+-- MailActivation : SHARE_WARN_SENDER_ABOUT_SHARE_EXPIRATION_WITHOUT_DOWNLOAD
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (261, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (262, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (263, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)
+ 	VALUES(27, false, 'SHARE_WARN_SENDER_ABOUT_SHARE_EXPIRATION_WITHOUT_DOWNLOAD', 261, 262, 263, 1, true);
+
+-- MailActivation : SHARE_WARN_RECIPIENT_ABOUT_EXPIRED_SHARE
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (264, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (265, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (266, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable) 
+ 	VALUES(28, false, 'SHARE_WARN_RECIPIENT_ABOUT_EXPIRED_SHARE', 264, 265, 266, 1, true);
+
+-- MailActivation : WORKGROUP_WARN_NEW_MEMBER
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (267, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (268, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (269, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable)  
+ 	VALUES(29, false, 'WORKGROUP_WARN_NEW_MEMBER', 267, 268, 269, 1, true);
+
+-- MailActivation : WORKGROUP_WARN_UPDATED_MEMBER
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (270, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (271, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (272, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable) 
+ 	VALUES(30, false, 'WORKGROUP_WARN_UPDATED_MEMBER', 270, 271, 272, 1, true);
+
+-- MailActivation : WORKGROUP_WARN_DELETED_MEMBER
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (273, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (274, true, true, 1, false);
+	INSERT INTO policy(id, status, default_status, policy, system)
+VALUES (275, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable) 
+ 	VALUES(31, false, 'WORKGROUP_WARN_DELETED_MEMBER', 273, 274, 275, 1, true);
+
+-- MailActivation : GUEST_WARN_GUEST_ABOUT_HIS_PASSWORD_RESET
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (276, true, true, 0, true);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (277, true, true, 1, false);
+INSERT INTO policy(id, status, default_status, policy, system)
+	VALUES (278, false, false, 2, true);
+INSERT INTO mail_activation(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, enable) 
+ 	VALUES(32, false, 'GUEST_WARN_GUEST_ABOUT_HIS_PASSWORD_RESET', 276, 277, 278, 1, true);
+-- MailActivation : END
+
+-- Mail Layout 
+--######   TODO
+-- End mail layout
 
 -- End of your requests
 
