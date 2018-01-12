c0ebc991fbf2249e61b149386f1bc8b2d89efb74
Mehdi Attia <mattia@linagora.com>
2018-01-12 16:44:17 +0100

    FIX welcome message langage by removing nl and mq

--- src/main/resources/sql/common/import-welcome-messages.sql
+++ src/main/resources/sql/common/import-welcome-messages.sql
@@ -7,10 +7,6 @@ INSERT INTO welcome_messages_entry(id, lang, value, welcome_messages_id)
 INSERT INTO welcome_messages_entry(id, lang, value, welcome_messages_id)
 	VALUES (2, 'fr', '<h2>Bienvenue dans LinShare</h2><p>Bienvenue dans LinShare, le logiciel libre de partage de fichiers sécurisé.</p>', 1);
 INSERT INTO welcome_messages_entry(id, lang, value, welcome_messages_id)
-	VALUES (3, 'mq', '<h2>Bienvini an lè Linshare</h2>,<p>an solusyon lib de partaj de fichié sékirisé.</p>', 1);
-INSERT INTO welcome_messages_entry(id, lang, value, welcome_messages_id)
-	VALUES (4, 'vi', '<h2>Chào mừng bạn đến với Linshare</h2><p>Chào mừng bạn đến với Linshare, phần mềm nguồn mở chia sẻ file bảo mật.</p>', 1);
-INSERT INTO welcome_messages_entry(id, lang, value, welcome_messages_id)
-	VALUES (5, 'nl', '<h2>Welkom bij LinShare</h2>,<p>het Open Source-systeem om grote bestanden te delen.</p>', 1);
+	VALUES (3, 'vi', '<h2>Chào mừng bạn đến với Linshare</h2><p>Chào mừng bạn đến với Linshare, phần mềm nguồn mở chia sẻ file bảo mật.</p>', 1);
 -- Default setting welcome messages for all domains
 UPDATE domain_abstract SET welcome_messages_id = 1;
