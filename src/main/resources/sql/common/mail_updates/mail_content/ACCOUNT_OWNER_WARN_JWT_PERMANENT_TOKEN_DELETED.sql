984deb6db54ff43bfd8b21c94377142018ba7e55
Alvin LAMARTINIERE <alamartiniere@linagora.com>
2018-11-23 18:03:54 +0100

    Issue #334 : Add Single mail content for JWT permanent Token
    Python script result allowing to version the mail contents

---
+++ src/main/resources/sql/common/mail_updates/mail_content/ACCOUNT_OWNER_WARN_JWT_PERMANENT_TOKEN_DELETED.sql
@@ -0,0 +1,50 @@
+UPDATE mail_content SET subject='[(#{subject})]',body='<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+   <head data-th-replace="layout :: header"></head>
+   <body>
+      <div
+         th:replace="layout :: email_base(upperMainContentArea = ~{::#main-content},bottomSecondaryContentArea = ~{::#secondary-content})">
+         <!--/*  Upper main-content */-->
+         <section id="main-content">
+            <!--/* main-content container */-->
+            <div th:replace="layout :: contentUpperSection( ~{::#section-content})">
+               <div id="section-content">
+                  <!--/* Greetings for external or internal user */-->
+                  <div>
+                     <th:block data-th-replace="layout :: greetings(${recipient.firstName})"/>
+                  </div>
+                  <!--/* End of Greetings for external or internal recipient */-->
+                  <!--/* Main email  message content*/-->
+                  <p>
+                     <span data-th-utext="#{mainMsg(${owner.firstName},${owner.lastName})}">
+                 Peter WILSON has deleted a permanent authentication token for your account
+                     </span>
+                  </p>
+                  <!--/* End of Main email message content*/-->
+               </div>
+               <!--/* End of section-content*/-->
+            </div>
+            <!--/* End of main-content container */-->
+         </section>
+         <!--/* End of upper main-content*/-->
+         <!--/* Secondary content for  bottom email section */-->
+         <section id="secondary-content">
+           <th:block data-th-replace="layout :: infoStandardArea(#{tokenLabel},${label})"/>
+           <th:block data-th-replace="layout :: infoDateArea(#{tokenCreationDate},${creationDate})"/>
+           <div data-th-if="${!#strings.isEmpty(description)}">
+             <th:block data-th-replace="layout :: infoStandardArea(#{tokenDescription},${description})"/>
+           </div>
+         </section>
+         <!--/* End of Secondary content for bottom email section */-->
+      </div>
+   </body>
+</html>',messages_french='subject = Suppression d''''un jeton d''''accès permanent
+mainMsg =  <b> {0} <span style="text-transform:uppercase">{1}</span></b> a supprimé un jeton d''''accès permanent pour votre compte.
+tokenCreationDate = Date de création
+tokenLabel = Nom
+tokenDescription = Description
+tokenIdentifier = Identifiant',messages_english='subject = Deletion of a permanent authentication token
+mainMsg =  <b> {0} <span style="text-transform:uppercase">{1}</span></b> has deleted a permanent authentication token for your account.
+tokenCreationDate = Creation date
+tokenLabel = Name
+tokenDescription = Description' WHERE id=33;
