4943231c8268ca5c613e12fc3122dec1dd77a3ad
Fayçal BORDJAH <fbordjah@linagora.com>
2019-04-25 09:51:54 +0200

    Add updated russian mails to migration script

--- src/main/resources/sql/postgresql/Migration_2.2.0_to_2.3.0.sql
+++ src/main/resources/sql/postgresql/Migration_2.2.0_to_2.3.0.sql
@@ -865,27 +865,424 @@ UPDATE mail_content SET body='<!DOCTYPE html>
 </html>' WHERE id = 29;
 --end mail content
 
--- Add russian mail content
+-- Add russian mail message to mail tables
 ALTER TABLE mail_content ADD COLUMN messages_russian text;
---To be deleted once we have russian translation
-UPDATE mail_content SET messages_russian = messages_english;
-ALTER TABLE mail_content ALTER COLUMN messages_russian SET NOT NULL;
+ALTER TABLE mail_footer ADD COLUMN messages_russian text;
+ALTER TABLE mail_layout ADD COLUMN messages_russian text;
+--End
+
+-- Update the all mail_content tables by adding the russian messages
+UPDATE mail_content
+SET messages_russian='beginningMainMsgInt = Ваш файл
+endingMainMsgInt = будет автоматически удален через <b> {0} дней</b> из вашего личного пространства.
+subject = Файл {0} будет удален
+uploadedThe = Дата загрузки'
+WHERE id = 1;
+
+UPDATE mail_content
+SET messages_russian='downloadBtn = Загрузить
+downloadLink = Загрузить по ссылке
+helpMsgPlural = , чтобы получить доступ ко всем документам рассылки.
+helpMsgSingular = , чтобы получить доступ ко всем документам рассылки.
+helpPasswordMsgSingular = Перейдите по ссылке ниже, чтобы загрузить файлы и ввести пароль.
+helpPasswordMsgPlural = Перейдите по ссылке ниже, чтобы загрузить файлы и ввести пароль.
+mainMsgPlural = <b> {0} <span style="text-transform:uppercase">{1}</span> </b> поделился с вами файлами <b>{2} файлов</b>.
+mainMsgSingular = <b> {0} <span style="text-transform:uppercase">{1}</span> </b> поделился с вами  <b>{2} файлами</b>.
+msgFrom = Вы получили сообщение от
+name = {0} {1}
+password = Пароль
+subjectCustomAlt =by {0} {1}
+subjectPlural = {0} {1} поделился с вами файлами
+subjectSingular = {0} {1} поделился с вами файлами
+click = Перейдите по
+link = ссылке'
+WHERE id = 2;
+
+UPDATE mail_content
+SET messages_russian='numFilesMsgPlural = Вы поделились <b>{0} files</b>
+numFilesMsgSingular = Вы поделились <b>{0} file</b>
+recipientCountMsgPlural =   с <b>{1} recipients</b>. Срок действия рассылки закончится: {0}.
+recipientCountMsgSingular =   с <b>{1} recipient</b>. Срок действия рассылки закончится: {0}.
+subjectPlural =  Вы поделились некоторыми файлами
+subjectSingular =Вы поделились файлом
+msgFor = Ваше сообщение рассылки'
+WHERE id = 3;
+
+UPDATE mail_content
+SET messages_russian='downloadDate = Дата загрузки
+fileNameEndOfLine = {0}.
+mainMsgExt = Внешний пользователь <b>{0}</b> скачал(а) ваш файл
+mainMsgInt = <b> {0} <span style="text-transform:uppercase">{1}</span> </b> скачал(а) ваш файл
+subject = {0} {1} был скачан {2}
+subjectAnonymous = {0} был скачан {1}'
+WHERE id = 4;
+
+UPDATE mail_content
+SET messages_russian='deletedDate = Дата удаления
+mainMsg = <b>{0} <span style="text-transform:uppercase">{1}</span></b> удалил файл рассылки
+subject = {0} {1} удалил файл рассылки {2}'
+WHERE id = 5;
+
+UPDATE mail_content
+SET messages_russian='beginningMainMsgInt = Срок действия файла рассылки
+endingMainMsgInt = отправленного <b> {0} <span style="text-transform:uppercase">{1}</span></b>,  закончится через <b>{2} дней</b>.
+mainMsgExt = Срок действия файла рассылки <b>{0}</b> sent by <b> {1} <span style="text-transform:uppercase">{2}</span></b>,  закончится через <b>{3} дней</b>.
+name = {0} {1}
+sharedBy = Отправлено
+subject = Срок действия файла рассылки {0} заканчивается'
+WHERE id = 6;
+
+UPDATE mail_content
+SET messages_russian='downloadStatesTile = Статус загрузки
+mainMsgplural = Некоторые получатели рассылки не скачали <b>{0} файлов</b>. Вы можете найти детали о получателях рассылки ниже.
+mainMsgSingular = Некоторые получатели рассылки не скачали <b>{0} файлов</b>. Вы можете найти детали о получателях рассылки ниже.
+subjectPlural = Уведомдение о не скачанных файлах: {0} файлов были не скачанны.
+subjectSingular = Уведомдение о не скачанных файлах: {0} файлов были не скачанны.'
+WHERE id = 7;
+
+UPDATE mail_content
+SET messages_russian='accessToLinshareBTn = Активировать аккаунт
+accountExpiryDateTitle = Срок действия аккаунта
+activationLinkTitle = Ссылка активации
+mainMsg = <b> {0} <span style="text-transform:uppercase">{1}</span></b> создал гостевой аккаунт <b>{2}</b>  для вас, который позволяет надежно обмениваться файлами. <br/>Для входа в ваш аккаунт, завершите процесс регистрации, используя ссылку
+subject = {0}  {1} пригласил вас активировать ваш {2} аккаунт
+userNameTitle = Имя пользователя'
+WHERE id = 8;
+
+UPDATE mail_content
+SET messages_russian='accountExpiryDateTitle = Дата окончания действия аккаунта
+beginingMainMsg =  Используйте ссылку ниже для смены пароля к вашему аккаунту LinShare.
+changePasswordBtn = Изменить пароль
+endingMainMsg = Если вы не запрашивали смену пароля, пожалуйста, проигнорируйте это письмо. Ваш пароль не будет изменен пока вы не создадите новый, перейдя по ссылке.
+mainTile = Забыли пароль?
+resetLinkTitle = Ссылка на смену пароля LinShare
+subject =  Инструкция по смену пароля LinShare
+userNameTitle = Имя пользователя'
+WHERE id = 9;
+
+UPDATE mail_content
+SET messages_russian='endingMainMsg = в вашем запросе загрузки
+fileSize = Размер файла
+fileUploadedThe = Дата загрузки
+invitationClosureDate = Дата закрытия
+invitationCreationDate = Дата активации
+beginningMainMsg =  <b> {0} </b> загрузил файл
+endingMainMsg = в ваш запрос загрузки.
+numFilesInDepot = Всего загруженных файлов
+subject =  {0}  загрузил {1}  в ваш запрос загрузки
+uploadedOverTotal = {0} / {1} файлы
+totalUploaded = {0} файлы'
+WHERE id = 10;
+
+UPDATE mail_content
+SET messages_russian='invitationClosureDate = Дата закрытия
+invitationCreationDate = Дата активации
+mainMsg =  <b>{0}</b> не может загрузить файлы, так как в вашем личном пространстве недостаточно места. Пожалуйста, удалите некоторые файлы, чтобы освободить место.
+mainMsgTitle = Недостаточно свободного места.
+maxUploadDepotSize = Максимальный размер загрузки
+msgTitle = Сообщение загрузки:
+recipientsURequest = Получатели
+subject =  {0} не может загрузить файл, так как недостаточно свободного места'
+WHERE id = 11;
+
+UPDATE mail_content
+SET messages_russian='beginningMainMsgForRecipient = <b> {0} <span style="text-transform:uppercase">{1}</span> </b>''''s Действие запроса на загрузку закончится через <b>{2} дней</b>
+beginningMainMsgGrouped = Действие вашего приглашения закончится через <b>{0} дней</b>.
+beginningMainMsgUnGrouped =  Действие вашего приглашения закончится через <b>{0} дней</b>.
+endingMainMsgPlural =  вы получили <b>{0} файлов</b>.
+endingMainMsgPlural = Всего загрузка содержит <b> {0} файлов </b>.
+endingMainMsgPluralForRecipient = вы отправили <b> {0} файлов </b> в загрузку.
+endingMainMsgSingular =   вы получили <b>1 файл</b>.
+endingMainMsgSingular = Всего в репозитории of <b>1 файл </b>.
+endingMainMsgSingularForRecipient = вы отправили <b>1 файл </b> в репозиторий.
+filesInURDepot = Загруженные файлы
+formatMailSubject = : {0}
+invitationClosureDate = Дата закрытия
+invitationCreationDate = Дата активации
+msgTitle =  Сообщение запроса загрузки:
+recipientsURequest = Получатели
+subjectForOwner =  Срок действия вашего приглашения заканчивается.
+subjectForRecipient =  {0} {1}''''s срок действия вашего приглашения заканчивается.
+uploadFileBtn = Загрузить файл'
+WHERE id = 12;
+
+UPDATE mail_content
+SET messages_russian='beginningMainMsgForRecipient = <b> {0} <span style="text-transform:uppercase">{1}</span> </b>''''s Срок действия загрузки закончился
+beginningMainMsgGrouped = Срок действия вашего группового запроса загрузки закончился
+beginningMainMsgUnGrouped = Срок действия загрузки закончился.
+endingMainMsgPlural = Вы получили <b>{0} файлов</b>.
+endingMainMsgPluralForRecipient = вы отправили всего <b> {0} файлов </b>.
+endingMainMsgSingular = всего вы получили <b>1 файлов</b>.
+endingMainMsgSingularForRecipient = вы загрузили в репозиторий <b>1 файл </b> .
+filesInURDepot = Загружено файлов
+formatMailSubject = : {0}
+invitationClosureDate = Дата закрытия
+invitationCreationDate = Дата активации
+msgTitle = Сообщение загрузки
+recipientsURequest = Получатели
+subjectForOwner = Ваше приглашение  {0} больше не действительно
+subjectForRecipient =  {0} {1}''''s  приглешение {2} больше не действительно'
+WHERE id = 13;
+
+UPDATE mail_content
+SET messages_russian='endingMainMsgPlural = Всего в хранилище <b> {0} файлов </b>.
+endingMainMsgSingular =  Всего в хранилище <b>1 файл </b.
+filesInURDepot = Файлы загружены
+fileSize =  Общий размер файла
+groupedBeginningMainMsg = <b>{0}</b> закрыл ваше групповое хранилище для файлов запроса загрузки.
+invitationClosureDate = Дата закрытия
+invitationCreationDate = Дата активации
+msgTitle =  Запрос загрузки содержит сообщение:
+numFilesInDepot = Всего загруженных файлов
+recipientsURequest = Получатели
+subject =  {0} закрыл ваше хранилище для файлов запроса загрузки {1}
+ungroupedBeginningMainMsg  = <b>{0}</b> закрыл ваше хранилище для файлов запроса загрузки.
+uploadedOverTotal = {0} / {1} файлов
+totalUploaded = {0} файлов'
+WHERE id = 14;
+
+UPDATE mail_content
+SET messages_russian='invitationClosureDate = Дата закрытия
+invitationCreationDate = Дата активации
+mainMsg = <b>{0}</b> удалил файл <b> {1} </b> из вашего запроса загрузки.
+msgTitle = Загрузка содержит сообщение:
+subject = {0} удалил файл из загрузки {1}'
+WHERE id = 15;
+
+UPDATE mail_content
+SET messages_russian='buttonMsg = Доступ
+closureDate = Дата закрытия
+depotSize = Допустимый размер
+mainMsg = <b>{0} {1}</b> пригласил вас в свой запрос загрузки <b>{2}</b>.
+msgFrom = Сообщение от
+msgAlt = Репозиторий {0} {1} активен.
+msgProtected = Разблокируйте его, перейдя по ссылке ниже и введя пароль.
+msgUnProtected = Получите доступ, перейдя по ссылке ниже.
+name = {0} {1}
+password = Пароль
+recipientsOfDepot = Получатель
+subject = {0} {1}  пригласил вас в свой запрос загрузки {2}'
+WHERE id = 16;
+
+UPDATE mail_content
+SET messages_russian='buttonMsg = Доступ
+closureDate = Дата закрытия
+depotSize = Допустимый размер
+mainMsg = Ваш запрос загрузки <b>{0}</b> активен.
+msgLink = Получите доступ к нему, перейдя по ссылке ниже.
+recipientsOfDepot = Получатель
+subject = Ваш запрос загрузки {0} активен'
+WHERE id = 17;
+
+UPDATE mail_content
+SET messages_russian='buttonMsg = Доступ
+closureDate = Дата закрытия
+depotSize = Размер
+mainMsg = <b>{0} {1}</b> напоминает вам о загрузке ваших файлов.
+mainMsgEnd =
+msgFrom = Сообщение от
+msgUnProtected = Для того, чтобы загрузить ваши файлы, пожалуйста, перейдите по ссылке ниже.
+name = {0} {1}
+recipientsOfDepot = Получатели
+subject = {0} {1} ожидает ваши файлы'
+WHERE id = 18;
+
+UPDATE mail_content
+SET messages_russian='buttonMsg = Доступ к загрузке
+closureDate = Дата закрытия загрузки
+mainMsg = <b>{0} {1}</b> изменил пароль к загрузке {2}
+msgProtected = Новый пароль и доступ к загрузке доступны ниже.
+password = Пароль
+subject = {0} {1} отправил вам новый пароль к загрузке {2}'
+WHERE id = 19;
+
+UPDATE mail_content
+SET messages_russian='activationDate = Дата активации
+closureDate = Дата закрытия
+customDate= MMMM d, yyyy.
+depotSize = Допустимый размер
+mainMsg = <b>{0} {1}</b> открыл для вас доступ к его запросу загрузки, созданному
+msgFrom = Сообщение от
+name = {0} {1}
+recipientsOfDepot = Получатели
+subject = {0} {1} открыл для вас доступ к его запросу загрузки.'
+WHERE id = 20;
+
+UPDATE mail_content
+SET messages_russian='closureDate = Дата закрытия
+filesInURDepot = Файлы
+mainMsg = <b>{0} {1}</b> закрыл запрос загрузки {2}.
+recipientsOfDepot = Получатели
+subject = {0} {1} закрыл запрос загрузки {2}'
+WHERE id = 21;
+
+UPDATE mail_content
+SET messages_russian='deletionDate = Дата удаления
+mainMsg =  <b> {0} <span style="text-transform:uppercase">{1}</span> </b> закрыл ваш доступ к загрузке {2}.
+subject = {0} {1} закрыл ваш доступ к загрузке {2}'
+WHERE id = 22;
+
+UPDATE mail_content
+SET messages_russian='activationDate = Дата активации
+closureRight = Права закрытия
+deletionRight = Права удаления
+depotSize = Размер репозитория
+expiryDate = Дата закрытия
+local = Локальный
+enableNotification = Разрешить уведомления
+mainMsg =   <b> {0} <span style="text-transform:uppercase">{1}</span> </b>  обновил некоторые настройки запроса загрузки.
+maxFileNum = Номер файла
+maxFileSize = Размер файла
+msgFrom =  Новое сообщение от
+name = {0} {1}
+nameOfDepot: Название загрузки
+secondaryMsg = Список обновленных настроек доступен ниже.
+subject = Обновленные настройки для запроса загрузки {0}'
+WHERE id = 23;
+
+UPDATE mail_content
+SET messages_russian='closureDate = Срок действия загрузки
+deletionDate = Дата удаления
+mainMsg =  <b> {0} <span style="text-transform:uppercase">{1}</span> </b> удалил файл <b>{2} </b> из хранилища {3}.
+subject = {0} {1} удалил файл {2} из хранилища'
+WHERE id = 24;
+
+UPDATE mail_content
+SET messages_russian='accessToLinshareBTn = Истечение срока действия аккаунта
+accountCreationDateTitle = Дата создания аккаунта
+accountExpiryDateTitle = Дата истечения срока действия аккаунта
+activationLinkTitle = Ссылка активации
+mainMsg = Срок действия гостевого аккаунта <b> {0} <span style="text-transform:uppercase">{1}</span></b> заканчивается через {2} дня. Если вам все еще нужен аккаунт, продлите срок его действия.
+subject = {0}  {1} срок действия гостевого аккакунта скоро закончится.
+userEmailTitle = Электронная почта'
+WHERE id = 25;
+
+UPDATE mail_content
+SET messages_russian='accessToLinshareBTn = Срок действия вашей рассылки скоро закончится
+shareRecipientTitle = Получатель
+shareFileTitle = Файл рассылки
+shareCreationDateTitle = Дата создания
+shareExpiryDateTitle = Дата истечения срока действия
+activationLinkTitle = Ссылка активации
+beginningMainMsg = Срок действия файла рассылки
+endingMainMsg =  закончится через {0} дней, а файла не были скачаны получателем <b> {1} <span style="text-transform:uppercase">{2}</span></b>.
+subject = Срок действия рассылки скоро закончится, а файлы не были скачаны
+name = {0} {1}
+fileNameEndOfLine = {0}'
+WHERE id = 26;
+
+UPDATE mail_content
+SET messages_russian='shareFileTitle = Файл рассылки
+shareCreationDateTitle = Дата создания
+shareExpiryDateTitle = Дата срока истечения действия
+activationLinkTitle = Ссылка активации
+beginningMainMsg =  У файла рассылки
+endingMainMsg = отправленного <b> {0} <span style="text-transform:uppercase">{1}</span></b> истек срок действия и он был удален <b>system</b>.
+subject = Срок действия файла рассылки {0} истек
+fileNameEndOfLine = {0}'
+WHERE id = 27;
+
+UPDATE mail_content
+SET messages_russian='workGroupCreationDateTitle = Дата создания
+mainMsg = <b> {0} <span style="text-transform:uppercase">{1}</span></b> добавил вас в рабочую группу <br>
+simpleMainMsg = Вас добавили в рабочую группу
+subject = Вас добавили в рабочую группу {0}
+workGroupRight = Права по умолчанию
+workGroupNameTitle = Название рабочей группы'
+WHERE id = 28;
+
+UPDATE mail_content
+SET messages_russian='workGroupUpdatedDateTitle = Дата обновления
+mainMsg = Ваш статус в рабочей группе
+mainMsgNext= был обновлен
+mainMsgNextBy= by <b> {0} <span style="text-transform:uppercase">{1}</span></b>.
+subject =  Ваш статус в рабочей группе {0} был обновлен.
+workGroupRight = Действующий статус
+workGroupNameTitle = Название рабочей группы'
+WHERE id = 29;
+
+UPDATE mail_content
+SET messages_russian='subject = У вас больше нет доступа к рабочей группе {0}.
+mainMsg = <b> {0} <span style="text-transform:uppercase">{1}</span></b> удалил вас из рабочей группы  <b>{2}</b>
+simpleMsg =  У вас больше нет доступа к рабочей группе <b>{0}</b>.
+workGroupNameTitle = Название рабочей группы'
+WHERE id = 30;
+
+UPDATE mail_content
+SET messages_russian='accountCreationDateTitle = Дата создания аккаунта
+accountExpiryDateTitle = Дата окончания действия аккаунта
+mainMsg = Пароль аккаунта {0} <b>{1}</b> был изменен.
+subject = Ваш пароль был изменен'
+WHERE id=31;
+
+UPDATE mail_content
+SET messages_russian='subject = Создание постоянного токена аутентификации
+mainMsg =  <b> {0} <span style="text-transform:uppercase">{1}</span></b> создал постоянный токен аутентификации для вашей учетной записи.
+tokenCreationDate = Дата создания
+tokenLabel = Имя
+tokenDescription = Описание'
+WHERE id=32;
+
+UPDATE mail_content
+SET messages_russian='subject = Удаление постоянного токена аутентификации
+mainMsg =  <b> {0} <span style="text-transform:uppercase">{1}</span></b> удалил постоянный токен аутентификации для вашего аккаунта.
+tokenCreationDate = Дата создания
+tokenLabel = Имя
+tokenDescription = Описание'
+WHERE id=33;
+--End 
+
+--Update the mail footer with the russian message
+UPDATE mail_footer SET messages_russian='learnMoreAbout=Узнать больше
+productOfficialWebsite=http://www.linshare.org/',footer='<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+  <body>
+    <div data-th-fragment="email_footer">
+                                <td style="border-collapse:collapse;padding: 6px 0 4px 17px;" valign="top">
+                                  <p style="margin: 0; font-size: 10px;"><span th:text="#{learnMoreAbout}">En savoir plus sur</span>
+<a   th:href="@{#{productOfficialWebsite}}"  target="_blank"style="text-decoration:none; color:#a9a9a9;"><strong th:text="#{productName}">LinShare</strong>™</a>
+                                  </p>
+                                </td>
+                                <td style="border-collapse:collapse; padding:  6px 17px 4px 0;"  valign="top" width="60">
+                                  <img alt="libre-and-free" height="9"
+                                       src="cid:logo.libre.and.free@linshare.org"
+                                       style="line-height:100%;width:60px;height:9px;padding:0" width="60" />
+ </td>
+   </div>
+ </body>
+ </html>'
+WHERE id=1;
+--End
+
+--Update mail layout with the new russian message
+UPDATE mail_layout SET messages_russian='common.availableUntil = Срок действия
+common.byYou= | Вами
+common.download= Загрузить
+common.filesInShare = Прикрепленные файлы
+common.recipients = Получатели
+common.titleSharedThe= Дата создания
+date.format=d MMMM, yyyy
+productCompagny= Linagora
+productName=LinShare
+workGroupRightAdminTitle = Администратор
+workGroupRightWirteTitle = Автор
+workGroupRightContributeTitle = Редактор
+workGroupRightReadTitle = Читатель
+welcomeMessage = Здравствуйте, {0}'
+WHERE id=1;
+--End
+
+--Set the messages to be not null on database.
 ALTER TABLE mail_content ALTER COLUMN messages_english SET NOT NULL;
 ALTER TABLE mail_content ALTER COLUMN messages_french SET NOT NULL;
-
-ALTER TABLE mail_footer ADD COLUMN messages_russian text;
---To be deleted once we have russian translation
-UPDATE mail_footer SET messages_russian = messages_english;
+ALTER TABLE mail_content ALTER COLUMN messages_russian SET NOT NULL;
 ALTER TABLE mail_footer ALTER COLUMN messages_russian SET NOT NULL;
 ALTER TABLE mail_footer ALTER COLUMN messages_english SET NOT NULL;
 ALTER TABLE mail_footer ALTER COLUMN messages_french SET NOT NULL;
-
-ALTER TABLE mail_layout ADD COLUMN messages_russian text;
---To be deleted once we have russian translation
-UPDATE mail_layout SET messages_russian = messages_english;
-ALTER TABLE mail_layout ALTER COLUMN messages_russian SET NOT NULL;
 ALTER TABLE mail_layout ALTER COLUMN messages_english SET NOT NULL;
 ALTER TABLE mail_layout ALTER COLUMN messages_french SET NOT NULL;
+ALTER TABLE mail_layout ALTER COLUMN messages_russian SET NOT NULL;
+--End
 
 -- LinShare version
 SELECT ls_version();
