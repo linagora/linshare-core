95aed6dcd0c33bd1201515c17709cb5e536faa11
Fayçal BORDJAH <fbordjah@linagora.com>
2019-06-14 09:42:59 +0200

    Avoid display file edition functionality on admin IHM

--- src/main/resources/sql/postgresql/Migration_2.2.0_to_2.3.0.sql
+++ src/main/resources/sql/postgresql/Migration_2.2.0_to_2.3.0.sql
@@ -199,11 +199,11 @@ INSERT INTO functionality_boolean(functionality_id, boolean_value)
 
 	-- Functionality : WORK_GROUP__FILE_EDITION
 INSERT INTO policy(id, status, default_status, policy, system)
-	VALUES (303, true, true, 1, false);
+	VALUES (303, false, false, 2, true);
 INSERT INTO policy(id, status, default_status, policy, system)
-	VALUES (304, true, true, 1, false);
+	VALUES (304, false, false, 2, true);
 INSERT INTO policy(id, status, default_status, policy, system) 
-	VALUES (305, true, true, 1, false);
+	VALUES (305, false, false, 2, true);
 INSERT INTO functionality(id, system, identifier, policy_activation_id, policy_configuration_id, policy_delegation_id, domain_id, parent_identifier, param, creation_date, modification_date)
 	VALUES (65, false, 'WORK_GROUP__FILE_EDITION', 303, 304, 305, 1, 'WORK_GROUP', true, now(), now());
 INSERT INTO functionality_string(functionality_id, string_value) 
