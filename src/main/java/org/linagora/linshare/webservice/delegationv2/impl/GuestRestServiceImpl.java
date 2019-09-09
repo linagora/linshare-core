6333902fecc839cc8e334c7b7916fc917d2540bc
Marwan JEBAHI <mjebahi@linagora.com>
2019-09-09 15:36:54 +0200

    Add missing endpoints for delegation v2 api

---
+++ src/main/java/org/linagora/linshare/webservice/delegationv2/impl/GuestRestServiceImpl.java
@@ -0,0 +1,202 @@
+/*
+ * LinShare is an open source filesharing software, part of the LinPKI software
+ * suite, developed by Linagora.
+ * 
+ * Copyright (C) 2015-2018 LINAGORA
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display of the “LinShare™” trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source
+ * and free version of LinShare™, powered by Linagora © 2009–2018. Contribute to
+ * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
+ * e-mails sent with the Program, (ii) retain all hypertext links between
+ * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
+ * refrain from infringing Linagora intellectual property rights over its
+ * trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to LinShare software.
+ */
+
+package org.linagora.linshare.webservice.delegationv2.impl;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.linagora.linshare.core.exception.BusinessException;
+import org.linagora.linshare.core.facade.webservice.common.dto.GuestDto;
+import org.linagora.linshare.core.facade.webservice.delegation.GuestFacade;
+import org.linagora.linshare.webservice.WebserviceBase;
+import org.linagora.linshare.webservice.delegationv2.GuestRestService;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+
+
+
+@Path("{actorUuid}/guests")
+@Api(value = "/rest/delegation/v2/{actorUuid}/guests", basePath = "/rest/delegation/v2/", description = "Guests service.",
+	produces = "application/json,application/xml", consumes = "application/json,application/xml")
+@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+public class GuestRestServiceImpl extends WebserviceBase implements GuestRestService {
+
+	private GuestFacade guestFacade;
+
+	public GuestRestServiceImpl(GuestFacade guestFacade) {
+		super();
+		this.guestFacade = guestFacade;
+	}
+
+	@Path("/")
+	@POST
+	@ApiOperation(value = "Create a guest.", response = GuestDto.class)
+	@ApiResponses({ @ApiResponse(code = 403, message = "Current logged in account does not have the delegation role."),
+					@ApiResponse(code = 404, message = "Guest not found."),
+					@ApiResponse(code = 400, message = "Bad request : missing required fields."),
+					@ApiResponse(code = 500, message = "Internal server error."),
+					})
+	@Override
+	public GuestDto create(
+			@ApiParam(value = "The actor (user) uuid.", required = true) @PathParam("actorUuid") String actorUuid,
+			@ApiParam(value = "Guest to create.", required = true) GuestDto guest)
+					throws BusinessException {
+		return guestFacade.create(actorUuid, guest);
+	}
+
+	@Path("/{identifier}")
+	@GET
+	@ApiOperation(value = "Get a guest.", response = GuestDto.class)
+	@ApiResponses({ @ApiResponse(code = 403, message = "Current logged in account does not have the delegation role.") ,
+					@ApiResponse(code = 404, message = "Guest not found."),
+					@ApiResponse(code = 400, message = "Bad request : missing required fields."),
+					@ApiResponse(code = 500, message = "Internal server error."),
+					})
+	@Override
+	public GuestDto get(
+			@ApiParam(value = "The actor (user) uuid.", required = true) @PathParam("actorUuid") String actorUuid,
+			@ApiParam(value = "The guest identifier, could be uuid or mail.", required = true) @PathParam("identifier") String identifier,
+			@ApiParam(value = "Boolean value to search by domain.") @DefaultValue("false") @QueryParam("mail") Boolean isMail,
+			@ApiParam(value = "Domain identifier.") @QueryParam("domain") String domain)
+			throws BusinessException {
+		if (isMail) {
+			return guestFacade.find(actorUuid, domain, identifier);
+		}
+		return guestFacade.find(actorUuid, identifier);
+	}
+
+	@Path("/{identifier}")
+	@HEAD
+	@ApiOperation(value = "Get a guest.", response = GuestDto.class)
+	@ApiResponses({ @ApiResponse(code = 403, message = "Current logged in account does not have the delegation role.") ,
+					@ApiResponse(code = 404, message = "Guest not found."),
+					@ApiResponse(code = 400, message = "Bad request : missing required fields."),
+					@ApiResponse(code = 500, message = "Internal server error."),
+					})
+	@Override
+	public void head(
+			@ApiParam(value = "The actor (user) uuid.", required = true) @PathParam("actorUuid") String actorUuid,
+			@ApiParam(value = "The guest identifier, could be uuid or mail.", required = true) @PathParam("identifier") String identifier,
+			@ApiParam(value = "Boolean value to search by domain.") @DefaultValue("false") @QueryParam("mail") Boolean isMail,
+			@ApiParam(value = "Domain identifier.") @QueryParam("domain") String domain)
+			throws BusinessException {
+		if (isMail) {
+			guestFacade.find(actorUuid, domain, identifier);
+		}
+		guestFacade.find(actorUuid, identifier);
+	}
+
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/")
+	@GET
+	@ApiOperation(value = "Get all guests.", response = GuestDto.class, responseContainer = "Set")
+	@ApiResponses({ @ApiResponse(code = 403, message = "Current logged in account does not have the delegation role.") ,
+					@ApiResponse(code = 404, message = "Guest not found."),
+					@ApiResponse(code = 400, message = "Bad request : missing required fields."),
+					@ApiResponse(code = 500, message = "Internal server error."),
+					})
+	@Override
+	public List<GuestDto> getAll(
+			@ApiParam(value = "The actor (user) uuid.", required = true) @PathParam("actorUuid") String actorUuid) throws BusinessException {
+		return guestFacade.findAll(actorUuid);
+	}
+
+	@Path("/")
+	@PUT
+	@ApiOperation(value = "Update a guest.", response = GuestDto.class)
+	@ApiResponses({ @ApiResponse(code = 403, message = "Current logged in account does not have the delegation role.") ,
+					@ApiResponse(code = 404, message = "Guest or guest not found."),
+					@ApiResponse(code = 400, message = "Bad request : missing required fields."),
+					@ApiResponse(code = 500, message = "Internal server error."),
+					})
+	@Override
+	public GuestDto update(
+			@ApiParam(value = "The actor (user) uuid.", required = true) @PathParam("actorUuid") String actorUuid,
+			@ApiParam(value = "Guest to update.", required = true) GuestDto guest)
+			throws BusinessException {
+		return guestFacade.update(actorUuid, guest);
+	}
+
+	@Path("/")
+	@DELETE
+	@ApiOperation(value = "Delete a guest.", response = GuestDto.class)
+	@ApiResponses({ @ApiResponse(code = 403, message = "Current logged in account does not have the delegation role.") ,
+					@ApiResponse(code = 404, message = "Guest or guest not found."),
+					@ApiResponse(code = 400, message = "Bad request : missing required fields."),
+					@ApiResponse(code = 500, message = "Internal server error."),
+					})
+	@Override
+	public GuestDto delete(
+			@ApiParam(value = "The actor (user) uuid.", required = true) @PathParam("actorUuid") String actorUuid,
+			@ApiParam(value = "Guest to delete.", required = true) GuestDto guest)
+					throws BusinessException {
+		return guestFacade.delete(actorUuid, guest);
+	}
+
+	@Path("/{uuid}")
+	@DELETE
+	@ApiOperation(value = "Delete a guest.", response = GuestDto.class)
+	@ApiResponses({ @ApiResponse(code = 403, message = "Current logged in account does not have the delegation role.") ,
+					@ApiResponse(code = 404, message = "Guest or guest not found."),
+					@ApiResponse(code = 400, message = "Bad request : missing required fields."),
+					@ApiResponse(code = 500, message = "Internal server error."),
+					})
+	@Override
+	public GuestDto delete(
+			@ApiParam(value = "The actor (user) uuid.", required = true) @PathParam("actorUuid") String actorUuid, 
+			@ApiParam(value = "The guest uuid.", required = true) @PathParam("uuid") String uuid)
+					throws BusinessException {
+		return guestFacade.delete(actorUuid, uuid);
+	}
+
+}
