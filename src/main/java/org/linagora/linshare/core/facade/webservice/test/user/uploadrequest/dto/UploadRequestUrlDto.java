426fb9b678f4212dec52a39dcc7451cf7ea3f252
Marwan JEBAHI <mjebahi@linagora.com>
2019-12-04 10:07:45 +0100

    Test api to find uploadRequestUrls of an uploadRequest

---
+++ src/main/java/org/linagora/linshare/core/facade/webservice/test/user/uploadrequest/dto/UploadRequestUrlDto.java
@@ -0,0 +1,135 @@
+/*
+ * LinShare is an open source filesharing software, part of the LinPKI software
+ * suite, developed by Linagora.
+ * 
+ * Copyright (C) 2019 LINAGORA
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display of the “LinShare™” trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source
+ * and free version of LinShare™, powered by Linagora © 2009–2019. Contribute to
+ * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
+ * e-mails sent with the Program, (ii) retain all hypertext links between
+ * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
+ * refrain from infringing Linagora intellectual property rights over its
+ * trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to LinShare software.
+ */
+package org.linagora.linshare.core.facade.webservice.test.user.uploadrequest.dto;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.linagora.linshare.core.domain.entities.UploadRequestUrl;
+import org.linagora.linshare.core.facade.webservice.uploadrequest.dto.ContactDto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@XmlRootElement(name = "UploadRequestUrlTest")
+public class UploadRequestUrlDto {
+
+	private ContactDto contact;
+
+	private String uuid;
+
+	private String path;
+
+	private String password;
+
+	private String temporaryPlainTextPassword;
+
+	private Date creationDate;
+
+	private Date modificationDate;
+
+	public UploadRequestUrlDto() {
+		super();
+	}
+
+	public UploadRequestUrlDto(UploadRequestUrl uploadRequestUrl) {
+		super();
+		this.uuid = uploadRequestUrl.getUuid();
+		this.contact = new ContactDto(uploadRequestUrl.getContact());
+		this.path = uploadRequestUrl.getPath();
+		this.creationDate = uploadRequestUrl.getCreationDate();
+		this.modificationDate = uploadRequestUrl.getModificationDate();
+	}
+
+	public ContactDto getContact() {
+		return contact;
+	}
+
+	public void setContact(ContactDto contact) {
+		this.contact = contact;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Date getModificationDate() {
+		return modificationDate;
+	}
+
+	public void setModificationDate(Date modificationDate) {
+		this.modificationDate = modificationDate;
+	}
+
+	public boolean isProtectedByPassword() {
+		return password != null;
+	}
+
+	public String getTemporaryPlainTextPassword() {
+		return temporaryPlainTextPassword;
+	}
+
+	public void setTemporaryPlainTextPassword(String temporaryPlainTextPassword) {
+		this.temporaryPlainTextPassword = temporaryPlainTextPassword;
+	}
+}
