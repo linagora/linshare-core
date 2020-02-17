3678d9ddb46b0b6ce81ae0a5d3ed0a53af68055c
Frédéric MARTIN <fmartin@linagora.com>
2020-02-17 11:32:35 +0100

    Adding adminv4 endpoint with openapi v3 support.

---
+++ src/main/java/org/linagora/linshare/webservice/adminv4/impl/AuthenticationRestServiceImpl.java
@@ -0,0 +1,123 @@
+/*
+ * LinShare is an open source filesharing software, part of the LinPKI software
+ * suite, developed by Linagora.
+ * 
+ * Copyright (C) 2015-2018 LINAGORA
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display of the “LinShare™” trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source
+ * and free version of LinShare™, powered by Linagora © 2009–2018. Contribute to
+ * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
+ * e-mails sent with the Program, (ii) retain all hypertext links between
+ * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
+ * refrain from infringing Linagora intellectual property rights over its
+ * trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to LinShare software.
+ */
+package org.linagora.linshare.webservice.adminv4.impl;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.linagora.linshare.core.domain.constants.Role;
+import org.linagora.linshare.core.exception.BusinessException;
+import org.linagora.linshare.core.facade.webservice.admin.AdminGenericFacade;
+import org.linagora.linshare.core.facade.webservice.admin.UserFacade;
+import org.linagora.linshare.core.facade.webservice.common.dto.PasswordDto;
+import org.linagora.linshare.core.facade.webservice.common.dto.UserDto;
+import org.linagora.linshare.webservice.WebserviceBase;
+import org.linagora.linshare.webservice.adminv4.AuthenticationRestService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+
+
+@Path("/authentication")
+@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+public class AuthenticationRestServiceImpl extends WebserviceBase implements AuthenticationRestService {
+
+	private final AdminGenericFacade adminFacade;
+
+	private final UserFacade userFacade;
+
+	public AuthenticationRestServiceImpl(final AdminGenericFacade adminFacade, final UserFacade userFacade) {
+		this.adminFacade = adminFacade;
+		this.userFacade = userFacade;
+	}
+
+	@Path("/")
+	@GET
+	@Operation(summary = "No operation.")
+	@Override
+	public void noop() {
+		return; // do nothing
+	}
+
+	@Path("/authorized")
+	@GET
+	@Operation(
+			summary = "Check if user is authorized.",
+			responses = {
+		            @ApiResponse(
+		                content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserDto.class))),
+		                responseCode = "200"
+		            )
+		        }
+	)
+	@Override
+	public UserDto isAuthorized() throws BusinessException {
+		return adminFacade.isAuthorized(Role.ADMIN);
+	}
+
+	@Path("/change_password")
+	@POST
+//	@Operation(
+//			summary = "Change the password of the current user."
+//	)
+	@Override
+	public void changePassword(PasswordDto password) throws BusinessException {
+		userFacade.changePassword(password);
+	}
+
+	@Path("/logout")
+	@GET
+//	@Operation(summary = "Logout the current user.")
+	@Override
+	public void logout() {
+		// This code is never reach because the URL will be catch by spring security before.
+		// This function was created just to show the logout URL into WADL.
+	}
+
+	@Path("/version")
+	@GET
+	@Override
+	public String getVersion() {
+		return getCoreVersion();
+	}
+}
