5f970f71b1cce37489e31426e358a76720bcfe57
Marwan JEBAHI <mjebahi@linagora.com>
2020-06-18 14:34:06 +0200

    Add npe mapper exception

--- src/main/java/org/linagora/linshare/webservice/interceptor/NullPointerExceptionMapper.java
+++ src/main/java/org/linagora/linshare/webservice/interceptor/NullPointerExceptionMapper.java
@@ -2,7 +2,7 @@
  * LinShare is an open source filesharing software, part of the LinPKI software
  * suite, developed by Linagora.
  * 
- * Copyright (C) 2018 LINAGORA
+ * Copyright (C) 2020 LINAGORA
  * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License as published by the Free
@@ -12,7 +12,7 @@
  * Public License, subsections (b), (c), and (e), pursuant to which you must
  * notably (i) retain the display of the “LinShare™” trademark/logo at the top
  * of the interface window, the display of the “You are using the Open Source
- * and free version of LinShare™, powered by Linagora © 2009–2018 to
+ * and free version of LinShare™, powered by Linagora © 2009–2020. Contribute to
  * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
  * e-mails sent with the Program, (ii) retain all hypertext links between
  * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
@@ -31,25 +31,42 @@
  * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
  * applicable to LinShare software.
  */
+package org.linagora.linshare.webservice.interceptor;
 
-package org.linagora.linshare.core.domain.constants;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
 
-public enum ExceptionType {
+import org.linagora.linshare.core.domain.constants.ExceptionType;
+import org.linagora.linshare.core.exception.BusinessErrorCode;
+import org.linagora.linshare.core.facade.webservice.admin.ExceptionStatisticAdminFacade;
+import org.linagora.linshare.core.facade.webservice.common.dto.ErrorDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
-	ILLEGAL_ARGUMENT_EXCEPTION, 
-	JSON_PARSE_EXCEPTION, 
-	BUSINESS_EXCEPTION, 
-	EXCEPTION,
-	UNRECOGNIZED_PROPERTY_EXCEPTION,
-	DATA_INTEGRITY_VIOLATION_EXCEPTION,
-	MISMATCHED_INPUT_EXCEPTION,
-	NULL_POINTER_EXCEPTION;
+@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+public class NullPointerExceptionMapper implements ExceptionMapper<NullPointerException> {
 
-	public static ExceptionType fromString(String s) {
-		try {
-			return ExceptionType.valueOf(s.toUpperCase());
-		} catch (RuntimeException e) {
-			throw new IllegalArgumentException("Doesn't match an existing Exception Type");
-		}
+	@Autowired
+	protected ExceptionStatisticAdminFacade exceptionStatisticFacade;
+
+	private static final Logger logger = LoggerFactory.getLogger(NullPointerExceptionMapper.class);
+
+	@Override
+	public Response toResponse(NullPointerException exception) {
+		logger.error("Bad data format : " + exception.getLocalizedMessage() + ". ",
+				exception);
+		ErrorDto errorDto = new ErrorDto(BusinessErrorCode.WEBSERVICE_BAD_REQUEST_NULL_POINTER_EXCEPTION.getCode(),
+				"Bad data format : " + exception.toString()
+				+ ", " + exception.getClass().toString());
+		ResponseBuilder response = Response.status(Status.BAD_REQUEST);
+		exceptionStatisticFacade.createExceptionStatistic(null, exception.getStackTrace(), ExceptionType.NULL_POINTER_EXCEPTION);
+		response.entity(errorDto);
+		return response.build();
 	}
+
 }
