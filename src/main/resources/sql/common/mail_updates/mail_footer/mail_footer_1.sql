c6ea87fface87d58b87aff6f2f0b3ab644da5af4
Fayçal BORDJAH <fbordjah@linagora.com>
2020-06-30 14:50:15 +0200

    Formatting mails footer and layout

--- src/main/resources/sql/common/mail_updates/mail_footer/mail_footer_1.sql
+++ src/main/resources/sql/common/mail_updates/mail_footer/mail_footer_1.sql
@@ -3,18 +3,21 @@ productOfficialWebsite=http://www.linshare.org/',messages_english='learnMoreAbou
 productOfficialWebsite=http://www.linshare.org/',messages_russian='learnMoreAbout=Узнать больше
 productOfficialWebsite=http://www.linshare.org/',footer='<!DOCTYPE html>
 <html xmlns:th="http://www.thymeleaf.org">
-  <body>
-    <div data-th-fragment="email_footer">
-                                <td style="border-collapse:collapse;padding: 6px 0 4px 17px;" valign="top">
-                                  <p style="margin: 0; font-size: 10px;"><span th:text="#{learnMoreAbout}">En savoir plus sur</span>
-<a   th:href="@{#{productOfficialWebsite}}"  target="_blank"style="text-decoration:none; color:#a9a9a9;"><strong th:text="#{productName}">LinShare</strong>™</a>
-                                  </p>
-                                </td>
-                                <td style="border-collapse:collapse; padding:  6px 17px 4px 0;"  valign="top" width="60">
-                                  <img alt="libre-and-free" height="9"
-                                       src="cid:logo.libre.and.free@linshare.org"
-                                       style="line-height:100%;width:60px;height:9px;padding:0" width="60" />
- </td>
-   </div>
- </body>
- </html>' WHERE id=1;
+   <body>
+      <div data-th-fragment="email_footer">
+         <td style="border-collapse:collapse;padding: 6px 0 4px 17px;" valign="top">
+            <p style="margin: 0; font-size: 10px;">
+               <span th:text="#{learnMoreAbout}">Learn more about</span>
+               <a th:href="@{#{productOfficialWebsite}}"  target="_blank" style="text-decoration:none; color:#a9a9a9;">
+                 <strong th:text="#{productName}">LinShare</strong>™
+               </a>
+            </p>
+         </td>
+         <td style="border-collapse:collapse; padding:  6px 17px 4px 0;"  valign="top" width="60">
+            <img alt="libre-and-free" height="9"
+               src="cid:logo.libre.and.free@linshare.org"
+               style="line-height:100%;width:60px;height:9px;padding:0" width="60" />
+         </td>
+      </div>
+   </body>
+</html>' WHERE id=1;
