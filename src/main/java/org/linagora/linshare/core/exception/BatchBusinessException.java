63de10f529824ea9da902f7d3c777dc14ee67772
Frédéric MARTIN <fmartin@linagora.com>
2020-07-10 15:17:36 +0200

    Update LinShare copyright headers (message)

--- src/main/java/org/linagora/linshare/core/exception/BatchBusinessException.java
+++ src/main/java/org/linagora/linshare/core/exception/BatchBusinessException.java
@@ -9,15 +9,17 @@
  * later version, provided you comply with the Additional Terms applicable for
  * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
  * Public License, subsections (b), (c), and (e), pursuant to which you must
- * notably (i) retain the display of the “LinShare™” trademark/logo at the top
- * of the interface window, the display of the “You are using the Open Source
- * and free version of LinShare™, powered by Linagora © 2009–2020. Contribute to
- * Linshare R&D by subscribing to an Enterprise offer!” infobox and in the
- * e-mails sent with the Program, (ii) retain all hypertext links between
- * LinShare and linshare.org, between linagora.com and Linagora, and (iii)
- * refrain from infringing Linagora intellectual property rights over its
- * trademarks and commercial brands. Other Additional Terms apply, see
- * <http://www.linagora.com/licenses/> for more details.
+ * notably (i) retain the display in the interface of the “LinShare™”
+ * trademark/logo, the "Libre & Free" mention, the words “You are using the Free
+ * and Open Source version of LinShare™, powered by Linagora © 2009–2020.
+ * Contribute to Linshare R&D by subscribing to an Enterprise offer!”. You must
+ * also retain the latter notice in all asynchronous messages such as e-mails
+ * sent with the Program, (ii) retain all hypertext links between LinShare and
+ * http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks and
+ * commercial brands. Other Additional Terms apply, see
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for more
+ * details.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -27,8 +29,9 @@
  * You should have received a copy of the GNU Affero General Public License and
  * its applicable Additional Terms for LinShare along with this program. If not,
  * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
- * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
- * applicable to LinShare software.
+ * version 3 and
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for the
+ * Additional Terms applicable to LinShare software.
  */
 
 package org.linagora.linshare.core.exception;
