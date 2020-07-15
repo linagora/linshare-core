de0cd40b242acf5513c600f7ab7fae97f165c87c
Marwan JEBAHI <mjebahi@linagora.com>
2020-07-15 10:09:05 +0200

    Add healthcheck api

---
+++ src/main/java/org/linagora/linshare/webservice/healthcheck/impl/HealthCheckRestServiceImpl.java
@@ -0,0 +1,63 @@
+/*
+ * LinShare is an open source filesharing software developed by LINAGORA.
+ * 
+ * Copyright (C) 2015-2020 LINAGORA
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display in the interface of the “LinShare™”
+ * trademark/logo, the "Libre & Free" mention, the words “You are using the Free
+ * and Open Source version of LinShare™, powered by Linagora © 2009–2020.
+ * Contribute to Linshare R&D by subscribing to an Enterprise offer!”. You must
+ * also retain the latter notice in all asynchronous messages such as e-mails
+ * sent with the Program, (ii) retain all hypertext links between LinShare and
+ * http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks and
+ * commercial brands. Other Additional Terms apply, see
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for more
+ * details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for the
+ * Additional Terms applicable to LinShare software.
+ */
+package org.linagora.linshare.webservice.healthcheck.impl;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.linagora.linshare.webservice.healthcheck.HealthCheckRestService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+@Path("/")
+@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+public class HealthCheckRestServiceImpl implements HealthCheckRestService {
+
+	@Path("/")
+	@GET
+	@Operation(summary = "Health check of the different services of LinShare.", responses = {
+			@ApiResponse(responseCode = "204")
+			})
+	@Override
+	public void healthCheckServices() {
+		// TODO: Implement the business logic of the healthCheck of the different services of LinShare
+	}
+}
