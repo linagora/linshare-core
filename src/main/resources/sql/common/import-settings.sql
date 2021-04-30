47dfa0e7b7676e069928d515b976fb1410149282
Frederic MARTIN <fmartin@linagora.com>
2021-04-30 08:19:39 +0000

    Update LinShare's version in import-settings.sql

--- src/main/resources/sql/common/import-settings.sql
+++ src/main/resources/sql/common/import-settings.sql
@@ -1,5 +1,5 @@
 -- LinShare version
-INSERT INTO version (id, version, creation_date) VALUES (1, '4.1.0', now());
+INSERT INTO version (id, version, creation_date) VALUES (1, '4.2.0', now());
 
 -- Sequence for hibernate
 SELECT setval('hibernate_sequence', 1000);
