fe0fc03ccf146d9ea2d4c6d5a685ca4193c889b8
Marwan JEBAHI <mjebahi@linagora.com>
2021-06-30 12:00:28 +0200

    Fix glitch between activation and closure date for war before expiry UR

--- src/main/resources/sql/postgresql/Patch_4.1.3.sql
+++ src/main/resources/sql/postgresql/Patch_4.1.3.sql
@@ -1,3 +1,68 @@
+-- This script should only be executed after LinShare 4.1.0 installation
+-- This script is idempotent it has no additional effect if run more than once on the same database 
+BEGIN;
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET client_min_messages = info;
+SET default_with_oids = false;
+
+CREATE OR REPLACE FUNCTION ls_check_user_connected() RETURNS void AS $$
+BEGIN
+	DECLARE database VARCHAR := (SELECT current_database());
+	DECLARE user_connected VARCHAR := (SELECT current_user);
+	DECLARE error VARCHAR := ('You are actually connected with the user "postgres", you should be connected with your LinShare database user, we are about to stop the migration script.');
+	BEGIN
+		RAISE INFO 'Connected to "%" with user "%"', database, user_connected;
+		IF (user_connected = 'postgres') THEN
+			RAISE WARNING '%', error;
+		--	DIRTY: did it to stop the process cause there is no clean way to do it.
+		--	Expected error: query has no destination for result data.
+			SELECT '';
+		END IF;
+	END;
+END
+$$ LANGUAGE plpgsql;
+
+
+-- Check if can apply the patch to the current LinShare version 4.1.0
+CREATE OR REPLACE FUNCTION ls_prechecks_patch() RETURNS void AS $$
+BEGIN
+	DECLARE current_version VARCHAR := '4.1.0';
+	DECLARE start VARCHAR := concat('You are about to apply a database patch to LinShare : ', current_version);
+	DECLARE version_history_from VARCHAR := (SELECT version from version ORDER BY id DESC LIMIT 1);
+	DECLARE database_info VARCHAR = version();
+	DECLARE error VARCHAR := concat('This patch should only be applied to the version : ', current_version);
+	DECLARE connection_id INT := pg_backend_pid();
+	DECLARE row record;
+	BEGIN
+		RAISE NOTICE '%', start;
+		RAISE NOTICE 'Your current LinShare database version is: %', version_history_from;
+		RAISE NOTICE 'Your database history is :';
+		FOR row IN (SELECT * FROM version ORDER BY id DESC) LOOP
+			RAISE INFO '%', row.version;
+		END LOOP;
+		RAISE NOTICE 'Your database system information is : %', database_info;
+		IF (current_version <> version_history_from) THEN
+			RAISE WARNING 'You must be in version : % to run this script. You are actually in version: %', current_version, version_history_from;
+			IF EXISTS (SELECT * from version where version = current_version) THEN
+				RAISE WARNING '%', error;
+			END IF;
+			RAISE WARNING 'We are about to abort the migration script, all the following instructions will be aborted and transaction will rollback.';
+			RAISE INFO 'You should expect the following error : "query has no destination for result data".';
+	--		DIRTY: did it to stop the process cause there is no clean way to do it.
+	--		Expected error: query has no destination for result data.
+			select error;
+		END IF;
+	END;
+END
+$$ LANGUAGE plpgsql;
+
+
+SELECT ls_check_user_connected();
+SELECT ls_prechecks_patch();
+
+-- Update the UPLOAD_REQUEST_WARN_BEFORE_EXPIRY
 UPDATE mail_content SET subject='[# th:if="${warnOwner}"] [( #{subjectForOwner})]
 [/]
 [# th:if="${!warnOwner}"]
@@ -114,3 +179,5 @@ recipientsURequest = Получатели
 subjectForOwner =  Срок действия вашего приглашения заканчивается.
 subjectForRecipient =  {0} {1}''''s срок действия вашего приглашения заканчивается.
 uploadFileBtn = Загрузить файл' WHERE id=12;
+
+COMMIT;
