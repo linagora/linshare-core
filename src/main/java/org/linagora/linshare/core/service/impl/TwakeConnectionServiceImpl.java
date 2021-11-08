b70c0215e490c2a43c75d79bfb49982ece1e35e9
Antoine DUPRAT <aduprat@linagora.com>
2021-11-08 13:27:41 +0100

    Introduce TwakeConnectionServiceImpl

---
+++ src/main/java/org/linagora/linshare/core/service/impl/TwakeConnectionServiceImpl.java
@@ -0,0 +1,70 @@
+/*
+ * LinShare is an open source filesharing software developed by LINAGORA.
+ * 
+ * Copyright (C) 2015-2021 LINAGORA
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display in the interface of the “LinShare™”
+ * trademark/logo, the "Libre & Free" mention, the words “You are using the Free
+ * and Open Source version of LinShare™, powered by Linagora © 2009–2021.
+ * Contribute to Linshare R&D by subscribing to an Enterprise offer!”. You must
+ * also retain the latter notice in all asynchronous messages such as e-mails
+ * sent with the Program, (ii) retain all hypertext links between LinShare and
+ * http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks and
+ * commercial brands. Other Additional Terms apply, see
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for more
+ * details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for the
+ * Additional Terms applicable to LinShare software.
+ */
+
+package org.linagora.linshare.core.service.impl;
+
+import java.util.List;
+
+import org.apache.commons.lang3.Validate;
+import org.linagora.linshare.core.business.service.SanitizerInputHtmlBusinessService;
+import org.linagora.linshare.core.domain.entities.AbstractDomain;
+import org.linagora.linshare.core.domain.entities.TwakeConnection;
+import org.linagora.linshare.core.exception.BusinessErrorCode;
+import org.linagora.linshare.core.exception.BusinessException;
+import org.linagora.linshare.core.repository.AbstractDomainRepository;
+import org.linagora.linshare.core.repository.hibernate.TwakeConnectionRepositoryImpl;
+
+public class TwakeConnectionServiceImpl extends RemoteServerServiceImpl<TwakeConnection> {
+
+	public TwakeConnectionServiceImpl(
+			TwakeConnectionRepositoryImpl twakeConnectionRepositoryImpl,
+			SanitizerInputHtmlBusinessService sanitizerInputHtmlBusinessService,
+			AbstractDomainRepository abstractDomainRepository) {
+		super(twakeConnectionRepositoryImpl, sanitizerInputHtmlBusinessService, abstractDomainRepository);
+	}
+
+	@Override
+	protected void updateFields(TwakeConnection remoteServer, TwakeConnection updateRemoteServer) {
+		updateRemoteServer.setClientId(remoteServer.getClientId());
+		updateRemoteServer.setClientSecret(remoteServer.getClientSecret());
+	}
+
+	@Override
+	public List<AbstractDomain> findAllDomainsByRemoteServer(TwakeConnection twakeConnection) {
+		Validate.notNull(twakeConnection, "Twake connection must be set.");
+		throw new BusinessException(BusinessErrorCode.NOT_IMPLEMENTED_YET, "Not implemented");
+	}
+}
