4bc2f62c304a8ab6267a96b41d472647ad76cdb5
Marwan JEBAHI <mjebahi@linagora.com>
2021-11-29 16:53:10 +0100

    Add domainQuotaDto

---
+++ src/main/java/org/linagora/linshare/core/facade/webservice/adminv5/dto/DomainQuotaDto.java
@@ -0,0 +1,377 @@
+/*
+ * LinShare is an open source filesharing software developed by LINAGORA.
+ * 
+ * Copyright (C) 2015-2021 LINAGORA
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display in the interface of the “LinShare™”
+ * trademark/logo, the "Libre & Free" mention, the words “You are using the Free
+ * and Open Source version of LinShare™, powered by Linagora © 2009–2021.
+ * Contribute to Linshare R&D by subscribing to an Enterprise offer!”. You must
+ * also retain the latter notice in all asynchronous messages such as e-mails
+ * sent with the Program, (ii) retain all hypertext links between LinShare and
+ * http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks and
+ * commercial brands. Other Additional Terms apply, see
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for more
+ * details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for the
+ * Additional Terms applicable to LinShare software.
+ */
+package org.linagora.linshare.core.facade.webservice.adminv5.dto;
+
+import java.util.Date;
+
+import org.linagora.linshare.core.domain.entities.DomainQuota;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.google.common.base.MoreObjects;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(name = "DomainQuota", description = "A domain quota instance for a domain.")
+public class DomainQuotaDto {
+
+	public static DomainQuotaDto from(DomainQuota dq) {
+		return builder()
+			.uuid(dq.getUuid())
+			.domain(new DomainLightDto(dq.getDomain()))
+			.parentDomain(new DomainLightDto(dq.getParentDomain()))
+			.quota(dq.getQuota())
+			.quotaOverride(dq.getQuotaOverride())
+			.defaultQuota(dq.getDefaultQuota())
+			.defaultQuotaOverride(dq.getDefaultQuotaOverride())
+			.usedSpace(dq.getCurrentValue())
+			.yesterdayUsedSpace(dq.getLastValue())
+			.maintenance(dq.getMaintenance())
+			.batchModificationDate(dq.getBatchModificationDate())
+			.currentValueForSubdomains(dq.getCurrentValueForSubdomains())
+			.domainShared(dq.getDomainShared())
+			.domainSharedOverride(dq.getDomainSharedOverride())
+			.defaultDomainShared(dq.getDefaultDomainShared())
+			.defaultDomainSharedOverride(dq.getDefaultDomainSharedOverride())
+			.build();
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	@JsonPOJOBuilder(withPrefix = "")
+	public static class Builder {
+		private String uuid;
+		private DomainLightDto domain;
+		private DomainLightDto parentDomain;
+		private Long quota;
+		private Boolean quotaOverride;
+		private Long defaultQuota;
+		private Boolean defaultQuotaOverride;
+		private Long usedSpace;
+		private Long yesterdayUsedSpace;
+		private Boolean maintenance;
+		private Date batchModificationDate;
+		private Date creationDate;
+		private Date modificationDate;
+		private Long currentValueForSubdomains;
+		private Boolean domainShared;
+		private Boolean domainSharedOverride;
+		private Boolean defaultDomainShared;
+		private Boolean defaultDomainSharedOverride;
+
+		public Builder uuid(String uuid) {
+			this.uuid= uuid;
+			return this;
+		}
+
+		public Builder domain(DomainLightDto domain) {
+			this.domain = domain;
+			return this;
+		}
+
+		public Builder parentDomain(DomainLightDto parentDomain) {
+			this.parentDomain = parentDomain;
+			return this;
+		}
+
+		public Builder quota(Long quota) {
+			this.quota = quota;
+			return this;
+		}
+
+		public Builder quotaOverride(Boolean quotaOverride) {
+			this.quotaOverride = quotaOverride;
+			return this;
+		}
+
+		public Builder defaultQuota(Long defaultQuota) {
+			this.defaultQuota = defaultQuota;
+			return this;
+		}
+
+		public Builder defaultQuotaOverride(Boolean defaultQuotaOverride) {
+			this.defaultQuotaOverride = defaultQuotaOverride;
+			return this;
+		}
+
+		public Builder usedSpace(Long usedSpace) {
+			this.usedSpace = usedSpace;
+			return this;
+		}
+
+		public Builder yesterdayUsedSpace(Long yesterdayUsedSpace) {
+			this.yesterdayUsedSpace = yesterdayUsedSpace;
+			return this;
+		}
+
+		public Builder maintenance(Boolean maintenance) {
+			this.maintenance = maintenance;
+			return this;
+		}
+
+		public Builder batchModificationDate(Date batchModificationDate) {
+			this.batchModificationDate = batchModificationDate;
+			return this;
+		}
+
+		public Builder currentValueForSubdomains(Long currentValueForSubdomains) {
+			this.currentValueForSubdomains = currentValueForSubdomains;
+			return this;
+		}
+
+		public Builder domainShared(Boolean domainShared) {
+			this.domainShared = domainShared;
+			return this;
+		}
+
+		public Builder domainSharedOverride(Boolean domainSharedOverride) {
+			this.domainSharedOverride = domainSharedOverride;
+			return this;
+		}
+
+		public Builder defaultDomainSharedOverride(Boolean defaultDomainSharedOverride) {
+			this.defaultDomainSharedOverride = defaultDomainSharedOverride;
+			return this;
+		}
+	
+		public Builder defaultDomainShared(Boolean defaultDomainShared) {
+			this.defaultDomainShared = defaultDomainShared;
+			return this;
+		}
+
+		public DomainQuotaDto build() {
+			return new DomainQuotaDto(uuid, domain, parentDomain, quota, quotaOverride, defaultQuota,
+					defaultQuotaOverride, usedSpace, yesterdayUsedSpace, maintenance, creationDate, modificationDate,
+					batchModificationDate, currentValueForSubdomains, domainShared, domainSharedOverride,
+					defaultDomainShared, defaultDomainSharedOverride);
+		}
+	}
+
+	@Schema(description = "Resource's uuid")
+	private final String uuid;
+
+	@Schema(description = "The domain which this quota belongs to.")
+	private final DomainLightDto domain;
+
+	@Schema(description = "The parent domain which this quota belongs to.")
+	private final DomainLightDto parentDomain;
+
+	@Schema(description = "The value of the current container quota limit")
+	private final Long quota;
+
+	@Schema(description = "If true, allow to override quota field for the current object (domain, container or account)"
+			+ "Otherwise the quota value can/will be updated by cascade by the defaultQuota field of your parent."
+			+ "Useless for root domain quota")
+	private final Boolean quotaOverride;
+
+	@Schema(description = "The default quota value allowed for each child quota.")
+	private final Long defaultQuota;
+
+	@Schema(description = "By default, defaultQuota equal quota, every modification of the quota field will be applied to defaultQuota too."
+			+ "If true, the defaultQuota value is unlinked from quota field.")
+	private final Boolean defaultQuotaOverride;
+
+	@Schema(description = "The used space (Read only)")
+	private final Long usedSpace;
+
+	@Schema(description = "The value of yesterday's used space (Read only)")
+	private final Long yesterdayUsedSpace;
+
+	@Schema(description = "If set to true, uploads are disable due to server maintenance.")
+	private final Boolean maintenance;
+
+	@Schema(description = "Quota creation date (Read only)")
+	private final Date creationDate;
+
+	@Schema(description = "Quota last modification date (Read only)")
+	private final Date modificationDate;
+
+	@Schema(description = "Quota last modification date by a batch (Read only)")
+	private final Date batchModificationDate;
+
+	@Schema(description = "The current quota value of subdomains")
+	private final Long currentValueForSubdomains;
+
+	@Schema(description = "If true, this mode exempt from settings each container's quota of this domain(Personal/sharedSpaces quota), the global comsuption of all domain's users will be seen.")
+	private final Boolean domainShared;
+
+	@Schema(description = "Shows if domainShared is overrided on the subdomains..")
+	private final Boolean domainSharedOverride;
+
+	@Schema(description = "Default value of domainShared on the subdomains.")
+	private final Boolean defaultDomainShared;
+
+	@Schema(description = "Default value of overriding the domainShared on the subdomains.")
+	private final Boolean defaultDomainSharedOverride;
+
+	private DomainQuotaDto(String uuid, DomainLightDto domain, DomainLightDto parentDomain, Long quota,
+			Boolean quotaOverride, Long defaultQuota, Boolean defaultQuotaOverride, Long usedSpace,
+			Long yesterdayUsedSpace, Boolean maintenance, Date creationDate, Date modificationDate,
+			Date batchModificationDate, Long currentValueForSubdomains,
+			Boolean domainShared, Boolean domainSharedOverride, Boolean defaultDomainShared,
+			Boolean defaultDomainSharedOverride) {
+		super();
+		this.uuid = uuid;
+		this.domain = domain;
+		this.parentDomain = parentDomain;
+		this.quota = quota;
+		this.quotaOverride = quotaOverride;
+		this.defaultQuota = defaultQuota;
+		this.defaultQuotaOverride = defaultQuotaOverride;
+		this.usedSpace = usedSpace;
+		this.yesterdayUsedSpace = yesterdayUsedSpace;
+		this.maintenance = maintenance;
+		this.creationDate = creationDate;
+		this.modificationDate = modificationDate;
+		this.batchModificationDate = batchModificationDate;
+		this.currentValueForSubdomains = currentValueForSubdomains;
+		this.domainShared = domainShared;
+		this.domainSharedOverride = domainSharedOverride;
+		this.defaultDomainShared = defaultDomainShared;
+		this.defaultDomainSharedOverride = defaultDomainSharedOverride;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public DomainLightDto getDomain() {
+		return domain;
+	}
+
+	public DomainLightDto getParentDomain() {
+		return parentDomain;
+	}
+
+	public Long getQuota() {
+		return quota;
+	}
+
+	public Boolean getQuotaOverride() {
+		return quotaOverride;
+	}
+
+	public Long getDefaultQuota() {
+		return defaultQuota;
+	}
+
+	public Boolean getDefaultQuotaOverride() {
+		return defaultQuotaOverride;
+	}
+
+	public Long getUsedSpace() {
+		return usedSpace;
+	}
+
+	public Long getYesterdayUsedSpace() {
+		return yesterdayUsedSpace;
+	}
+
+	public Boolean getMaintenance() {
+		return maintenance;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public Date getModificationDate() {
+		return modificationDate;
+	}
+
+	public Date getBatchModificationDate() {
+		return batchModificationDate;
+	}
+
+	public Long getCurrentValueForSubdomains() {
+		return currentValueForSubdomains;
+	}
+
+	public Boolean getDomainShared() {
+		return domainShared;
+	}
+
+	public Boolean getDomainSharedOverride() {
+		return domainSharedOverride;
+	}
+
+	public Boolean getDefaultDomainShared() {
+		return defaultDomainShared;
+	}
+
+	public Boolean getDefaultDomainSharedOverride() {
+		return defaultDomainSharedOverride;
+	}
+
+	public DomainQuota toObject() {
+		DomainQuota quota = new DomainQuota();
+		quota.setUuid(getUuid());
+		quota.setQuota(getQuota());
+		quota.setQuotaOverride(getQuotaOverride());
+		quota.setDefaultQuota(getDefaultQuota());
+		quota.setDefaultQuotaOverride(getDefaultQuotaOverride());
+		quota.setMaintenance(getMaintenance());
+		quota.setDomainShared(getDomainShared());
+		quota.setDomainSharedOverride(getDomainSharedOverride());
+		quota.setDefaultDomainShared(getDefaultDomainShared());
+		quota.setDefaultDomainSharedOverride(getDefaultDomainSharedOverride());
+		return quota;
+	}
+
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(this)
+				.add("uuid", uuid)
+				.add("domain", domain)
+				.add("quota", quota)
+				.add("quotaOverride", quotaOverride)
+				.add("defaultQuota", defaultQuota)
+				.add("defaultQuotaOverride",defaultQuotaOverride)
+				.add("usedSpace", usedSpace)
+				.add("maintenance", maintenance)
+				.add("domainShared", domainShared)
+				.add("domainSharedOverride", domainSharedOverride)
+				.add("defaultDomainShared", defaultDomainShared)
+				.add("defaultDomainSharedOverride", defaultDomainSharedOverride)
+				.add("creationDate", creationDate)
+				.add("modificationDate", modificationDate)
+				.toString();
+	}
+}
---
+++ src/main/java/org/linagora/linshare/webservice/adminv5/impl/DomainQuotaRestServiceImpl.java
@@ -0,0 +1,126 @@
+/*
+ * LinShare is an open source filesharing software developed by LINAGORA.
+ * 
+ * Copyright (C) 2015-2021 LINAGORA
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for
+ * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
+ * Public License, subsections (b), (c), and (e), pursuant to which you must
+ * notably (i) retain the display in the interface of the “LinShare™”
+ * trademark/logo, the "Libre & Free" mention, the words “You are using the Free
+ * and Open Source version of LinShare™, powered by Linagora © 2009–2021.
+ * Contribute to Linshare R&D by subscribing to an Enterprise offer!”. You must
+ * also retain the latter notice in all asynchronous messages such as e-mails
+ * sent with the Program, (ii) retain all hypertext links between LinShare and
+ * http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks and
+ * commercial brands. Other Additional Terms apply, see
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for more
+ * details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for LinShare along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
+ * version 3 and
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for the
+ * Additional Terms applicable to LinShare software.
+ */
+package org.linagora.linshare.webservice.adminv5.impl;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.linagora.linshare.core.exception.BusinessErrorCode;
+import org.linagora.linshare.core.exception.BusinessException;
+import org.linagora.linshare.core.facade.webservice.adminv5.dto.DomainQuotaDto;
+import org.linagora.linshare.webservice.WebserviceBase;
+import org.linagora.linshare.webservice.adminv5.DomainQuotaRestService;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+
+
+@Produces({MediaType.APPLICATION_JSON })
+@Consumes({MediaType.APPLICATION_JSON })
+@Path("/domains/{domainUuid}/domain_quotas")
+public class DomainQuotaRestServiceImpl extends WebserviceBase implements DomainQuotaRestService {
+
+//	private DomainQuotaFacade facade;
+//
+//	public DomainQuotaRestServiceImpl(DomainQuotaFacade facade) {
+//		this.facade = facade;
+//	}
+
+	@Path("/")
+	@GET
+	@Operation(summary = "Return domain quotas.", responses = {
+		@ApiResponse(
+			content = @Content(array = @ArraySchema(schema = @Schema(implementation = DomainQuotaDto.class))),
+			responseCode = "200"
+		)
+	})
+	@Override
+	public List<DomainQuotaDto> findAll(
+			@Parameter(description = "Filter domain quotas by its parent uuid.", required = false) 
+				@QueryParam("parent") String parentUuid) throws BusinessException {
+		throw new BusinessException(BusinessErrorCode.NOT_IMPLEMENTED_YET, "Not implemented yet.");
+	}
+
+	@Path("/{uuid}")
+	@GET
+	@Operation(summary = "find domain quota", responses = {
+		@ApiResponse(
+			content = @Content(array = @ArraySchema(schema = @Schema(implementation = DomainQuotaDto.class))),
+			responseCode = "200"
+		)
+	})
+	@Override
+	public DomainQuotaDto find(
+			@Parameter(description = "Domain quota Uuid", required = true)
+				@PathParam("uuid")
+					String uuid,
+			@Parameter(description = "Compute real time quota value. Carefull it could be time consuming.", required = false)
+				@QueryParam("realtime") @DefaultValue("false")
+					boolean realTime
+					) throws BusinessException {
+		throw new BusinessException(BusinessErrorCode.NOT_IMPLEMENTED_YET, "Not implemented yet.");
+	}
+
+	@Path("/{uuid : .*}")
+	@PUT
+	@Operation(summary = "Update a domain quota", responses = {
+		@ApiResponse(
+			content = @Content(array = @ArraySchema(schema = @Schema(implementation = DomainQuotaDto.class))),
+			responseCode = "200"
+		)
+	})
+	@Override
+	public DomainQuotaDto update(
+			@Parameter(description = "Domain quota to update. Only quota, override and maintenance field can be updated. If null they will be ignored.", required = true) DomainQuotaDto dto,
+			@Parameter(description = "Domain quota Uuid, if null dto.uuid is used.", required = false)
+				@PathParam("uuid") String uuid) throws BusinessException {
+		throw new BusinessException(BusinessErrorCode.NOT_IMPLEMENTED_YET, "Not implemented yet.");
+	}
+
+}
