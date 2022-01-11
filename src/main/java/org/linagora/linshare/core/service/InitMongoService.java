eec5750c1ae387ea016e97b14f9cd068437f5632
Antoine DUPRAT <aduprat@linagora.com>
2022-01-11 10:47:46 +0100

    Methods in interface are always public

--- src/main/java/org/linagora/linshare/core/service/InitMongoService.java
+++ src/main/java/org/linagora/linshare/core/service/InitMongoService.java
@@ -1,7 +1,7 @@
 /*
  * LinShare is an open source filesharing software developed by LINAGORA.
  * 
- * Copyright (C) 2018-2021 LINAGORA
+ * Copyright (C) 2018-2022 LINAGORA
  * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License as published by the Free
@@ -38,6 +38,6 @@ package org.linagora.linshare.core.service;
 import org.linagora.linshare.core.exception.BusinessException;
 
 public interface InitMongoService {
-	public void init() throws BusinessException;
+	void init() throws BusinessException;
 }
 
