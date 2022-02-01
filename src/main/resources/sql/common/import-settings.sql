10c6706774dfb8c7cae39b31f708f7dd4836b4f1
Frédéric MARTIN <fmartin@linagora.com>
2022-02-01 18:10:44 +0100

    Add post update hook to replace linshare version in sql script

--- src/main/resources/sql/common/import-settings.sql
+++ src/main/resources/sql/common/import-settings.sql
@@ -1,5 +1,5 @@
 -- LinShare version
-INSERT INTO version (id, version, creation_date) VALUES (1, '5.0.0', now());
+INSERT INTO version (id, version, creation_date) VALUES (1, '5.1.0', now());
 
 -- Sequence for hibernate
 SELECT setval('hibernate_sequence', 1000);
