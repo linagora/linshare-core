66dd1acbd1daf178ad94096ac237c3c127c4e65a
Frédéric MARTIN <fmartin@linagora.com>
2022-04-07 15:01:05 +0200

    Add specific error code when OIDC user provider is not found by its discriminant.

--- src/main/java/org/linagora/linshare/core/repository/hibernate/OIDCUserProviderRepositoryImpl.java
+++ src/main/java/org/linagora/linshare/core/repository/hibernate/OIDCUserProviderRepositoryImpl.java
@@ -3,61 +3,64 @@
  * 
  * Copyright (C) 2015-2022 LINAGORA
  * 
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version, provided you comply with the Additional Terms applicable for
- * LinShare software by Linagora pursuant to Section 7 of the GNU Affero General
- * Public License, subsections (b), (c), and (e), pursuant to which you must
- * notably (i) retain the display in the interface of the “LinShare™”
- * trademark/logo, the "Libre & Free" mention, the words “You are using the Free
- * and Open Source version of LinShare™, powered by Linagora © 2009–2022.
- * Contribute to Linshare R&D by subscribing to an Enterprise offer!”. You must
- * also retain the latter notice in all asynchronous messages such as e-mails
- * sent with the Program, (ii) retain all hypertext links between LinShare and
- * http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain
- * from infringing Linagora intellectual property rights over its trademarks and
- * commercial brands. Other Additional Terms apply, see
- * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for more
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for LinShare software by Linagora pursuant to
+ * Section 7 of the GNU Affero General Public License, subsections (b), (c), and (e), pursuant to which you must notably
+ * (i) retain the display in the interface of the “LinShare™” trademark/logo, the "Libre & Free" mention, the words “You
+ * are using the Free and Open Source version of LinShare™, powered by Linagora © 2009–2022. Contribute to Linshare R&D
+ * by subscribing to an Enterprise offer!”. You must also retain the latter notice in all asynchronous messages such as
+ * e-mails sent with the Program, (ii) retain all hypertext links between LinShare and http://www.linshare.org, between
+ * linagora.com and Linagora, and (iii) refrain from infringing Linagora intellectual property rights over its
+ * trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for more details.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Affero General Public License and
- * its applicable Additional Terms for LinShare along with this program. If not,
- * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License
- * version 3 and
- * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for the
- * Additional Terms applicable to LinShare software.
+ * You should have received a copy of the GNU Affero General Public License and its applicable Additional Terms for
+ * LinShare along with this program. If not, see <http://www.gnu.org/licenses/> for the GNU Affero General Public
+ * License version 3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for the Additional Terms
+ * applicable to LinShare software.
  */
 package org.linagora.linshare.core.repository.hibernate;
 
+import java.util.Optional;
+
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Restrictions;
+import org.linagora.linshare.core.domain.entities.AbstractDomain;
 import org.linagora.linshare.core.domain.entities.OIDCUserProvider;
 import org.linagora.linshare.core.repository.OIDCUserProviderRepository;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
-public class OIDCUserProviderRepositoryImpl extends AbstractRepositoryImpl<OIDCUserProvider> implements OIDCUserProviderRepository {
+public class OIDCUserProviderRepositoryImpl extends AbstractRepositoryImpl<OIDCUserProvider>
+		implements OIDCUserProviderRepository {
 
 	public OIDCUserProviderRepositoryImpl(HibernateTemplate hibernateTemplate) {
 		super(hibernateTemplate);
 	}
 
 	@Override
-	public OIDCUserProvider findByDomainDiscriminator(String domainDiscriminator) {
-		return DataAccessUtils.singleResult(findByCriteria(Restrictions.eq(
-				"domainDiscriminator", domainDiscriminator)));
+	public Optional<OIDCUserProvider> findByDomainDiscriminator(String discriminator) {
+		return Optional.ofNullable(
+				DataAccessUtils.singleResult(findByCriteria(Restrictions.eq("domainDiscriminator", discriminator))));
+	}
+
+	@Override
+	public boolean isDomainDiscriminatorAlreadyInUse(String discriminator, AbstractDomain domain) {
+		DetachedCriteria det = DetachedCriteria.forClass(OIDCUserProvider.class);
+		det.add(Restrictions.eq("domainDiscriminator", discriminator));
+		det.add(Restrictions.not(Restrictions.eq("domain", domain)));
+		return DataAccessUtils.singleResult(findByCriteria(det)) != null;
 	}
 
 	@Override
 	protected DetachedCriteria getNaturalKeyCriteria(OIDCUserProvider entity) {
-		DetachedCriteria det = DetachedCriteria.forClass(OIDCUserProvider.class).add(
-				Restrictions.eq("id", entity.getId()));
+		DetachedCriteria det = DetachedCriteria.forClass(OIDCUserProvider.class)
+				.add(Restrictions.eq("id", entity.getId()));
 		return det;
 	}
 }
