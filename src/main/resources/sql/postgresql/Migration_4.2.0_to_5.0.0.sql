557d60e293f82e2ba14f3e4696000d88c9df6fd6
Marwan JEBAHI <mjebahi@linagora.com>
2022-06-27 16:32:31 +0200

    Update drive to workspace on email notification

--- src/main/resources/sql/postgresql/Migration_4.2.0_to_5.0.0.sql
+++ src/main/resources/sql/postgresql/Migration_4.2.0_to_5.0.0.sql
@@ -1246,6 +1246,206 @@ recipientsURequest = Получатели
 subjectForOwner =  Срок действия вашего приглашения заканчивается.
 subjectForRecipient =  {0} {1}''''s срок действия вашего приглашения заканчивается.
 uploadFileBtn = Загрузить файл' WHERE id=12;
+
+-- WORK_SPACE_WARN_NEW_MEMBER
+UPDATE mail_content SET subject='[( #{subject(${workSpaceName})})]',body='<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+<head  data-th-replace="layout :: header"></head>
+<body>
+<div th:replace="layout :: email_base(upperMainContentArea = ~{::#main-content},bottomSecondaryContentArea = ~{::#secondary-content})">
+  <!--/* Upper main-content*/-->
+  <section id="main-content">
+    <div th:replace="layout :: contentUpperSection( ~{::#section-content})">
+      <div id="section-content">
+        <!--/* Greetings */-->
+        <th:block data-th-replace="layout :: greetings(${member.firstName})"/>
+        <!--/* End of Greetings  */-->
+        <!--/* Main email  message content*/-->
+        <p>
+            <span th:if="${owner.firstName} !=null AND ${owner.lastName} !=null" data-th-utext="#{mainMsg(${owner.firstName},${owner.lastName})}"></span>
+            <span th:if="${owner.firstName} ==null OR ${owner.lastName} ==null" data-th-utext="#{simpleMainMsg}"></span>
+            <span>
+              <a target="_blank" style="color:#1294dc;text-decoration:none;"  data-th-text="${workSpaceName}" th:href="@{${workSpaceLink}}" >
+               link
+             </a>
+            </span>
+          <!--/* Activation link for initialisation of the guest account */-->
+             </p> <!--/* End of Main email  message content*/-->
+      </div><!--/* End of section-content*/-->
+    </div><!--/* End of main-content container*/-->
+  </section> <!--/* End of upper main-content*/-->
+  <!--/* Secondary content for  bottom email section */-->
+  <section id="secondary-content">
+    <th:block th:switch="${workSpaceMember.role.name}">
+      <p th:case="''WORK_SPACE_ADMIN''"> <th:block data-th-replace="layout :: infoStandardArea(#{workSpaceRight}, #{workSpaceRoleAdminTitle})"/></p>
+      <p th:case="''WORK_SPACE_WRITER''"> <th:block data-th-replace="layout :: infoStandardArea(#{workSpaceRight}, #{workSpaceRoleWriteTitle})"/></p>
+      <p th:case="''WORK_SPACE_READER''"> <th:block data-th-replace="layout :: infoStandardArea(#{workSpaceRight}, #{workSpaceRoleReadTitle})"/></p>
+    </th:block>
+    <th:block data-th-replace="layout :: infoStandardArea(#{workSpaceNameTitle},${workSpaceName})"/>
+    <th:block data-th-replace="layout :: infoDateArea(#{workSpaceMemberCreationDateTitle},${workSpaceMember.creationDate})"/>
+    <div th:if="${!childMembers.isEmpty()}">
+      <th:block data-th-utext="#{nestedWorkGroupsList}"/>
+      <ul style="padding: 5px 17px; margin: 0;list-style-type:disc;">
+        <li style="color:#787878;font-size:10px" th:each="member : ${childMembers}">
+            <span style="color:#787878;font-size:13px">
+              <th:block data-th-utext="#{displayWorkSpaceAndRole(${member.node.name},${member.role.name})}"/>
+          </li>
+      </ul>  
+    </div>
+  </section>  <!--/* End of Secondary content for bottom email section */-->
+</div>
+</body>
+</html>',messages_french='workSpaceMemberCreationDateTitle = Date de création
+mainMsg =  <b> {0} <span style="text-transform:uppercase">{1}</span> </b> vous a ajouté au WorkSpace: <br>
+simpleMainMsg = Vous avez été ajouté au WorkSpace
+subject = Vous avez été ajouté au WorkSpace {0}
+workSpaceRight = Droit par défaut
+workSpaceNameTitle = Nom du WorkSpace
+nestedWorkGroupsList=Vous avez automatiquement été ajouté aux groupes de travail suivants :
+displayWorkSpaceAndRole ={0} avec un rôle <span style="text-transform:uppercase">{1}</span>',messages_english='workSpaceMemberCreationDateTitle = Creation date
+mainMsg = <b> {0} <span style="text-transform:uppercase">{1}</span></b> added you to the WorkSpace: <br>
+simpleMainMsg = You have been added to the WorkSpace
+subject = You have been added to the WorkSpace {0}
+workSpaceRight = Default right
+workSpaceNameTitle = WorkSpace Name
+nestedWorkGroupsList=You have been automatically added to the following workgroups:
+displayWorkSpaceAndRole ={0} with a <span style="text-transform:uppercase">{1}</span> role',messages_russian='workSpaceMemberCreationDateTitle = Creation date
+mainMsg = <b> {0} <span style="text-transform:uppercase">{1}</span></b> added you to the WorkSpace: <br>
+simpleMainMsg = You have been added to the WorkSpace
+subject = You have been added to the WorkSpace {0}
+workSpaceRight = Default right
+workSpaceNameTitle = WorkSpace Name
+nestedWorkGroupsList=You have been automatically added to the following workgroups:
+displayWorkSpaceAndRole ={0} with a <span style="text-transform:uppercase">{1}</span> role' WHERE id=34;
+
+
+-- WORK_SPACE_WARN_UPDATED_MEMBER
+UPDATE mail_content SET subject='[(#{subject(${workSpaceName})})]',body='<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+<head  data-th-replace="layout :: header"></head>
+<body>
+<div th:replace="layout :: email_base(upperMainContentArea = ~{::#main-content},bottomSecondaryContentArea = ~{::#secondary-content})">
+  <!--/* Upper main-content*/-->
+  <section id="main-content">
+    <div th:replace="layout :: contentUpperSection( ~{::#section-content})">
+      <div id="section-content">
+        <!--/* Greetings */-->
+        <th:block data-th-replace="layout :: greetings(${member.firstName})"/>
+        <!--/* End of Greetings  */-->
+        <!--/* Main email  message content*/-->
+        <p>
+          <span data-th-utext="#{mainMsg}"></span>
+          <span>
+               <a target="_blank" style="color:#1294dc;text-decoration:none;"  data-th-text="${workSpaceName}" th:href="@{${workSpaceLink}}" >
+                link </a>
+          </span>
+          <span data-th-utext="#{mainMsgNext}"></span>
+          <span th:if="${owner.firstName} != null AND ${owner.firstName} != null" data-th-utext="#{mainMsgNextBy(${owner.firstName},${owner.lastName})}"></span>
+
+             </p> <!--/* End of Main email  message content*/-->
+      </div><!--/* End of section-content*/-->
+    </div><!--/* End of main-content container*/-->
+  </section> <!--/* End of upper main-content*/-->
+  <!--/* Secondary content for  bottom email section */-->
+  <section id="secondary-content">
+    <th:block data-th-replace="layout :: infoStandardArea(#{workSpaceNameTitle},${workSpaceName})"/>
+    <th:block th:switch="${workSpaceMember.role.name}">
+      <p th:case="''WORK_SPACE_ADMIN''"> <th:block data-th-replace="layout :: infoStandardArea(#{workSpaceRight}, #{workSpaceRoleAdminTitle})"/></p>
+      <p th:case="''WORK_SPACE_WRITER''"> <th:block data-th-replace="layout :: infoStandardArea(#{workSpaceRight}, #{workSpaceRoleWriteTitle})"/></p>
+      <p th:case="''WORK_SPACE_READER''"> <th:block data-th-replace="layout :: infoStandardArea(#{workSpaceRight}, #{workSpaceRoleReadTitle})"/></p>
+    </th:block>
+    <th:block th:switch="${workSpaceMember.nestedRole.name}">
+      <p th:case="''ADMIN''"> <th:block data-th-replace="layout :: infoStandardArea(#{workGroupRight}, #{workGroupRightAdminTitle})"/></p>  
+      <p th:case="''CONTRIBUTOR''"> <th:block data-th-replace="layout :: infoStandardArea(#{workGroupRight}, #{workGroupRightWirteTitle})"/></p>
+      <p th:case="''WRITER''"> <th:block data-th-replace="layout :: infoStandardArea(#{workGroupRight}, #{workGroupRightWirteTitle})"/></p>
+      <p th:case="''READER''"> <th:block data-th-replace="layout :: infoStandardArea(#{workGroupRight}, #{workGroupRightReadTitle})"/></p>
+    </th:block>
+    <th:block data-th-replace="layout :: infoDateArea(#{workSpaceMemberUpdatedDateTitle},${workSpaceMember.modificationDate})"/>
+    <div th:if="${nbrWorkgroupsUpdated != 0}">
+    <th:block data-th-replace="layout :: infoStandardArea(#{nbrWorkgoups},${nbrWorkgroupsUpdated})"/>
+      <th:block data-th-utext="#{nestedWorkGroupsList}"/>
+      <ul>
+        <li  th:each="member : ${nestedMembers}">
+              <th:block data-th-utext="${member.node.name}"/>
+        </li>
+        <span th:if="${nbrWorkgroupsUpdated > 3}">
+             <li>...</li>
+        </span>
+      </ul>  
+    </div>
+  </section>  <!--/* End of Secondary content for bottom email section */-->
+</div>
+</body>
+</html>',messages_french='workSpaceMemberUpdatedDateTitle = Date de la mise à jour
+mainMsg = Vos droits sur le WorkSpace
+mainMsgNext = et dans ses WorkGroups contenus ont été mis à jour
+mainMsgNextBy= par <b> {0} <span style="text-transform:uppercase">{1}</span></b>.
+subject =  Vos droits sur le WorkSpace {0} ont été mis à jour
+workSpaceRight = Droit sur le WorkSpace
+workGroupRight =  Droit sur le groupe de travail
+workSpaceNameTitle = Nom du WorkSpace
+nestedWorkGroupsList = Liste des workgoups
+nbrWorkgoups = Nombre de groupe de travail mis à jours',messages_english='workSpaceMemberUpdatedDateTitle = Updated date
+mainMsg = Your roles on the WorkSpace
+mainMsgNext= and workgroups inside it, have been updated
+mainMsgNextBy= by <b> {0} <span style="text-transform:uppercase">{1}</span></b>.
+subject =  Your roles on the WorkSpace {0} was updated.
+workSpaceRight = WorkSpace right
+workGroupRight = Workgroup right
+workSpaceNameTitle = WorkSpace Name
+nestedWorkGroupsList = Workgroups list
+nbrWorkgoups = Number of updated workGroups',messages_russian='workSpaceMemberUpdatedDateTitle = Updated date
+mainMsg = Your roles on the WorkSpace
+mainMsgNext= and workgroups inside it, have been updated
+mainMsgNextBy= by <b> {0} <span style="text-transform:uppercase">{1}</span></b>.
+subject =  Your roles on the WorkSpace {0} was updated.
+workSpaceRight = WorkSpace right
+workGroupRight = Workgroup right
+workSpaceNameTitle = WorkSpace Name
+nestedWorkGroupsList = Workgroups list
+nbrWorkgoups = Number of updated workGroups' WHERE id=35;
+
+
+-- WORK_SPACE_WARN_DELETED_MEMBER
+UPDATE mail_content SET subject='[( #{subject(${workGroupName})})]',body='<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+<head  data-th-replace="layout :: header"></head>
+<body>
+<div th:replace="layout :: email_base(upperMainContentArea = ~{::#main-content},bottomSecondaryContentArea = ~{::#secondary-content})">
+  <!--/* Upper main-content*/-->
+  <section id="main-content">
+    <div th:replace="layout :: contentUpperSection( ~{::#section-content})">
+      <div id="section-content">
+        <!--/* Greetings */-->
+        <th:block data-th-replace="layout :: greetings(${member.firstName})"/>
+        <!--/* End of Greetings  */-->
+        <!--/* Main email  message content*/-->
+        <p>
+          <span th:if="${owner.firstName} !=null AND ${owner.lastName} !=null" data-th-utext="#{mainMsg(${owner.firstName},${owner.lastName},${workGroupName})}"></span>
+          <span th:if="${owner.firstName} ==null OR ${owner.lastName} ==null" data-th-utext="#{simpleMsg(${workGroupName})}"></span>
+            
+          <!--/* Activation link for initialisation of the guest account */-->
+             </p> <!--/* End of Main email  message content*/-->
+      </div><!--/* End of section-content*/-->
+    </div><!--/* End of main-content container*/-->
+  </section> <!--/* End of upper main-content*/-->
+  <!--/* Secondary content for  bottom email section */-->
+  <section id="secondary-content">
+    <th:block data-th-replace="layout :: infoStandardArea(#{workGroupNameTitle},${workGroupName})"/>
+  </section>  <!--/* End of Secondary content for bottom email section */-->
+</div>
+</body>
+</html>',messages_french='subject = Les accès au workspace {0} et à ses workgroups vous ont été retirés.
+mainMsg = <b> {0} <span style="text-transform:uppercase">{1}</span></b> vous a retiré du workspace <b>{2}</b>
+simpleMsg = Les accès au workspace <b>{0}</b> vous ont été retirés.
+workGroupNameTitle = Nom du workspace',messages_english='subject = Your access to the workspace {0}  and its workgroups was withdrawn
+mainMsg = <b> {0} <span style="text-transform:uppercase">{1}</span></b> removed you from the workspace  <b>{2}</b>
+simpleMsg =  Your access to the workspace <b>{0}</b> was withdrawn.
+workGroupNameTitle = Workspace Name',messages_russian='subject = Your access to the workspace {0}  and its workgroups was withdrawn
+mainMsg = <b> {0} <span style="text-transform:uppercase">{1}</span></b> removed you from the workspace  <b>{2}</b>
+simpleMsg =  Your access to the workspace <b>{0}</b> was withdrawn.
+workGroupNameTitle = Workspace Name' WHERE id=36;
+
 ---- End of your queries
 
 -- Upgrade LinShare version
