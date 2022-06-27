7226282d66400980d5e9467e7a7e2a08ae1c6c8d
Antoine DUPRAT <aduprat@linagora.com>
2022-06-27 12:20:19 +0200

    Enhance AddFirstLastNameToActorAuditsUpgradeTaskImpl

--- src/main/java/org/linagora/linshare/core/upgrade/v5_1/AddFirstLastNameToActorAuditsUpgradeTaskImpl.java
+++ src/main/java/org/linagora/linshare/core/upgrade/v5_1/AddFirstLastNameToActorAuditsUpgradeTaskImpl.java
@@ -35,10 +35,12 @@ package org.linagora.linshare.core.upgrade.v5_1;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.linagora.linshare.core.batches.impl.GenericUpgradeTaskImpl;
 import org.linagora.linshare.core.domain.constants.UpgradeTaskType;
 import org.linagora.linshare.core.domain.entities.Account;
+import org.linagora.linshare.core.domain.entities.SystemAccount;
 import org.linagora.linshare.core.domain.entities.User;
 import org.linagora.linshare.core.exception.BatchBusinessException;
 import org.linagora.linshare.core.exception.BusinessException;
@@ -50,6 +52,8 @@ import org.linagora.linshare.mongo.entities.logs.AuditLogEntryUser;
 import org.linagora.linshare.mongo.repository.AuditUserMongoRepository;
 import org.linagora.linshare.mongo.repository.UpgradeTaskLogMongoRepository;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.Fields;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
@@ -77,67 +81,90 @@ public class AddFirstLastNameToActorAuditsUpgradeTaskImpl extends GenericUpgrade
 
 	@Override
 	public List<String> getAll(BatchRunContext batchRunContext) {
-		Query query = Query.query(Criteria.where
+		Aggregation aggregation = Aggregation.newAggregation(
+			Aggregation.match(Criteria.where
 				("actor").exists(true).and
+				("actor.uuid").exists(true).and
 				("actor.firstName").exists(false).and
-				("actor.lastName").exists(false));
-		List<String> audits = mongoTemplate.findDistinct(query, "uuid", AuditLogEntryUser.class, String.class);
-		return audits;
+				("actor.lastName").exists(false)),
+			Aggregation.group("actor"),
+			Aggregation.project(Fields.from(Fields.field("actor.uuid"))));
+		return mongoTemplate.aggregate(aggregation, "audit_log_entries", UUID.class)
+			.getMappedResults()
+			.stream()
+			.map(UUID::getUuid)
+			.collect(Collectors.toUnmodifiableList());
+	}
+
+	private static class UUID {
+		private final String uuid;
+
+		public UUID(String uuid) {
+			this.uuid = uuid;
+		}
+
+		public String getUuid() {
+			return uuid;
+		}
 	}
 
 	@Override
 	public ResultContext execute(BatchRunContext batchRunContext, String identifier, long total, long position)
 			throws BatchBusinessException, BusinessException {
-		AuditLogEntryUser audit = userMongoRepository.findByUuid(identifier);
-		BatchResultContext<AuditLogEntryUser> res = new BatchResultContext<>(audit);
-		res.setProcessed(false);
-		if (audit == null) {
-			return res;
+		BatchResultContext batchResultContext = new BatchResultContext<>(identifier);
+		try {
+			String firstName;
+			String lastName;
+			Account account = accountRepository.findActivateAndDestroyedByLsUuid(identifier);
+			if (Objects.nonNull(account)) {
+				if (account instanceof User) {
+					User actor = (User) account;
+					firstName = actor.getFirstName();
+					lastName = actor.getLastName();
+				} else if (account instanceof SystemAccount) {
+					firstName = "System";
+					lastName = "System";
+				} else {
+					logger.warn("Not an user: %s", identifier);
+					firstName = "John";
+					lastName = "Doe";
+				}
+			} else {
+				logger.warn("Can not find actor with uuid:%s, fake first and last name will be used", identifier);
+				firstName = "John";
+				lastName = "Doe";
+			}
+			Query query = new Query();
+			query.addCriteria(Criteria.where("actor.uuid").is(identifier));
+			Update update = new Update();
+			update.set("actor.firstName", firstName);
+			update.set("actor.lastName", lastName);
+			mongoTemplate.updateMulti(query, update, AuditLogEntryUser.class);
+			batchResultContext.setProcessed(true);
+			return batchResultContext;
+		} catch (Exception e) {
+			logger.error("An error occurred", e);
+			throw new BatchBusinessException(batchResultContext, "fail");
 		}
-		String firstName;
-		String lastName;
-		Account account = accountRepository.findActivateAndDestroyedByLsUuid(audit.getActor().getUuid());
-		if (Objects.nonNull(account)) {
-			User actor = (User) account;
-			firstName = actor.getFirstName();
-			lastName = actor.getLastName();
-		} else {
-			logger.warn("Can not find actor with uuid:%s, fake first and last name will be used",
-					audit.getActor().getUuid());
-			firstName = "John";
-			lastName = "Doe";
-		}
-		updateAudit(identifier, firstName, lastName);
-		res.setProcessed(true);
-		return res;
-	}
-
-	private void updateAudit(String identifier, String firstName, String lastName) {
-		Query query = new Query();
-		query.addCriteria(Criteria.where("uuid").is(identifier));
-		Update update = new Update();
-		update.set("actor.firstName", firstName);
-		update.set("actor.lastName", lastName);
-		mongoTemplate.findAndModify(query, update, AuditLogEntryUser.class);
 	}
 
 	@Override
 	public void notify(BatchRunContext batchRunContext, ResultContext context, long total, long position) {
 		@SuppressWarnings("unchecked")
-		BatchResultContext<AuditLogEntryUser> res = (BatchResultContext<AuditLogEntryUser>) context;
-		AuditLogEntryUser resource = res.getResource();
+		BatchResultContext<String> res = (BatchResultContext<String>) context;
+		String resource = res.getResource();
 		if (resource != null) {
-			logInfo(batchRunContext, total, position, "First and last name were added successfully to actor on audit traces: " + resource.toString());
+			logInfo(batchRunContext, total, position, "First and last name were added successfully to actor on audit traces: " + resource);
 		} else {
-			logInfo(batchRunContext, total, position, "AuditLogEntryUser update were skipped, can not find related resource : " + res.toString());
+			logInfo(batchRunContext, total, position, "AuditLogEntryUser update were skipped, can not find related resource : " + res);
 		}
 	}
 
 	@Override
 	public void notifyError(BatchBusinessException exception, String identifier, long total, long position, BatchRunContext batchRunContext) {
 		@SuppressWarnings("unchecked")
-		BatchResultContext<AuditLogEntryUser> res = (BatchResultContext<AuditLogEntryUser>) exception.getContext();
-		AuditLogEntryUser resource = res.getResource();
+		BatchResultContext<String> res = (BatchResultContext<String>) exception.getContext();
+		String resource = res.getResource();
 		logError(total, position, exception.getMessage(), batchRunContext);
 		logger.error("Error occurred while adding first and last name to actor on audit traces : "
 				+ resource +
