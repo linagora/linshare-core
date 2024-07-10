484d6226b94a27836552b443b577505084f50b41
Walid Boudiche <wboudiche@linagora.com>
2024-07-10 17:19:11 +0000

    Update import-settings.sql

--- src/main/resources/sql/common/import-settings.sql
+++ src/main/resources/sql/common/import-settings.sql
@@ -1,5 +1,5 @@
 -- LinShare version
-INSERT INTO version (id, version, creation_date) VALUES (1, '6.2.0', now());
+INSERT INTO version (id, version, creation_date) VALUES (1, '6.3.0', now());
 
 -- Sequence for hibernate
 SELECT setval('hibernate_sequence', 1000);
