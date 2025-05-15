268f1e307d23ad2ca87bef25814773c2571e85ca
Manel MATTOUSSI <mmattoussi@linagora.com>
2025-05-15 18:58:38 +0100

    fix a column baseDn in the script sql

--- src/test/resources/import-test-user-synchronization.sql
+++ src/test/resources/import-test-user-synchronization.sql
@@ -1,5 +1,5 @@
 -- Third user provider used on OidcDomain
-INSERT INTO user_provider(id, uuid, provider_type, baseDn, creation_date, modification_date)
+INSERT INTO user_provider(id, uuid, provider_type, base_dn, creation_date, modification_date)
 VALUES
     (52, '6668197e-301e-11ec-8d3d-0242ac130003', 'OIDC_PROVIDER', 'ou=OidcDomain,dc=linshare,dc=org', now(), now()),
     (53, '6668197e-301e-11ec-8d3d-0242ac130053', 'OIDC_PROVIDER', null, now(), now());
